e8731629b671048d208a45b0e82df94ace68bec4
Micaiah Smith-Pierce <micaiah@smithpierce.net>
2020-09-11 05:25:13 -0400

    incorrect entropy stable implementation

--- include/kernels/local/cpg_euler_tensor.hpp
+++ include/kernels/local/cpg_euler_tensor.hpp
@@ -48,40 +48,53 @@ void cpg_euler_tensor(double * read, double * write, int n_elem,
             }
           }
 
-          // Calculate flux
-          double flux [n_var*row_size];
+          // Calculate intermediary variables related to the flux
+          double flux_var [(n_var + 2)*row_size];
           for (int i_qpoint = 0; i_qpoint < row_size; ++i_qpoint)
           {
             #define READ(i) row_r[(i)*row_size + i_qpoint]
-            #define FLUX(i) flux[(i)*row_size + i_qpoint]
-            double veloc = READ(i_axis)/READ(n_var - 2);
-            double pres = 0;
+            #define FLUX(i) flux_var[(i)*row_size + i_qpoint]
+            FLUX(n_var - 2) = READ(i_axis);
+            FLUX(n_var - 1) = 0;
             for (int j_axis = 0; j_axis < n_var - 2; ++j_axis)
             {
-              FLUX(j_axis) = READ(j_axis)*veloc;
-              pres += READ(j_axis)*READ(j_axis)/READ(n_var - 2);
+              FLUX(j_axis) = READ(j_axis)/READ(n_var - 2);
+              FLUX(n_var - 1) += FLUX(j_axis)*READ(j_axis);
             }
-            pres = (sp_heat_rat - 1.)*(READ(n_var - 1) - 0.5*pres);
-            FLUX(i_axis) += pres;
-            FLUX(n_var - 2) = READ(i_axis);
-            FLUX(n_var - 1) = (READ(n_var - 1) + pres)*veloc;
+            FLUX(n_var - 1) = READ(i_axis)/2*(sp_heat_rat - 1.)*(READ(n_var - 1) - 0.5*FLUX(n_var - 1));
+            FLUX(n_var) = std::log(READ(n_var - 2));
+            FLUX(n_var + 1) = std::log(READ(n_var - 1));
             #undef FLUX
             #undef READ
           }
 
           // Differentiate flux
-          for (int i_var = 0; i_var < n_var; ++i_var)
+          for (int i_qpoint = 0; i_qpoint < row_size; ++i_qpoint)
           {
-            for (int i_qpoint = 0; i_qpoint < row_size; ++i_qpoint)
+            for (int i_var = 0; i_var < n_var; ++i_var)
             {
-              row_w[i_var*row_size + i_qpoint] = 0;
-              for (int j_qpoint = 0; j_qpoint < row_size; ++j_qpoint)
+             row_w[i_var*row_size + i_qpoint] = 0;
+            }
+            for (int j_qpoint = 0; j_qpoint < row_size; ++j_qpoint)
+            {
+              #define WRITE(i) row_w[(i)*row_size + i_qpoint]
+              double coef = cfl*diff_mat[i_qpoint*row_size + j_qpoint];
+              double mass_log = (flux_var[(n_var - 2)*row_size + i_qpoint] - flux_var[(n_var - 2)*row_size + j_qpoint])/
+                                (flux_var[(n_var - 0)*row_size + i_qpoint] - flux_var[(n_var - 0)*row_size + j_qpoint]);
+              double beta_log = (flux_var[(n_var - 1)*row_size + i_qpoint] - flux_var[(n_var - 1)*row_size + j_qpoint])/
+                                (flux_var[(n_var + 1)*row_size + i_qpoint] - flux_var[(n_var + 1)*row_size + j_qpoint]);
+              double mass_ari = 0.5*(flux_var[(n_var - 2)*row_size + i_qpoint] + flux_var[(n_var - 2)*row_size + j_qpoint]);
+              double beta_ari = 0.5*(flux_var[(n_var - 1)*row_size + i_qpoint] + flux_var[(n_var - 1)*row_size + j_qpoint]);
+              double veloc_ari = 0.5*(flux_var[i_axis*row_size + i_qpoint] + flux_var[i_axis*row_size + j_qpoint]);
+              WRITE(n_var - 2) += coef*mass_log*veloc_ari;
+              double kin_ener = 0;
+              for (int j_axis = 0; j_axis < n_var - 2; ++j_axis)
               {
-                row_w[i_var*row_size + i_qpoint]
-                += diff_mat[i_qpoint*row_size + j_qpoint]*(  flux[i_var*row_size + j_qpoint]
-                                                           + flux[i_var*row_size + j_qpoint]);
+                double veloc_ari_j = 0.5*(flux_var[j_axis*row_size + i_qpoint] + flux_var[j_axis*row_size + j_qpoint]);
+                WRITE(j_axis) = WRITE(n_var - 2)*veloc_ari_j + mass_ari/(2*beta_ari);
+                kin_ener += 0.5*(flux_var[j_axis*row_size + i_qpoint]*flux_var[j_axis*row_size + i_qpoint] + flux_var[j_axis*row_size + j_qpoint]*flux_var[j_axis*row_size + j_qpoint]);
               }
-              row_w[i_var*row_size + i_qpoint] *= cfl;
+              WRITE(n_var - 1) = (0.5/((sp_heat_rat - 1.)*beta_log) - 0.5*kin_ener)*WRITE(n_var - 2) + 0;
             }
           }
 
