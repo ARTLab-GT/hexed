466c6bea655d289d076354ba6f25fcb95c828394
Micaiah Smith-Pierce <micaiah@smithpierce.net>
2020-09-20 21:35:35 -0400

    write kernel to invert read kernel

--- test/test_neighbor.cpp
+++ test/test_neighbor.cpp
@@ -1,6 +1,7 @@
 #include "catch.hpp"
 
 #include "kernels/neighbor/read_copy.hpp"
+#include "kernels/neighbor/write_copy.hpp"
 
 TEST_CASE("neighbor kernel read_copy<>()")
 {
@@ -79,5 +80,103 @@ TEST_CASE("neighbor kernel read_copy<>()")
     REQUIRE(write[0] == .2);
     REQUIRE(write[7] == 2.3);
     REQUIRE(write[8] == 2.6);
+  }
+}
+
+TEST_CASE("neighbor kernel write_copy<>()")
+{
+  SECTION("1D")
+  {
+    double read [12];
+    double write0 [3];
+    double write1 [12];
+    for (int i = 0; i < 12; ++i)
+    {
+      read[i] = i/10.;
+      write1[i] = 0.;
+    }
+    #define COPY(stride, ipf) \
+      read_copy <3, 4, 4>(&read  [0], &write0[0], stride, ipf); \
+      write_copy<3, 4, 4>(&write0[0], &write1[0], stride, ipf);
+    COPY(3, 0);
+    COPY(3, 1);
+    COPY(1, 0);
+    COPY(1, 1);
+    #undef COPY
+    for (int i = 0; i < 12; ++i)
+    {
+      if ((i%4 == 0) || (i%4 == 3))
+      {
+        REQUIRE(read[i] == write1[i]);
+      }
+      else
+      {
+        REQUIRE(write1[i] == 0);
+      }
+    }
+  }
+
+  SECTION("2D")
+  {
+    double read[18];
+    double write0[6];
+    double write1[18];
+    for (int i = 0; i < 18; ++i)
+    {
+      read[i] = i/10.;
+      write1[i] = 0.;
+    }
+    #define COPY(stride, ipf) \
+      read_copy <2, 9, 3>(&read  [0], &write0[0], stride, ipf); \
+      write_copy<2, 9, 3>(&write0[0], &write1[0], stride, ipf);
+    COPY(3, 0);
+    COPY(3, 1);
+    COPY(1, 0);
+    COPY(1, 1);
+    #undef COPY
+    for (int i = 0; i < 18; ++i)
+    {
+      if (i%9 != 4)
+      {
+        REQUIRE(read[i] == write1[i]);
+      }
+      else
+      {
+        REQUIRE(write1[i] == 0);
+      }
+    }
+  }
+
+  SECTION("3D")
+  {
+    double read[27];
+    double write0[9];
+    double write1[27];
+    for (int i = 0; i < 27; ++i)
+    {
+      read[i] = i/10.;
+      write1[i] = 0.;
+    }
+    #define COPY(stride, ipf) \
+      read_copy <1, 27, 3>(&read  [0], &write0[0], stride, ipf); \
+      write_copy<1, 27, 3>(&write0[0], &write1[0], stride, ipf);
+    COPY(9, 0);
+    COPY(9, 1);
+    COPY(3, 0);
+    COPY(3, 1);
+    COPY(1, 0);
+    COPY(1, 1);
+    #undef COPY
+    for (int i = 0; i < 27; ++i)
+    {
+      if (i != 13)
+      {
+        REQUIRE(read[i] == write1[i]);
+      }
+      else
+      {
+        REQUIRE(write1[i] == 0);
+      }
+    }
   }
 }
