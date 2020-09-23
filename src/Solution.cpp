0a095ab28f1abcc6803c1141176ab13f0de0bc5d
Micaiah Smith-Pierce <micaiah@smithpierce.net>
2020-09-23 19:09:59 -0400

    fixed block grid positioning

--- src/Solution.cpp
+++ src/Solution.cpp
@@ -1,5 +1,6 @@
 #include "Solution.hpp"
 #include "Initializer.hpp"
+#include <iostream>
 
 Solution::Solution(int n_var_arg, int n_dim_arg, int rank_arg, double bms)
 : n_var(n_var_arg), n_dim(n_dim_arg), basis(rank_arg), base_mesh_size(bms) {}
@@ -10,7 +11,7 @@ Grid& Solution::get_grid(int order_added)
 {
   if (grids.size() <= order_added)
   {
-    throw "The requested grid does not exist.";
+    throw "The requested Grid does not exist.";
   }
   else
   {
@@ -24,6 +25,33 @@ void Solution::update()
 
 void Solution::initialize(Initializer& init)
 {
+  for (Grid& g : grids)
+  {
+    double* state = g.state_r();
+    for (int i_elem = 0; i_elem < g.n_elem; ++i_elem)
+    {
+      std::vector<double> pos = g.get_pos(i_elem);
+      for (int i_qpoint = 0; i_qpoint < g.n_qpoint; ++i_qpoint)
+      {
+        std::vector<double> qpoint_pos;
+        for (int i_dim = 0; i_dim < g.n_dim; ++i_dim)
+        {
+          qpoint_pos.push_back(pos[i_qpoint + i_dim*g.n_qpoint]);
+        }
+        std::vector<double> mmtm = init.momentum(qpoint_pos);
+        std::vector<double> scalar = init.scalar_state(qpoint_pos);
+        int qpoint_ind = i_elem*g.n_dof + i_qpoint;
+        for (int i_dim = 0; i_dim < g.n_dim; ++i_dim)
+        {
+          state[qpoint_ind + i_dim*g.n_qpoint] = mmtm[i_dim];
+        }
+        for (int i_var = g.n_dim; i_var < g.n_var; ++i_var)
+        {
+          state[qpoint_ind + i_var*g.n_qpoint] = scalar[i_var - g.n_dim];
+        }
+      }
+    }
+  }
 }
 
 void Solution::add_block_grid(int ref_level, std::vector<int> lower_corner,
@@ -37,6 +65,22 @@ void Solution::add_block_grid(int ref_level, std::vector<int> lower_corner,
   double mesh_size = base_mesh_size;
   for (int i_rl = 0; i_rl < ref_level; ++i_rl) mesh_size /= 2;
   Grid g (n_var, n_dim, n_elem, mesh_size, basis);
+  for (int i_dim = 0, stride = 1; i_dim < n_dim; ++i_dim)
+  {
+    int row_size = upper_corner[i_dim] - lower_corner[i_dim];
+    for (int i_outer = 0; i_outer < n_elem/(stride*row_size); ++i_outer)
+    {
+      for (int i_inner = 0; i_inner < stride; ++i_inner)
+      {
+        for (int i_row = 0; i_row < row_size; ++i_row)
+        {
+          g.pos[n_dim*(i_row*stride + i_outer*stride*row_size + i_inner) + i_dim]
+          = i_row + lower_corner[i_dim];
+        }
+      }
+    }
+    stride *= row_size;
+  }
   grids.push_back(g);
 }
 
--- test/test_Solution.cpp
+++ test/test_Solution.cpp
@@ -20,7 +20,7 @@ class Test_initializer : public Initializer
 
 TEST_CASE("Solution class")
 {
-  Solution sol (3, 2, 4, 0.7);
+  Solution sol (4, 2, 4, 0.7);
   Grid* g;
   REQUIRE_THROWS(g = &sol.get_grid(0));
   std::vector<int> lc {-1, -1};
@@ -34,6 +34,10 @@ TEST_CASE("Solution class")
     REQUIRE(g->state_r()[0] == 0.0);
     REQUIRE(g->n_elem == 6);
     REQUIRE(g->mesh_size == 0.35);
+    REQUIRE(g->pos[0] == -1);
+    REQUIRE(g->get_pos(0)[0] == -0.35);
+    REQUIRE(g->get_pos(1)[0] == -0.);
+    REQUIRE(g->get_pos(1)[16] == -0.35);
     g = &sol.get_grid(1);
     REQUIRE(g->basis.rank == 4);
     REQUIRE(g->n_elem == 16);
@@ -46,10 +50,11 @@ TEST_CASE("Solution class")
     sol.initialize(ti);
     g = &sol.get_grid(0);
     REQUIRE(g->basis.rank == 4);
+    REQUIRE(g->n_elem == 6);
     REQUIRE(g->state_r()[0] == 0.1);
-    REQUIRE(g->state_r()[1] == 0.2);
-    REQUIRE(g->state_r()[2] == -0.35);
-    REQUIRE(g->state_r()[3] == 2.5);
+    REQUIRE(g->state_r()[16] == 0.2);
+    REQUIRE(g->state_r()[32] == -0.35);
+    REQUIRE(g->state_r()[48] == 2.5);
     int size = g->n_dof*g->n_elem;
     REQUIRE(g->state_r()[size - 1] == 2.5);
   }
