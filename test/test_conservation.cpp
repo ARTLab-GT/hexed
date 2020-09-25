6bf1426af8c13cab8df71c41260c22770fca7d06
Micaiah Smith-Pierce <micaiah@smithpierce.net>
2020-09-24 23:56:48 -0400

    corrected conservation test (fails)

--- test/test_conservation.cpp
+++ test/test_conservation.cpp
@@ -5,12 +5,12 @@
 #include <Solution.hpp>
 #include <Initializer.hpp>
 
-class Zero_at_boundary_init : public Initializer
+class Linear_init : public Initializer
 {
   public:
   int dim;
 
-  Zero_at_boundary_init(int dim_arg) : dim(dim_arg) {}
+  Linear_init(int dim_arg) : dim(dim_arg) {}
 
   virtual std::vector<double> momentum(std::vector<double> position)
   {
@@ -34,15 +34,8 @@ class Zero_at_boundary_init : public Initializer
 
   void set_properties(std::vector<double> pos)
   {
-    mass = exp(pos[0]);
-    pressure = 1.e5*exp(-pos[0]);
-    for (int i = 0; i < dim; ++i)
-    {
-      mass *= pos[i]*(1. - pos[i]);
-      pressure *= pos[i]*(1. - pos[i]);
-    }
+    mass = 1.;
     pressure += 1.e5;
-    mass += 1.;
     energy = pressure/0.4;
     _momentum.clear();
     for (int i = 0; i < dim; ++i)
@@ -64,11 +57,15 @@ TEST_CASE("Conservation of state variables")
   {
     Solution sol (3, 1, rank, length);
     sol.add_block_grid(2);
-    Zero_at_boundary_init init (1);
+    Linear_init init (1);
     sol.initialize(init);
     Grid& grid = sol.get_grid(0);
+    double * state = grid.state_r();
+    for (int i_state = 0; i_state < grid.n_qpoint; ++i_state)
+    {
+      state[i_state] = i_state + 1;
+    }
     grid.visualize("conservation_1d");
-    sol.update();
     sol.update();
     double * state_r = grid.state_r();
     double * state_w = grid.state_w();
@@ -76,30 +73,61 @@ TEST_CASE("Conservation of state variables")
     {
       state_r[i_state] = state_w[i_state] - state_r[i_state];
     }
-    Eigen::VectorXd si = grid.state_integral();
-    std::cout << si << "\n";
+    for (int i_var = 0; i_var < grid.n_var; ++i_var)
+    {
+      REQUIRE(grid.state_integral()(i_var) == Approx(0.).epsilon(0.001));
+    }
   }
 
   SECTION("2D")
   {
     Solution sol (4, 2, rank, length);
     sol.add_block_grid(2);
-    Zero_at_boundary_init init (2);
+    Linear_init init (2);
     sol.initialize(init);
     Grid& grid = sol.get_grid(0);
-    sol.update();
+    double * state = grid.state_r();
+    for (int i_state = 0; i_state < grid.n_qpoint; ++i_state)
+    {
+      state[i_state] = i_state + 1;
+    }
     grid.visualize("conservation_2d");
+    sol.update();
+    double * state_r = grid.state_r();
+    double * state_w = grid.state_w();
+    for (int i_state = 0; i_state < grid.n_elem*grid.n_dof; ++i_state)
+    {
+      state_r[i_state] = state_w[i_state] - state_r[i_state];
+    }
+    for (int i_var = 0; i_var < grid.n_var; ++i_var)
+    {
+      REQUIRE(grid.state_integral()(i_var) == Approx(0.).epsilon(0.001));
+    }
   }
 
   SECTION("3D")
   {
     Solution sol (5, 3, rank, length);
     sol.add_block_grid(2);
-    Zero_at_boundary_init init (3);
+    Linear_init init (3);
     sol.initialize(init);
     Grid& grid = sol.get_grid(0);
+    double * state = grid.state_r();
+    for (int i_state = 0; i_state < grid.n_qpoint; ++i_state)
+    {
+      state[i_state] = i_state + 1;
+    }
     grid.visualize("conservation_3d");
     sol.update();
-    sol.update();
+    double * state_r = grid.state_r();
+    double * state_w = grid.state_w();
+    for (int i_state = 0; i_state < grid.n_elem*grid.n_dof; ++i_state)
+    {
+      state_r[i_state] = state_w[i_state] - state_r[i_state];
+    }
+    for (int i_var = 0; i_var < grid.n_var; ++i_var)
+    {
+      REQUIRE(grid.state_integral()(i_var) == Approx(0.).epsilon(0.001));
+    }
   }
 }
