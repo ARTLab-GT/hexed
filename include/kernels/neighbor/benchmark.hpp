099e010ef89a16e3ba4edaadfec8460071a50231
Micaiah Smith-Pierce <micaiah@smithpierce.net>
2020-09-25 02:36:56 -0400

    updated names

--- include/kernels/neighbor/benchmark.hpp
+++ include/kernels/neighbor/benchmark.hpp
@@ -3,7 +3,7 @@
 
 #include <kernels/neighbor/read_copy.hpp>
 #include <kernels/neighbor/write_copy.hpp>
-#include <kernels/neighbor/average_flux.hpp>
+#include <kernels/neighbor/average.hpp>
 
 template<int n_var, int n_qpoint, int row_size>
 void average_neighbor(double** connections_r, double** connections_w, int n_connections)
@@ -20,7 +20,7 @@ void average_neighbor(double** connections_r, double** connections_w, int n_conn
       const int con_offset = i_axis*n_connections + 2*i_con;
       read_copy<n_var, n_qpoint, row_size>(connections_r[con_offset    ], face0, stride, 1);
       read_copy<n_var, n_qpoint, row_size>(connections_r[con_offset + 1], face1, stride, 0);
-      average_flux<face_size>(&face0[0], &face1[0], &face_w[0]);
+      average<face_size>(&face0[0], &face1[0], &face_w[0]);
       write_copy<n_var, n_qpoint, row_size>(face_w, connections_w[con_offset    ], stride, 1);
       write_copy<n_var, n_qpoint, row_size>(face_w, connections_w[con_offset + 1], stride, 0);
     }
