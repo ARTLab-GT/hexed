65cf308e4605688d80b82ab6602e0c8721af0992
Micaiah Smith-Pierce <micaiah@smithpierce.net>
2020-10-16 16:14:27 -0400

    Revert "neighbor boundary kernel (not tested)"
    This reverts commit fa70b1ec6b6dda483dae2305b7a539145a1a25d2.

--- include/kernels/kernel_types.hpp
+++ include/kernels/kernel_types.hpp
@@ -8,10 +8,7 @@ namespace cartdg
 
 typedef void (*Local_kernel)(double*, double*, int, const Eigen::MatrixXd&, double, double);
 typedef void (*Neighbor_kernel)(double***, double***, int*, const Eigen::VectorXd,
-                                double, double);
-typedef void (*Ghost_function)(double* read, double* write, int n_points);
-typedef void (*Neighbor_boundary_kernel)(double***, double***, int*, const Eigen::VectorXd,
-                                         Ghost_function, double, double);
+              double, double);
 typedef double (*Max_char_speed_kernel)(double*, int, double);
 
 }
