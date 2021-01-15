0861c07129c7d9511821d55b7e652799b5cf7c27
Micaiah Smith-Pierce <micaiah@smithpierce.net>
2021-01-15 11:54:37 -0500

    test for Error_func (fails)

--- test/test_Domain_func.cpp
+++
@@ -1,8 +0,0 @@
-#include <catch.hpp>
-
-#include <Domain_func.hpp>
-
-TEST_CASE("Error_func")
-{
-  REQUIRE(false); // not implemented
-}
---
+++ test/test_func.cpp
@@ -0,0 +1,76 @@
+#include <catch.hpp>
+
+#include <Domain_func.hpp>
+
+class Arbitrary_func : public cartdg::Spacetime_func
+{
+  public:
+  virtual std::vector<double> operator()(std::vector<double> pos, double time)
+  {
+    auto result = pos;
+    double mult [] {-2, 0.3, 4, 0., 0.01};
+    for (int i_axis = 0; i_axis < int(pos.size()); ++i_axis)
+    {
+      result[i_axis] = pos[i_axis]*mult[i_axis] - 2*time;
+    }
+    return result;
+  }
+};
+
+TEST_CASE("Domain_func and Spacetime_func subclasses")
+{
+
+  std::vector<std::vector<double>> test_pos
+  {
+    {},
+    {0.2, 7.9},
+    {0.},
+    {0., 0., 0.},
+  };
+  std::vector<double> test_time {1, 0., 10, -1.3};
+  std::vector<std::vector<double>> test_state
+  {
+    {},
+    {1.3, 0.02},
+    {1.},
+    {0., 0., 0.},
+  };
+  std::vector<std::vector<double>> test_error
+  {
+    {},
+    {-2*0.2 - 2*0 - 1.3, 0.3*7.9 - 2*0 - 0.02},
+    {-2*0. - 2*10. - 1.},
+    {-2*-1.3, -2*-1.3, -2*-1.3},
+  };
+
+  SECTION("Constant_func")
+  {
+    cartdg::Constant_func cf;
+    cf.value = std::vector<double> {3.2, -0.7};
+    for (auto pos : test_pos)
+    {
+      for (auto time : test_time)
+      {
+        auto result = cf(pos, time);
+        REQUIRE(result.size() == 2);
+        REQUIRE(result[0] == 3.2);
+        REQUIRE(result[1] == -0.7);
+      }
+    }
+  }
+
+  SECTION("Error_func")
+  {
+    Arbitrary_func af;
+    cartdg::Error_func ef(af);
+    for (unsigned i_test = 0; i_test < test_pos.size(); ++i_test)
+    {
+      auto error = ef(test_pos[i_test], test_time[i_test], test_state[i_test]);
+      REQUIRE(error.size() == test_error[i_test].size());
+      for (unsigned i_val = 0; i_val < error.size(); ++i_val)
+      {
+        REQUIRE(error[i_val] == test_state[i_test][i_val]);
+      }
+    }
+  }
+}
