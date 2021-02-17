51bde40a0f5c84900b1bee460bb93ffef3f9c8f7
Micaiah Smith-Pierce <micaiah@smithpierce.net>
2021-02-17 16:09:44 -0500

    basic test for deformed kernel

--- test/test_local.cpp
+++ test/test_local.cpp
@@ -1,5 +1,6 @@
 #include <catch.hpp>
 #include <kernels/local/cpg_euler_matrix.hpp>
+#include <kernels/local/cpg_euler_deformed.hpp>
 
 TEST_CASE("CPG Euler matrix form")
 {
@@ -29,11 +30,11 @@ TEST_CASE("CPG Euler matrix form")
     {
       for (int i_qpoint = 0; i_qpoint < rank; ++i_qpoint)
       {
-        CHECK((write[i_elem][0][i_qpoint] - read[i_elem][0][i_qpoint])
+        REQUIRE((write[i_elem][0][i_qpoint] - read[i_elem][0][i_qpoint])
               == Approx(0.1*(mmtm*mmtm/mass + pres)));
-        CHECK((write[i_elem][1][i_qpoint] - read[i_elem][1][i_qpoint])
+        REQUIRE((write[i_elem][1][i_qpoint] - read[i_elem][1][i_qpoint])
               == Approx(0.1*mmtm));
-        CHECK((write[i_elem][2][i_qpoint] - read[i_elem][2][i_qpoint])
+        REQUIRE((write[i_elem][2][i_qpoint] - read[i_elem][2][i_qpoint])
               == Approx(0.1*((ener + pres)*veloc)));
       }
     }
@@ -67,15 +68,103 @@ TEST_CASE("CPG Euler matrix form")
     {
       for (int i_qpoint = 0; i_qpoint < rank*rank*rank; ++i_qpoint)
       {
-        CHECK((write[i_elem][0][i_qpoint] - read[i_elem][0][i_qpoint])
+        REQUIRE((write[i_elem][0][i_qpoint] - read[i_elem][0][i_qpoint])
               == Approx(0.1*(mass*veloc0*(veloc0 + veloc1 + veloc2) + pres)));
-        CHECK((write[i_elem][1][i_qpoint] - read[i_elem][1][i_qpoint])
+        REQUIRE((write[i_elem][1][i_qpoint] - read[i_elem][1][i_qpoint])
               == Approx(0.1*(mass*veloc1*(veloc0 + veloc1 + veloc2) + pres)));
-        CHECK((write[i_elem][2][i_qpoint] - read[i_elem][2][i_qpoint])
+        REQUIRE((write[i_elem][2][i_qpoint] - read[i_elem][2][i_qpoint])
               == Approx(0.1*(mass*veloc2*(veloc0 + veloc1 + veloc2) + pres)));
-        CHECK((write[i_elem][3][i_qpoint] - read[i_elem][3][i_qpoint])
+        REQUIRE((write[i_elem][3][i_qpoint] - read[i_elem][3][i_qpoint])
               == Approx(0.1*mass*(veloc0 + veloc1 + veloc2)));
-        CHECK((write[i_elem][4][i_qpoint] - read[i_elem][4][i_qpoint])
+        REQUIRE((write[i_elem][4][i_qpoint] - read[i_elem][4][i_qpoint])
+              == Approx(0.1*((ener + pres)*(veloc0 + veloc1 + veloc2))));
+      }
+    }
+  }
+}
+
+TEST_CASE("CPG Euler deformed elements")
+{
+  cartdg::Kernel_settings settings;
+  settings.d_t_by_d_pos = 0.1;
+  SECTION("1D regular")
+  {
+    const int n_elem = 5;
+    const int rank = 2;
+    double read [n_elem][3][rank];
+    double write[n_elem][3][rank];
+    double jacobian [n_elem][1][1][rank] {};
+    Eigen::MatrixXd diff_mat = -Eigen::MatrixXd::Identity(rank, rank);
+    double mass = 1.225; double veloc = 10; double pres = 1e5;
+    double mmtm = mass*veloc; double ener = pres/0.4 + 0.5*mass*veloc*veloc;
+    for (int i_elem = 0; i_elem < n_elem; ++i_elem)
+    {
+      for (int i_qpoint = 0; i_qpoint < rank; ++i_qpoint)
+      {
+          read[i_elem][0][i_qpoint] = mmtm;
+          read[i_elem][1][i_qpoint] = mass;
+          read[i_elem][2][i_qpoint] = ener;
+      }
+    }
+    cartdg::cpg_euler_deformed<3, 2, 2>(&read[0][0][0], &write[0][0][0], 
+                                        &jacobian[0][0][0][0], n_elem,
+                                        diff_mat, settings);
+    for (int i_elem = 0; i_elem < n_elem; ++i_elem)
+    {
+      for (int i_qpoint = 0; i_qpoint < rank; ++i_qpoint)
+      {
+        REQUIRE((write[i_elem][0][i_qpoint] - read[i_elem][0][i_qpoint])
+              == Approx(0.1*(mmtm*mmtm/mass + pres)));
+        REQUIRE((write[i_elem][1][i_qpoint] - read[i_elem][1][i_qpoint])
+              == Approx(0.1*mmtm));
+        REQUIRE((write[i_elem][2][i_qpoint] - read[i_elem][2][i_qpoint])
+              == Approx(0.1*((ener + pres)*veloc)));
+      }
+    }
+  }
+
+  SECTION("3D regular")
+  {
+    const int n_elem = 5;
+    const int rank = 3;
+    double read [n_elem][5][rank*rank*rank];
+    double write[n_elem][5][rank*rank*rank];
+    double jacobian [n_elem][3][3][rank*rank*rank] {};
+    Eigen::MatrixXd diff_mat = -Eigen::MatrixXd::Identity(rank, rank);
+    double mass = 1.225;
+    double veloc0 = 10; double veloc1 = -20; double veloc2 = 30;
+    double pres = 1e5;
+    double ener = pres/0.4 + 0.5*mass*(veloc0*veloc0 + veloc1*veloc1 + veloc2*veloc2);
+    for (int i_elem = 0; i_elem < n_elem; ++i_elem)
+    {
+      for (int i_qpoint = 0; i_qpoint < rank*rank*rank; ++i_qpoint)
+      {
+          read[i_elem][0][i_qpoint] = mass*veloc0;
+          read[i_elem][1][i_qpoint] = mass*veloc1;
+          read[i_elem][2][i_qpoint] = mass*veloc2;
+          read[i_elem][3][i_qpoint] = mass;
+          read[i_elem][4][i_qpoint] = ener;
+          jacobian[i_elem][0][0][i_qpoint] = 1.;
+          jacobian[i_elem][1][1][i_qpoint] = 1.;
+          jacobian[i_elem][2][2][i_qpoint] = 1.;
+      }
+    }
+    cartdg::cpg_euler_deformed<5, 27, 3>(&read[0][0][0], &write[0][0][0], 
+                                         &jacobian[0][0][0][0], n_elem,
+                                         diff_mat, settings);
+    for (int i_elem = 0; i_elem < n_elem; ++i_elem)
+    {
+      for (int i_qpoint = 0; i_qpoint < rank*rank*rank; ++i_qpoint)
+      {
+        REQUIRE((write[i_elem][0][i_qpoint] - read[i_elem][0][i_qpoint])
+              == Approx(0.1*(mass*veloc0*(veloc0 + veloc1 + veloc2) + pres)));
+        REQUIRE((write[i_elem][1][i_qpoint] - read[i_elem][1][i_qpoint])
+              == Approx(0.1*(mass*veloc1*(veloc0 + veloc1 + veloc2) + pres)));
+        REQUIRE((write[i_elem][2][i_qpoint] - read[i_elem][2][i_qpoint])
+              == Approx(0.1*(mass*veloc2*(veloc0 + veloc1 + veloc2) + pres)));
+        REQUIRE((write[i_elem][3][i_qpoint] - read[i_elem][3][i_qpoint])
+              == Approx(0.1*mass*(veloc0 + veloc1 + veloc2)));
+        REQUIRE((write[i_elem][4][i_qpoint] - read[i_elem][4][i_qpoint])
               == Approx(0.1*((ener + pres)*(veloc0 + veloc1 + veloc2))));
       }
     }
