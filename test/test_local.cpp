e3dadecbfd118f73dc70fc38d421cd7b3b0479fc
Micaiah Smith-Pierce <micaiah@smithpierce.net>
2021-02-17 20:41:51 -0500

    test for deformed case (fails)

--- test/test_local.cpp
+++ test/test_local.cpp
@@ -210,4 +210,52 @@ TEST_CASE("CPG Euler deformed elements")
       }
     }
   }
+
+  SECTION("2D non-constant deformed")
+  {
+    const int n_elem = 5;
+    const int rank = 6;
+    double read [n_elem][4][rank*rank];
+    double write[n_elem][4][rank*rank];
+    double jacobian [n_elem][2][2][rank*rank] {};
+    cartdg::Gauss_lobatto basis (rank);
+    Eigen::MatrixXd diff_mat = basis.diff_mat();
+    for (int i_elem = 0; i_elem < n_elem; ++i_elem)
+    {
+      for (int i = 0; i < rank; ++i)
+      {
+        for (int j = 0; j < rank; ++j)
+        {
+          int i_qpoint = i*rank + j;
+          double pos0 = basis.node(i)*(1. - 0.5*basis.node(j));
+          double pos1 = basis.node(j)*(1. - 0.3*basis.node(i));
+          double mass = 1 + 0.1*pos0 + 0.2*pos1;
+          double veloc0 = 10; double veloc1 = -20;
+          double pres = 1e5;
+          double ener = pres/0.4 + 0.5*mass*(veloc0*veloc0 + veloc1*veloc1);
+
+          read[i_elem][0][i_qpoint] = mass*veloc0;
+          read[i_elem][1][i_qpoint] = mass*veloc1;
+          read[i_elem][2][i_qpoint] = mass;
+          read[i_elem][3][i_qpoint] = ener;
+
+          jacobian[i_elem][0][0][i_qpoint] = 1.;
+          jacobian[i_elem][0][1][i_qpoint] = -0.5*basis.node(i);
+          jacobian[i_elem][1][0][i_qpoint] = -0.3*basis.node(j);
+          jacobian[i_elem][1][1][i_qpoint] = 1.;
+        }
+      }
+    }
+    cartdg::cpg_euler_deformed<4, rank*rank, rank>(&read[0][0][0], &write[0][0][0],
+                                                   &jacobian[0][0][0][0], n_elem,
+                                                   diff_mat, settings);
+    for (int i_elem = 0; i_elem < n_elem; ++i_elem)
+    {
+      for (int i_qpoint = 0; i_qpoint < rank*rank; ++i_qpoint)
+      {
+        CHECK((write[i_elem][2][i_qpoint] - read[i_elem][2][i_qpoint])
+                == Approx(-0.1*(0.1*10 - 0.2*20)));
+      }
+    }
+  }
 }
