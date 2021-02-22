2bac528f2cba51ddb820eab89ae3e96484701d6a
Micaiah Smith-Pierce <micaiah@smithpierce.net>
2021-02-22 04:32:51 -0500

    cleaned up kernels

--- include/kernels/neighbor/cpg_euler_hll.hpp
+++ include/kernels/neighbor/cpg_euler_hll.hpp
@@ -12,12 +12,12 @@ void cpg_euler_hll(double* state_r, double* d_flux_w, double mult,
   const int face_size = n_var*n_face_qpoint;
   for (int i_qpoint = 0; i_qpoint < n_face_qpoint; ++i_qpoint)
   {
-    double flux[n_var*2];
+    double flux[2][n_var];
     double wave_speed[2];
     for (int i_side = 0; i_side < 2; ++i_side)
     {
       #define READ(i) state_r[(i)*n_face_qpoint + i_qpoint + i_side*face_size]
-      #define FLUX(i) flux[i + n_var*i_side]
+      #define FLUX(i) flux[i_side][i]
       double veloc = READ(i_axis)/READ(n_var - 2);
       double pres = 0;
       for (int j_axis = 0; j_axis < n_var - 2; ++j_axis)
@@ -35,26 +35,22 @@ void cpg_euler_hll(double* state_r, double* d_flux_w, double mult,
     }
     for (int i_var = 0; i_var < n_var; ++i_var)
     {
+      const int i = i_var*n_face_qpoint + i_qpoint;
       double num_flux;
       // Note: these conditions are different from the standard ones, but it rarely makes
       // a difference, except in the case of opposing supersonic flows, where this behavior
       // is preferable
-      if      (std::min(wave_speed[0], wave_speed[1]) >= 0) num_flux = flux[i_var];
-      else if (std::max(wave_speed[1], wave_speed[0]) <= 0) num_flux = flux[i_var + n_var];
+      if      (std::min(wave_speed[0], wave_speed[1]) >= 0) num_flux = flux[0][i_var];
+      else if (std::max(wave_speed[1], wave_speed[0]) <= 0) num_flux = flux[1][i_var];
       else
       {
-        num_flux = (  wave_speed[1]*flux[i_var        ]
-                    - wave_speed[0]*flux[i_var + n_var]
-                    + wave_speed[0]*wave_speed[1]*(
-                      state_r[i_var*n_face_qpoint + i_qpoint + face_size]
-                    - state_r[i_var*n_face_qpoint + i_qpoint]))
+        num_flux = (wave_speed[1]*flux[0][i_var] - wave_speed[0]*flux[1][i_var]
+                    + wave_speed[0]*wave_speed[1]*(state_r[i + face_size] - state_r[i]))
                    / (wave_speed[1] - wave_speed[0]);
       }
 
-      d_flux_w[i_qpoint + i_var*n_face_qpoint            ]
-      = (flux[i_var] - num_flux)*mult;
-      d_flux_w[i_qpoint + i_var*n_face_qpoint + face_size]
-      = (num_flux - flux[i_var + n_var])*mult;
+      d_flux_w[i            ] = (flux[0][i_var] - num_flux)*mult;
+      d_flux_w[i + face_size] = (num_flux - flux[1][i_var])*mult;
     }
   }
 }
--- include/kernels/neighbor/cpg_euler_hll_deformed.hpp
+++ include/kernels/neighbor/cpg_euler_hll_deformed.hpp
@@ -18,30 +18,35 @@ void cpg_euler_hll_deformed(double* state_r, double* d_flux_w, double* jacobian,
     double flux[2][n_var];
     double wave_speed[2];
     double jacobian_det[2];
+
     for (int i_side = 0; i_side < 2; ++i_side)
     {
+      #define READ(i) state_r[(i)*n_face_qpoint + i_qpoint + i_side*face_size]
+      double pres = 0;
+      for (int i_axis = 0; i_axis < n_dim; ++i_axis)
+      {
+        pres += READ(i_axis)*READ(i_axis)/READ(n_var - 2);
+      }
+      pres = (sp_heat_rat - 1.)*(READ(n_var - 1) - 0.5*pres);
+      double sound_speed = std::sqrt(sp_heat_rat*pres/READ(n_var - 2));
+
       double qpoint_flux [n_dim][n_var];
       double veloc [n_dim];
-      double sound_speed;
       Eigen::Matrix<double, n_dim, n_dim> jac_mat;
       for (int i_axis = 0; i_axis < n_dim; ++i_axis)
       {
-        #define READ(i) state_r[(i)*n_face_qpoint + i_qpoint + i_side*face_size]
         veloc[i_axis] = READ(i_axis)/READ(n_var - 2);
-        double pres = 0;
         for (int j_axis = 0; j_axis < n_var - 2; ++j_axis)
         {
           qpoint_flux[i_axis][j_axis] = READ(j_axis)*veloc[i_axis];
-          pres += READ(j_axis)*READ(j_axis)/READ(n_var - 2);
-          jac_mat(i_axis, j_axis) = jacobian[(i_axis*n_dim + j_axis)*n_face_qpoint + i_qpoint + i_side*n_dim*n_dim*n_face_qpoint];
+          jac_mat(i_axis, j_axis) = jacobian[(i_axis*n_dim + j_axis
+                                              + i_side*n_dim*n_dim)*n_face_qpoint + i_qpoint];
         }
-        pres = (sp_heat_rat - 1.)*(READ(n_var - 1) - 0.5*pres);
         qpoint_flux[i_axis][i_axis] += pres;
         qpoint_flux[i_axis][n_var - 2] = READ(i_axis);
         qpoint_flux[i_axis][n_var - 1] = (READ(n_var - 1) + pres)*veloc[i_axis];
-        sound_speed = std::sqrt(sp_heat_rat*pres/READ(n_var - 2));
-        #undef READ
       }
+      #undef READ
 
       const int normal_dir = flip[i_side] ? -1 : 1;
       jacobian_det[i_side] = jac_mat.determinant();
@@ -54,10 +59,7 @@ void cpg_euler_hll_deformed(double* state_r, double* d_flux_w, double* jacobian,
         flux[i_side][i_var] = jac_mat.determinant()*normal_dir;
       }
       double normal_magnitude = 0.;
-      for (int i_axis = 0; i_axis < n_dim; ++i_axis)
-      {
-        jac_mat(i_axis, i_axis_arg[i_side]) = 0;
-      }
+      jac_mat.col(i_axis_arg[i_side]) = Eigen::Matrix<double, n_dim, 1>::Zero();
       for (int i_axis = 0; i_axis < n_dim; ++i_axis)
       {
         jac_mat(i_axis, i_axis_arg[i_side]) = 1;
@@ -75,6 +77,7 @@ void cpg_euler_hll_deformed(double* state_r, double* d_flux_w, double* jacobian,
 
     for (int i_var = 0; i_var < n_var; ++i_var)
     {
+      const int i = i_var*n_face_qpoint + i_qpoint;
       double num_flux;
       // Note: these conditions are different from the standard ones, but it rarely makes
       // a difference, except in the case of opposing supersonic flows, where this behavior
@@ -83,16 +86,13 @@ void cpg_euler_hll_deformed(double* state_r, double* d_flux_w, double* jacobian,
       else if (std::max(wave_speed[1], wave_speed[0]) <= 0) num_flux = flux[1][i_var];
       else
       {
-        num_flux = (  wave_speed[1]*flux[0][i_var]
-                    - wave_speed[0]*flux[1][i_var]
-                    + wave_speed[0]*wave_speed[1]*(
-                      state_r[i_var*n_face_qpoint + i_qpoint + face_size]
-                    - state_r[i_var*n_face_qpoint + i_qpoint]))
+        num_flux = (wave_speed[1]*flux[0][i_var] - wave_speed[0]*flux[1][i_var]
+                    + wave_speed[0]*wave_speed[1]*(state_r[i + face_size] - state_r[i]))
                    / (wave_speed[1] - wave_speed[0]);
       }
 
-      d_flux_w[i_qpoint + i_var*n_face_qpoint            ] = (flux[0][i_var] - num_flux)*mult/jacobian_det[0];
-      d_flux_w[i_qpoint + i_var*n_face_qpoint + face_size] = (num_flux - flux[1][i_var])*mult/jacobian_det[1];
+      d_flux_w[i            ] = (flux[0][i_var] - num_flux)*mult/jacobian_det[0];
+      d_flux_w[i + face_size] = (num_flux - flux[1][i_var])*mult/jacobian_det[1];
     }
   }
 }
