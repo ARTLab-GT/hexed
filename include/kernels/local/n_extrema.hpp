83025bd3f656d4607960ebfddc8d4b6c67f50afb
Micaiah Smith-Pierce <micaiah@smithpierce.net>
2021-03-11 13:21:32 -0500

    test for n_extrema (fails)

---
+++ include/kernels/local/n_extrema.hpp
@@ -0,0 +1,14 @@
+#ifndef N_EXTREMA_HPP_
+#define N_EXTREMA_HPP_
+
+namespace cartdg
+{
+template<int row_size>
+int n_extrema(double*)
+{
+  return 0;
+}
+
+}
+
+#endif
---
+++ test/test_tvd.cpp
@@ -0,0 +1,22 @@
+#include <catch.hpp>
+
+#include <cartdgConfig.hpp>
+#include <kernels/local/n_extrema.hpp>
+
+TEST_CASE("n_extrema")
+{
+  double read [7] {};
+  REQUIRE(cartdg::n_extrema<0>(read) == 0);
+  REQUIRE(cartdg::n_extrema<1>(read) == 0);
+  read[1] = 1.;
+  REQUIRE(cartdg::n_extrema<2>(read) == 0);
+  REQUIRE(cartdg::n_extrema<3>(read) == 1);
+  read[1] = 0;
+  REQUIRE(cartdg::n_extrema<3>(read) == 0);
+  REQUIRE(cartdg::n_extrema<6>(read) == 0);
+  read[1] = 1;
+  read[2] = -1;
+  REQUIRE(cartdg::n_extrema<6>(read) == 2);
+  read[4] = -1;
+  REQUIRE(cartdg::n_extrema<6>(read) == 3);
+}
