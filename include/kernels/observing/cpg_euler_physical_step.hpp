8aa8fd290944848dd2cb2829a4dbead5c603c1d7
Micaiah Smith-Pierce <micaiah@smithpierce.net>
2021-03-12 04:22:15 -0500

    passed density tests (pressure fails)

--- include/kernels/observing/cpg_euler_physical_step.hpp
+++ include/kernels/observing/cpg_euler_physical_step.hpp
@@ -10,7 +10,20 @@ template<int n_var, int n_qpoint>
 double cpg_euler_physical_step(double* read, double* write, int n_elem,
                                Kernel_settings& settings)
 {
+  double max_diff = settings.max_difference;
   double step = 1.;
+  for (int i_elem = 0; i_elem < n_elem; ++i_elem)
+  {
+    for (int i_qpoint = 0; i_qpoint < n_qpoint; ++i_qpoint)
+    {
+      #define READ(i_var) read[i_elem*n_var*n_qpoint + (i_var)*n_qpoint + i_qpoint]
+      #define WRITE(i_var) write[i_elem*n_var*n_qpoint + (i_var)*n_qpoint + i_qpoint]
+      double mass_diff = 1. - WRITE(n_var - 2)/READ(n_var - 2);
+      if (mass_diff > 0) step = std::min(step, max_diff/mass_diff);
+      #undef READ
+      #undef WRITE
+    }
+  }
   return step;
 }
 
