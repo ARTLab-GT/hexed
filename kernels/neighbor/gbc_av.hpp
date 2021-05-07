772d01bc4a845c4adb65b173b2a202a8a2d17c6c
Micaiah Smith-Pierce <micaiah@smithpierce.net>
2021-05-07 03:11:01 -0400

    fixed bug in gbc_av

--- kernels/neighbor/gbc_av.hpp
+++ kernels/neighbor/gbc_av.hpp
@@ -16,6 +16,7 @@ void gbc_av(std::vector<Ghost_boundary_condition*>& ghost_bound_conds,
             int i_var, int i_axis, double weight,
             Kernel_settings& settings)
 {
+  double mult = 1./(weight*settings.d_pos);
   const int n_dof = n_var*n_qpoint;
   for (Ghost_boundary_condition* gbc : ghost_bound_conds)
   {
@@ -30,7 +31,7 @@ void gbc_av(std::vector<Ghost_boundary_condition*>& ghost_bound_conds,
         read_copy<1, n_qpoint, row_size>(read + i_elem*n_qpoint, flux, stride, gbc->is_positive_face);
         for (int i_qpoint = 0; i_qpoint < n_qpoint; ++i_qpoint)
         {
-          flux[i_qpoint] /= weight;
+          flux[i_qpoint] *= mult;
           if (gbc->is_positive_face) flux[i_qpoint] *= -1;
         }
         write_copy<1, n_qpoint, row_size>(flux, write + i_elem*n_dof + i_var*n_qpoint, stride, gbc->is_positive_face);
