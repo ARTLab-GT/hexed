8f0d7bf84728b943c13dd6aa747dc07a981fcceb
Micaiah Smith-Pierce <micaiah@smithpierce.net>
2021-05-13 03:13:55 -0400

    minor tweaks

--- script/benchmark.py
+++ script/benchmark.py
@@ -1,7 +1,14 @@
 import matplotlib.pyplot as plt
 import subprocess
 
-output = subprocess.run("benchmark/benchmark", stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+dim = 3
+row_size = 5
+n_side = 3e1
+
+cmd = [str(arg) for arg in ["benchmark/benchmark", dim, row_size, n_side]]
+output = subprocess.run(cmd, capture_output=True)
+if len(output.stderr) > 0:
+    err = str(output.stderr, "ascii")
+    raise Exception("Benchmark executable crashed with the followin error message:\n\n" + err)
 output = str(output.stdout, "ascii")
-if ("terminate" in output.lower()) or ("segmentation fault" in output.lower()):
-    raise Exception("Benchmark executable crashed with the followin error message:\n\n" + output)
+print(output)
