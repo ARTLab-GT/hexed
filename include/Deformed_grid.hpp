e4920917ae07edf0ca468059c8004bd0962379ea
Micaiah Smith-Pierce <micaiah@smithpierce.net>
2021-07-03 12:58:18 -0400

    edited comment

--- include/Deformed_grid.hpp
+++ include/Deformed_grid.hpp
@@ -35,7 +35,7 @@ class Deformed_grid : public Grid
   virtual void execute_neighbor(Kernel_settings&);
   virtual void execute_req_visc(Kernel_settings&);
   virtual void execute_cont_visc(Kernel_settings&);
-  virtual void execute_local_derivative(int i_var, int i_axis, Kernel_settings&); // FIXME: do something
+  virtual void execute_local_derivative(int i_var, int i_axis, Kernel_settings&); // FIXME: for now, does nothing
   virtual void execute_neighbor_derivative(int i_var, int i_axis, Kernel_settings&);
   virtual void execute_av_flux(Kernel_settings&);
   virtual void execute_local_av(int i_var, int i_axis, Kernel_settings&);
