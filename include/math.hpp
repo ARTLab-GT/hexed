7753b62e46b0646c7ae0806b6d4692564d6ba511
Micaiah Smith-Pierce <micaiah@smithpierce.net>
2021-07-04 13:22:46 -0400

    root accepts function-like type as template arg

--- include/math.hpp
+++ include/math.hpp
@@ -4,7 +4,24 @@
 namespace cartdg
 {
 
-double root(double (*func)(double arg), double init_guess, double atol=1e-10, double init_diff=1e-3);
+template <typename Func_type>
+double root(Func_type func, double init_guess, double atol=1e-10, double init_diff=1e-3)
+{
+  double guess_prev = init_guess - init_diff;
+  double func_prev = func(guess_prev);
+  double guess = init_guess;
+  do
+  {
+    double func_curr = func(guess);
+    double slope = (func_curr - func_prev)/(guess - guess_prev);
+    guess_prev = guess;
+    func_prev = func_curr;
+    guess -= func_curr/slope;
+  }
+  while (std::abs(guess - guess_prev) > atol);
+  return guess;
+}
+
 
 }
 
