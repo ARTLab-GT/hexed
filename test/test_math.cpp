ecd44a6e65163f8957d301950696aa609a514f4a
Micaiah Smith-Pierce <micaiah@smithpierce.net>
2021-07-04 12:16:00 -0400

    used lambda expr in test

--- test/test_math.cpp
+++ test/test_math.cpp
@@ -10,15 +10,12 @@ double quad_func(double x)
 {
   return (x + .6)*(4.*x - 8.);
 }
-double exp_func(double x)
-{
-  return std::exp(x) - 2.;
-}
 
 TEST_CASE("root finder")
 {
   REQUIRE(cartdg::root(lin_func, -1e7) == Approx(0.5));
   REQUIRE(cartdg::root(quad_func, -0.8) == Approx(-.6));
   REQUIRE(cartdg::root(quad_func, 2.3) == Approx(2.));
-  REQUIRE(cartdg::root(exp_func, 0.) == Approx(std::log(2.)));
+  REQUIRE(cartdg::root([](double x){return std::exp(x) - 2.;}, 0.)
+          == Approx(std::log(2.)));
 }
