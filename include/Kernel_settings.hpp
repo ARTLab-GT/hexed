f744cf1ecbfc5632051ee3d0acb5e130597ad7a2
Micaiah Smith-Pierce <micaiah@smithpierce.net>
2021-09-01 05:33:58 -0400

    updated Kernel_settings

--- include/Kernel_settings.hpp
+++ include/Kernel_settings.hpp
@@ -4,14 +4,14 @@
 namespace cartdg
 {
 
-class Kernel_settings
+struct Kernel_settings
 {
-  public:
   double d_t_by_d_pos = 0.;
   double d_pos = 1.;
   double cpg_heat_rat = 1.4;
   double max_difference = 0.5;
-  bool always_smear = false;
+  int i_read = 0;
+  int i_write = 1;
 };
 
 }
