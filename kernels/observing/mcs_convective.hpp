a7cdcdf3a2c7ed31b5d034dce227911f7cf5c994
Micaiah Smith-Pierce <micaiah@smithpierce.net>
2021-09-07 00:42:49 -0400

    attempted implementation of mcs_conv.

---
+++ kernels/observing/mcs_convective.hpp
@@ -0,0 +1,28 @@
+#ifndef CARTDG_MCS_CONVECTIVE_HPP_
+#define CARTDG_MCS_CONVECTIVE_HPP_
+
+#include <Kernel_settings.hpp>
+#include <Element.hpp>
+#include "char_speed_cpg_euler.hpp"
+
+namespace cartdg
+{
+
+// AUTOGENERATE LOOKUP BENCHMARK(regular, 1)
+template<int n_var, int n_qpoint, int row_size>
+double mcs_convective(elem_vec& elements, Kernel_settings& settings)
+{
+  const int n_dof = n_var*n_qpoint;
+  double heat_rat = settings.cpg_heat_rat;
+  const int i_read = settings.i_read;
+  double max_speed = 0.;
+  #pragma omp parallel for reduction(max:max_speed)
+  for (int i_elem = 0; i_elem < elements.size(); ++i_elem)
+  {
+    max_speed = std::max<double>(char_speed_cpg_euler<n_var, n_qpoint>(elements[i_elem]->stage(i_read), heat_rat), max_speed);
+  }
+  return max_speed;
+}
+
+}
+#endif
