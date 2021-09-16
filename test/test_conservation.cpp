68dbf775e59937709763af7cc0702a9c74fff1cd
Micaiah Smith-Pierce <micaiah@gatech.edu>
2021-09-16 05:13:11 -0400

    revamped 2D def cons test

--- test/test_conservation.cpp
+++ test/test_conservation.cpp
@@ -125,6 +125,12 @@ TEST_CASE("Conservation of state variables")
     cartdg::Regular_grid& grid = sol.reg_grids[0];
     sol.add_deformed_grid(1);
     cartdg::Deformed_grid& def_grid = sol.def_grids[0];
+
+    // I know this setup is complicated... sorry :(
+    for (int i = 0; i < 5; ++i) grid.add_element({2, i - 2});
+    for (int i = 0; i < 4; ++i) grid.add_element({1 - i, 2});
+    grid.auto_connect({5, 5});
+
     for (int i : {-1, 0})
     {
       for (int j : {-1, 0})
@@ -132,49 +138,80 @@ TEST_CASE("Conservation of state variables")
         def_grid.add_element({i, j});
       }
     }
-    grid.add_element({1, -1});
-    grid.add_element({1, 0});
-    grid.add_element({1, 1});
-    grid.add_element({0, 1});
-    grid.add_element({-1, 1});
-    grid.auto_connect({3, 3});
+    for (int i = 0; i < 3; ++i)
+    {
+      def_grid.add_element({1, i - 1});
+      def_grid.connect_non_def({i + 4, i}, {0, 0}, {1, 0}, grid);
+    }
+    def_grid.connect({5, 4}, {1, 1}, {0, 1});
+    def_grid.connect({6, 5}, {1, 1}, {0, 1});
+    for (int i = 0; i < 3; ++i)
+    {
+      def_grid.add_element({-i, 1});
+      def_grid.connect({i + 7, i + 6}, {0, 0}, {1, 0});
+      def_grid.connect_non_def({i + 7, i + 4}, {1, 1}, {1, 0}, grid);
+    }
+    for (int i = 0; i < 3; ++i)
+    {
+      def_grid.add_element({-2, -i});
+      def_grid.connect({i + 10, i + 9}, {1, 1}, {1, 0});
+      def_grid.connect_non_def({i + 7, 2 - i}, {0, 0}, {0, 1}, grid);
+    }
+    for (int i = 0; i < 3; ++i)
+    {
+      def_grid.add_element({i - 1, -2});
+      def_grid.connect({i + 13, i + 12}, {0, 0}, {0, 1});
+      def_grid.connect_non_def({i + 10, 8 - i}, {1, 1}, {0, 1}, grid);
+    }
+    def_grid.connect({4, 15}, {1, 1}, {0, 1});
 
     double center [] {0.1, 0.1};
-
-    def_grid.get_vertex(0).pos = {-0.5, -0.5, 0.};
-    def_grid.get_vertex(1).pos = {-0.5, 0., 0.};
-    def_grid.get_vertex(2).pos = {0., -0.5, 0.};
-    def_grid.get_vertex(3).pos = {center[0], center[1], 0.};
-
-    def_grid.get_vertex(4).pos = {-0.5, 0.5, 0.};
-    def_grid.get_vertex(5).pos = {0., 0.5, 0.};
-    def_grid.get_vertex(6).pos = {-0.5, 0., 0.};
-    def_grid.get_vertex(7).pos = {center[0], center[1], 0.};
-
-    def_grid.get_vertex( 9).pos = {center[0], center[1], 0.};
-    def_grid.get_vertex(12).pos = {center[0], center[1], 0.};
+    {
+      cartdg::Deformed_element& elem {def_grid.deformed_element(0)};
+      elem.vertex(3).pos = {center[0], center[1], 0.};
+    }
+    {
+      cartdg::Deformed_element& elem {def_grid.deformed_element(1)};
+      elem.vertex(0).pos = {-0.5, 0.5, 0.};
+      elem.vertex(1).pos = {0., 0.5, 0.};
+      elem.vertex(2).pos = {-0.5, 0., 0.};
+      elem.vertex(3).pos = {center[0], center[1], 0.};
+    }
+    {
+      cartdg::Deformed_element& elem {def_grid.deformed_element(2)};
+      elem.vertex(1).pos = {center[0], center[1], 0.};
+    }
+    {
+      cartdg::Deformed_element& elem {def_grid.deformed_element(3)};
+      elem.vertex(0).pos = {center[0], center[1], 0.};
+    }
+    {
+      cartdg::Deformed_element& elem {def_grid.deformed_element(6)};
+      elem.vertex(0).pos[0] += 0.1;
+      elem.vertex(0).pos[1] += 0.1;
+    }
 
     def_grid.connect({0, 1}, {1, 0}, {1, 1});
     def_grid.connect({2, 3}, {1, 1}, {1, 0});
     def_grid.connect({0, 2}, {0, 0}, {1, 0});
     def_grid.connect({1, 3}, {1, 0}, {1, 0});
+
+    def_grid.connect({2, 4}, {0, 0}, {1, 0});
+    def_grid.connect({3, 5}, {0, 0}, {1, 0});
+    def_grid.connect({3, 7}, {1, 1}, {1, 0});
+    def_grid.connect({1, 8}, {0, 1}, {0, 0});
+    def_grid.connect({0,11}, {0, 0}, {0, 1});
+    def_grid.connect({1,10}, {1, 0}, {0, 1});
+    def_grid.connect({2,14}, {1, 1}, {0, 1});
+    def_grid.connect({0,13}, {1, 1}, {0, 1});
+
     def_grid.calc_jacobian();
-    def_grid.update_connections();
-    def_grid.connect_non_def({2, 0}, {0, 0}, {1, 0}, grid);
-    def_grid.connect_non_def({3, 1}, {0, 0}, {1, 0}, grid);
-    def_grid.connect_non_def({3, 3}, {1, 1}, {1, 0}, grid);
-    def_grid.connect_non_def({1, 4}, {0, 1}, {0, 0}, grid);
-    def_grid.connect_non_def({0, 0}, {0, 0}, {0, 1}, grid);
-    def_grid.connect_non_def({1, 1}, {1, 0}, {0, 1}, grid);
-    def_grid.connect_non_def({0, 4}, {1, 1}, {0, 1}, grid);
-    def_grid.connect_non_def({2, 3}, {1, 1}, {0, 1}, grid);
 
     Initializer init (2);
     sol.initialize(init);
-    double * state = def_grid.state_r();
     for (int i_state = 0; i_state < grid.n_qpoint; ++i_state)
     {
-      state[i_state] = i_state + 1;
+      for (int i_elem : {0, 6 , 12}) def_grid.element(i_elem).stage(0)[i_state] = i_state + 1;
     }
 
     sol.visualize("conservation_2d_def_before");
