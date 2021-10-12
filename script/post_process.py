d458775e629836ff43f723621779c1b7e6602a45
Micaiah Smith-Pierce <micaiah@gatech.edu>
2021-10-12 19:37:56 -0400

    script turns mesh off for interior

--- script/post_process.py
+++ script/post_process.py
@@ -6,13 +6,12 @@ frame = tecplot.active_frame()
 files = os.listdir()
 files = [f for f in files if ".szplt" in f]
 files = [f for f in files if not ("qpoint" in f)]
-files = [f for f in files if not ("interior" in f)]
 data = tecplot.data.load_tecplot_szl(files)
 plot = frame.plot()
 for fieldmap in plot.active_fieldmaps:
     for zone in fieldmap.zones:
-        name = zone.name
-    if not ("edges" in zone.name):
-        plot.fieldmap(zone).mesh.show = False
-#plot.fieldmap(0).mesh.show = False
+        print(zone.name)
+        if not ("edges" in zone.name):
+            plot.fieldmap(zone).mesh.show = False
+    print()
 tecplot.save_layout("foo.lay")
