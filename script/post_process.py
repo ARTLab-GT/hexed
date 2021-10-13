fa6a51318a53ad266b5703bd87ed384578a1cc38
Micaiah Smith-Pierce <micaiah@gatech.edu>
2021-10-13 13:56:56 -0400

    compute primitive flow variables

--- script/post_process.py
+++ script/post_process.py
@@ -1,12 +1,24 @@
 import tecplot
 import os
 
+# basic plot setup
 tecplot.new_layout()
 frame = tecplot.active_frame()
 files = os.listdir()
 files = [f for f in files if ".szplt" in f]
 data = tecplot.data.load_tecplot_szl(files)
-plot = frame.plot()
+variable_names = frame.dataset.variable_names
+n_dim = len([var for var in variable_names if "pos" in var])
+if n_dim == 3:
+    plot_type = tecplot.constant.PlotType.Cartesian3D
+elif n_dim == 2:
+    plot_type = tecplot.constant.PlotType.Cartesian2D
+else:
+    plot_type = tecplot.constant.PlotType.XYLine
+plot = frame.plot(plot_type)
+plot.activate()
+
+# configure Zone Style
 for fieldmap in plot.active_fieldmaps:
     for zone in fieldmap.zones:
         fm = plot.fieldmap(zone)
@@ -20,4 +32,22 @@ for fieldmap in plot.active_fieldmaps:
             fm.mesh.show = False
         if not ("interior" in zone.name):
             fm.contour.show = False
-tecplot.save_layout("foo.lay")
+            fm.edge.show = False
+
+# compute new variables
+heat_rat = 1.4
+gas_const = 287.058
+equations = ""
+for i_dim in range(n_dim):
+    equations += f"{{velocity{i_dim}}} = {{state{i_dim}}}/{{state{n_dim}}}\n"
+equations += "{velocity_magnitude} = ("
+for i_dim in range(n_dim):
+    equations += f"{{velocity{i_dim}}}*{{velocity{i_dim}}} + "
+equations = equations[:-3] + ")**0.5\n"
+equations += f"""
+{{density}} = {{state{n_dim}}}
+{{pressure}} = ({{state{n_dim + 1}}} - 0.5*{{density}}*{{velocity_magnitude}}**2)*{heat_rat - 1}
+"""[1:]
+tecplot.data.operate.execute_equation(equations, ignore_divide_by_zero=True)
+
+tecplot.save_layout("all.lay")
