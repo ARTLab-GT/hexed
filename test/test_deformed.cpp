b8d393a0581817b15401fbe23153fb3cad4eb295
Micaiah Smith-Pierce <micaiah@gatech.edu>
2021-10-23 13:33:05 -0400

    abstracted test

--- test/test_deformed.cpp
+++ test/test_deformed.cpp
@@ -17,15 +17,17 @@ class Copy_bc : public cartdg::Ghost_boundary_condition
   }
 };
 
-TEST_CASE("Deformed elements")
+double deform (double node)
 {
-  const int row_size {CARTDG_MAX_BASIS_ROW_SIZE};
-  const int n_qpoint {row_size*row_size};
-  cartdg::Gauss_legendre basis {row_size};
-  cartdg::Deformed_grid grid {4, 2, 0, 0.2, basis};
+  return node*(1. - node)*4.;
+}
+
+void test(cartdg::Deformed_grid& grid)
+{
+  int row_size {grid.basis.row_size};
+  int n_qpoint {row_size*row_size};
   double veloc [2] {1.2, -1.3};
   double pres {101000};
-
   for (int i = 0, i_elem = 0; i < 3; ++i)
   {
     for (int j = 0; j < 3; ++j)
@@ -67,25 +69,33 @@ TEST_CASE("Deformed elements")
   double dt = 0.07;
   settings.d_t_by_d_pos = 0.07/0.2;
 
+  grid.execute_write_face(settings);
+  grid.execute_neighbor(settings);
+  grid.execute_local(settings);
+  for (int i_elem = 0; i_elem < 9; ++i_elem)
+  {
+    double* r {grid.element(i_elem).stage(settings.i_read)};
+    double* w {grid.element(i_elem).stage(settings.i_write)};
+    for (int i_dof = 0; i_dof < 4*n_qpoint; ++i_dof)
+    {
+      r[i_dof] = (w[i_dof] - r[i_dof])/dt;
+    }
+    for (int i_qpoint = 0; i_qpoint < n_qpoint; ++i_qpoint)
+    {
+      REQUIRE(r[2*n_qpoint + i_qpoint] == Approx(-0.03*veloc[0] - 0.02*veloc[1]));
+    }
+  }
+  cartdg::Tecplot_file file {"deformed_test", 2, 4, 0.};
+  grid.visualize_interior(file);
+}
+
+TEST_CASE("Deformed elements")
+{
+  const int row_size {CARTDG_MAX_BASIS_ROW_SIZE};
+  cartdg::Gauss_legendre basis {row_size};
   SECTION("plain")
   {
-    grid.execute_write_face(settings);
-    grid.execute_neighbor(settings);
-    grid.execute_local(settings);
-    for (int i_elem = 0; i_elem < 9; ++i_elem)
-    {
-      double* r {grid.element(i_elem).stage(settings.i_read)};
-      double* w {grid.element(i_elem).stage(settings.i_write)};
-      for (int i_dof = 0; i_dof < 4*n_qpoint; ++i_dof)
-      {
-        r[i_dof] = (w[i_dof] - r[i_dof])/dt;
-      }
-      for (int i_qpoint = 0; i_qpoint < n_qpoint; ++i_qpoint)
-      {
-        REQUIRE(r[2*n_qpoint + i_qpoint] == Approx(-0.03*veloc[0] - 0.02*veloc[1]));
-      }
-    }
-    cartdg::Tecplot_file file {"deformed_test", 2, 4, 0.};
-    grid.visualize_interior(file);
+    cartdg::Deformed_grid grid {4, 2, 0, 0.2, basis};
+    test(grid);
   }
 }
