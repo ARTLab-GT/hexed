931fc6f63f28aae4330523871e718882928cb14a
Micaiah Smith-Pierce <micaiah@gatech.edu>
2021-10-24 15:06:54 -0400

    Test class

--- test/test_deformed.cpp
+++ test/test_deformed.cpp
@@ -47,117 +47,136 @@ class Quadratic_def : public cartdg::Deformed_grid
   }
 };
 
-class Grid_adjustment
+class Test
 {
+  protected:
+  cartdg::Deformed_grid& grid;
+  double veloc [2] {1.2, -1.3};
+  double pres {101000};
   public:
-  virtual void displace_vertices(cartdg::Deformed_grid& grid)
-  {}
+  Test(cartdg::Deformed_grid& g) : grid{g} {}
+
+  void test()
+  {
+    int row_size {grid.basis.row_size};
+    int n_qpoint {row_size*row_size};
+    for (int i = 0, i_elem = 0; i < 3; ++i)
+    {
+      for (int j = 0; j < 3; ++j)
+      {
+        grid.add_element({i, j});
+        if (i > 0) grid.connect({i_elem - 3, i_elem}, {0, 0}, {1, 0});
+        if (j > 0) grid.connect({i_elem - 1, i_elem}, {1, 1}, {1, 0});
+        ++i_elem;
+      }
+    }
+    displace_vertices();
+    for (int i = 0, i_elem = 0; i < 3; ++i)
+    {
+      for (int j = 0; j < 3; ++j)
+      {
+        auto pos = grid.get_pos(i_elem);
+        double* stage {grid.element(i_elem).stage(0)};
+        for (int i_qpoint = 0; i_qpoint < grid.n_qpoint; ++i_qpoint)
+        {
+          double mass = 1. + 0.03*pos[i_qpoint] + 0.02*pos[i_qpoint + n_qpoint];
+          stage[0*n_qpoint + i_qpoint] = mass*veloc[0];
+          stage[1*n_qpoint + i_qpoint] = mass*veloc[1];
+          stage[2*n_qpoint + i_qpoint] = mass;
+          stage[3*n_qpoint + i_qpoint] = pres/0.4 + 0.5*mass*(veloc[0]*veloc[0] + veloc[1]*veloc[1]);
+        }
+        ++i_elem;
+      }
+    }
+
+    std::vector<Copy_bc> bcs;
+    for (int i_dim : {0, 1})
+    {
+      for (bool is_positive : {0, 1})
+      {
+        bcs.emplace_back(grid, i_dim, is_positive);
+        for (int i_row = 0; i_row < 3; ++i_row)
+        {
+          int stride {i_dim ? 1 : 3};
+          bcs.back().add_element(i_row*3/stride + is_positive*2*stride);
+        }
+      }
+    }
+    for (Copy_bc& bc : bcs) grid.ghost_bound_conds.push_back(&bc);
+    grid.purge_vertices();
+    grid.calc_jacobian();
+    cartdg::Kernel_settings settings;
+    double dt = 0.07;
+    settings.d_t_by_d_pos = 0.07/0.2;
+
+    grid.execute_write_face(settings);
+    test_momentum_rotation();
+    grid.execute_neighbor(settings);
+    grid.execute_local(settings);
+    for (int i_elem = 0; i_elem < 9; ++i_elem)
+    {
+      double* r {grid.element(i_elem).stage(settings.i_read)};
+      double* w {grid.element(i_elem).stage(settings.i_write)};
+      for (int i_dof = 0; i_dof < 4*n_qpoint; ++i_dof)
+      {
+        r[i_dof] = (w[i_dof] - r[i_dof])/dt;
+      }
+      for (int i_qpoint = 0; i_qpoint < n_qpoint; ++i_qpoint)
+      {
+        if (cartdg::global_debug_message["require"]) REQUIRE(r[2*n_qpoint + i_qpoint] == Approx(-0.03*veloc[0] - 0.02*veloc[1]));
+      }
+    }
+    if (!cartdg::global_debug_message["require"])
+    {
+      cartdg::Tecplot_file file {"deformed_test", 2, 4, 0.};
+      grid.visualize_interior(file);
+    }
+  }
+  virtual void displace_vertices() {}
+  virtual void test_momentum_rotation() {}
 };
 
-class Def_edge : public Grid_adjustment
+class Test_def_edge : public Test
 {
   public:
-  virtual void displace_vertices(cartdg::Deformed_grid& grid)
+  Test_def_edge(cartdg::Deformed_grid& g) : Test{g} {}
+  virtual void displace_vertices() override
   {
     grid.deformed_element(4).vertex(3).pos[0] += 0.05;
     grid.deformed_element(4).vertex(3).pos[1] -= 0.04;
   }
+  virtual void test_momentum_rotation()
+  {
+    const int row_size {grid.basis.row_size};
+    for (int i_qpoint = 0; i_qpoint < row_size; ++i_qpoint)
+    {
+      double* face {grid.deformed_element(4).face()};
+    }
+  }
 };
-
-void test(cartdg::Deformed_grid& grid, Grid_adjustment& adjust)
-{
-  int row_size {grid.basis.row_size};
-  int n_qpoint {row_size*row_size};
-  double veloc [2] {1.2, -1.3};
-  double pres {101000};
-  for (int i = 0, i_elem = 0; i < 3; ++i)
-  {
-    for (int j = 0; j < 3; ++j)
-    {
-      grid.add_element({i, j});
-      if (i > 0) grid.connect({i_elem - 3, i_elem}, {0, 0}, {1, 0});
-      if (j > 0) grid.connect({i_elem - 1, i_elem}, {1, 1}, {1, 0});
-      ++i_elem;
-    }
-  }
-  adjust.displace_vertices(grid);
-  for (int i = 0, i_elem = 0; i < 3; ++i)
-  {
-    for (int j = 0; j < 3; ++j)
-    {
-      auto pos = grid.get_pos(i_elem);
-      double* stage {grid.element(i_elem).stage(0)};
-      for (int i_qpoint = 0; i_qpoint < grid.n_qpoint; ++i_qpoint)
-      {
-        double mass = 1. + 0.03*pos[i_qpoint] + 0.02*pos[i_qpoint + n_qpoint];
-        stage[0*n_qpoint + i_qpoint] = mass*veloc[0];
-        stage[1*n_qpoint + i_qpoint] = mass*veloc[1];
-        stage[2*n_qpoint + i_qpoint] = mass;
-        stage[3*n_qpoint + i_qpoint] = pres/0.4 + 0.5*mass*(veloc[0]*veloc[0] + veloc[1]*veloc[1]);
-      }
-      ++i_elem;
-    }
-  }
-
-  std::vector<Copy_bc> bcs;
-  for (int i_dim : {0, 1})
-  {
-    for (bool is_positive : {0, 1})
-    {
-      bcs.emplace_back(grid, i_dim, is_positive);
-      for (int i_row = 0; i_row < 3; ++i_row)
-      {
-        int stride {i_dim ? 1 : 3};
-        bcs.back().add_element(i_row*3/stride + is_positive*2*stride);
-      }
-    }
-  }
-  for (Copy_bc& bc : bcs) grid.ghost_bound_conds.push_back(&bc);
-  grid.purge_vertices();
-  grid.calc_jacobian();
-  cartdg::Kernel_settings settings;
-  double dt = 0.07;
-  settings.d_t_by_d_pos = 0.07/0.2;
-
-  grid.execute_write_face(settings);
-  grid.execute_neighbor(settings);
-  grid.execute_local(settings);
-  for (int i_elem = 0; i_elem < 9; ++i_elem)
-  {
-    double* r {grid.element(i_elem).stage(settings.i_read)};
-    double* w {grid.element(i_elem).stage(settings.i_write)};
-    for (int i_dof = 0; i_dof < 4*n_qpoint; ++i_dof)
-    {
-      r[i_dof] = (w[i_dof] - r[i_dof])/dt;
-    }
-    for (int i_qpoint = 0; i_qpoint < n_qpoint; ++i_qpoint)
-    {
-      REQUIRE(r[2*n_qpoint + i_qpoint] == Approx(-0.03*veloc[0] - 0.02*veloc[1]));
-    }
-  }
-  cartdg::Tecplot_file file {"deformed_test", 2, 4, 0.};
-  grid.visualize_interior(file);
-}
 
 TEST_CASE("Deformed elements")
 {
   const int row_size {CARTDG_MAX_BASIS_ROW_SIZE};
   cartdg::Gauss_legendre basis {row_size};
-  Grid_adjustment default_adjust;
+  cartdg::global_debug_message["require"] = 1;
   SECTION("plain")
   {
     cartdg::Deformed_grid grid {4, 2, 0, 0.2, basis};
-    test(grid, default_adjust);
+    Test test {grid};
+    test.test();
   }
   SECTION("deformed interior")
   {
     Quadratic_def grid {4, 2, 0, 0.2, basis};
-    test(grid, default_adjust);
+    Test test {grid};
+    test.test();
   }
   SECTION("deformed edges")
   {
     cartdg::Deformed_grid grid {4, 2, 0, 0.2, basis};
-    Def_edge adjust;
-    test(grid, adjust);
+    cartdg::global_debug_message["require"] = 0;
+    Test_def_edge test {grid};
+    test.test();
   }
 }
