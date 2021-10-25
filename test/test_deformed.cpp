ff1dee1f3e60ce753fc1dba95d107daf18751d42
Micaiah Smith-Pierce <micaiah@gatech.edu>
2021-10-24 23:04:17 -0400

    oops... this one actually fails

--- test/test_deformed.cpp
+++ test/test_deformed.cpp
@@ -123,12 +123,9 @@ class Test
       }
       for (int i_qpoint = 0; i_qpoint < n_qpoint; ++i_qpoint)
       {
-        if (cartdg::global_debug_message["require"])
-        {
         REQUIRE(r[0*n_qpoint + i_qpoint] == Approx(-0.03*veloc[0]*veloc[0] - 0.02*veloc[1]*veloc[0]).scale(pres));
         REQUIRE(r[1*n_qpoint + i_qpoint] == Approx(-0.03*veloc[0]*veloc[1] - 0.02*veloc[1]*veloc[1]).scale(pres));
         REQUIRE(r[2*n_qpoint + i_qpoint] == Approx(-0.03*veloc[0] - 0.02*veloc[1]));
-        }
       }
     }
     cartdg::Tecplot_file file {"deformed_test", 2, 4, 0.};
