d9c3249a00c8f05971fcb8b4cb20a8497dcba6af
Micaiah Smith-Pierce <micaiah@gatech.edu>
2021-10-25 10:40:51 -0400

    passed conservation

--- test/test_Solution.cpp
+++ test/test_Solution.cpp
@@ -132,10 +132,8 @@ TEST_CASE("Integration of deformed elements")
   {
     def_grid.add_element({1, i - 1});
     grid.add_connection(&def_grid.element(i + 4), &grid.element(i + 1), 0);
-    //def_grid.connect_non_def({i + 4, i + 1}, {0, 0}, {1, 0}, grid);
   }
   grid.add_connection(&def_grid.element(6), &grid.element(5), 1);
-  //def_grid.connect_non_def({6, 5}, {1, 1}, {1, 0}, grid);
   def_grid.connect({5, 4}, {1, 1}, {0, 1});
   def_grid.connect({6, 5}, {1, 1}, {0, 1});
   for (int i = 0; i < 3; ++i)
@@ -143,28 +141,22 @@ TEST_CASE("Integration of deformed elements")
     def_grid.add_element({-i, 1});
     def_grid.connect({i + 7, i + 6}, {0, 0}, {1, 0});
     grid.add_connection(&def_grid.element(i + 7), &grid.element(i + 6), 1);
-    //def_grid.connect_non_def({i + 7, i + 6}, {1, 1}, {1, 0}, grid);
   }
   grid.add_connection(&grid.element(3), &def_grid.element(9), 0);
-  //def_grid.connect_non_def({9, 3}, {0, 0}, {0, 1}, grid);
   for (int i = 0; i < 3; ++i)
   {
     def_grid.add_element({-2, -i});
     def_grid.connect({i + 10, i + 9}, {1, 1}, {1, 0});
     grid.add_connection(&grid.element(2 - i), &def_grid.element(i + 10), 0);
-    //def_grid.connect_non_def({i + 10, 2 - i}, {0, 0}, {0, 1}, grid);
   }
   grid.add_connection(&grid.element(8), &def_grid.element(12), 1);
-  //def_grid.connect_non_def({12, 8}, {1, 1}, {0, 1}, grid);
   for (int i = 0; i < 3; ++i)
   {
     def_grid.add_element({i - 1, -2});
     def_grid.connect({i + 13, i + 12}, {0, 0}, {0, 1});
     grid.add_connection(&grid.element(7 - i), &def_grid.element(i + 13), 1);
-    //def_grid.connect_non_def({i + 13, 7 - i}, {1, 1}, {0, 1}, grid);
   }
   grid.add_connection(&def_grid.element(15), &grid.element(0), 0);
-  //def_grid.connect_non_def({15, 0}, {0, 0}, {1, 0}, grid);
   def_grid.connect({4, 15}, {1, 1}, {0, 1});
 
   double center [] {0.1, 0.1};
@@ -213,7 +205,6 @@ TEST_CASE("Integration of deformed elements")
   sol.initialize(init);
   auto file_name = "deformed_integration";
   sol.visualize(file_name);
-  //cartdg::global_debug_message["plot"] = 1;
   for (int i = 0; i < 10; ++i)
   {
     for (int j = 0; j < 10; ++j)
@@ -222,7 +213,6 @@ TEST_CASE("Integration of deformed elements")
     }
     sol.visualize(file_name);
   }
-  cartdg::global_debug_message.erase("plot");
 }
 
 TEST_CASE("Execution of non-penetration boundary condition")
