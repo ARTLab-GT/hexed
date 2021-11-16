5bd755e6f83b60da63ff36d5844c7e62386f22e6
Micaiah Smith-Pierce <micaiah@gatech.edu>
2021-11-16 15:24:30 -0500

    comments

--- include/Basis.hpp
+++ include/Basis.hpp
@@ -6,6 +6,11 @@
 namespace cartdg
 {
 
+/*
+ * Represents a basis of Lagrange (interpolating) polynomials. Interpolates between
+ * a set of quadrature points, a.k.a. nodes. The `i`th basis vector is the polynomial with a value
+ * of 1 at the `i`th node and 0 at all other nodes.
+ */
 class Basis
 {
   public:
@@ -13,10 +18,18 @@ class Basis
 
   Basis(int row_size_arg);
   virtual ~Basis();
+  // Return the `i`th interpolation node (i.e. quadrature point)
+  // Should be in interval [0, 1].
   virtual double node(int i) = 0;
+  // `node_weights(i)` is the quadrature weight associated with `node(i)`
+  // Integrates in domain [0, 1].
   virtual Eigen::VectorXd node_weights() = 0;
+  // Differentiation matrix. `diff_mat(i, j)` is the derivative of interpolating polynomial `j` evaluated at `node(i)`.
   virtual Eigen::MatrixXd diff_mat() = 0;
+  // `boundary()(i, j)` is the `j`th basis polynomial evaluated at `i`. `i` must be in {0, 1}.
   virtual Eigen::MatrixXd boundary() = 0;
+  // `orthogonal(degree)(i)` is the Legendre (orthogonal) polynomial evaluated at `node(i)`. Polynomial is
+  // scaled so that its norm is 1 with respect to the quadrature rule.
   virtual Eigen::VectorXd orthogonal(int degree) = 0;
   Eigen::MatrixXd interpolate(const Eigen::VectorXd& sample);
   virtual Eigen::MatrixXd prolong  (int i_half);
