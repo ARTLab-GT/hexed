4381a7ca64258ec2e2e036f31ace27fae096a8d9
Micaiah Smith-Pierce <micaiah@gatech.edu>
2021-11-16 21:19:45 -0500

    more comments

--- include/Basis.hpp
+++ include/Basis.hpp
@@ -31,8 +31,16 @@ class Basis
   // `orthogonal(degree)(i)` is the Legendre (orthogonal) polynomial evaluated at `node(i)`. Polynomial is
   // scaled so that its norm is 1 with respect to the quadrature rule.
   virtual Eigen::VectorXd orthogonal(int degree) = 0;
+  // Interpolate to arbitrary points (not for high performance)
+  // `interpolate(sample)(i, j)` is the `j`th basis polynomial evaluated at `sample(i)`
   Eigen::MatrixXd interpolate(const Eigen::VectorXd& sample);
+  // Prolong into space of one higher refinement level
+  // `prolong(i_half)(i, j)` is the `j`th basis polynomial evaluated at the `i`th node in the
+  // refined space. If `i_half` is 0, refined space is interval [0, 0.5]. if `i_half` is 1, then [0.5, 1]
   virtual Eigen::MatrixXd prolong  (int i_half);
+  // Restrict from refined space above to standard space. `restrict(i_half)(i, j)` is the
+  // value at the `i`th standard node of the orthogonal projection of the `j`th polynomial in the
+  // refined space into the standard space.
   virtual Eigen::MatrixXd restrict (int i_half);
 };
 
