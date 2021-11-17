fa6016a4617d2383b07923f33714f53f4432c5f8
Micaiah Smith-Pierce <micaiah@gatech.edu>
2021-11-17 16:05:35 -0500

    started prolong (still fails)

--- kernels/neighbor/prolong.hpp
+++ kernels/neighbor/prolong.hpp
@@ -1,6 +1,7 @@
 #ifndef CARTDG_PROLONG_HPP_
 #define CARTDG_PROLONG_HPP_
 
+#include <math.hpp>
 #include <Kernel_settings.hpp>
 #include <Basis.hpp>
 #include <Refined_face.hpp>
@@ -12,6 +13,13 @@ namespace cartdg
 template<int n_var, int n_qpoint, int row_size>
 void prolong(ref_face_vec& ref_faces, Basis& basis, Kernel_settings& settings)
 {
+  const int n_dim {n_var - 2};
+  const int n_face {custom_math::(2, n_dim - 1)};
+
+  double* coarse {ref_faces.coarse_face()};
+  for (int i_face = 0; i_face < n_face; ++i_face)
+  {
+  }
 }
 
 }
