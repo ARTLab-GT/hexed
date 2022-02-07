fd71284757e9d53316f4cb664c4bfae103129c90
Micaiah Smith-Pierce <micaiah@gatech.edu>
2022-02-07 17:09:41 -0500

    correct time step calculation

--- test/test_observing.cpp
+++ test/test_observing.cpp
@@ -70,7 +70,7 @@ TEST_CASE("Max characteristic speed")
         // set state to 0 velocity and speed of sound 100
         for (int i_dim = 0; i_dim < 3; ++i_dim) state[i_dim*n_qpoint + i_qpoint] = 0.;
         state[3*n_qpoint + i_qpoint] = 1.4;
-        // set a modeslty higher sound speed in element 1 just to mix it up
+        // set a modestly higher sound speed in element 1 just to mix it up
         state[4*n_qpoint + i_qpoint] = ((i_elem == 1) ? 1e6 : 1e4)/0.4;
         for (int i_jac = 0; i_jac < 9; ++i_jac) {
           jac[i_jac*n_qpoint + i_qpoint] = (i_jac/3 == i_jac%3) ? 1. : 0.; // set to identity mat
@@ -84,14 +84,15 @@ TEST_CASE("Max characteristic speed")
       // scale 2 entries of jacobian at one quadrature point.
       elems[1]->jacobian()[(i_dim*3 + i_dim)*n_qpoint + 2] = 0.5;
     }
-    // test that jacobian is accounted for
-    REQUIRE(cartdg::get_mcs_deformed_convective(3, 4)(elems, settings) == Approx(2e3));
+    elems[1]->time_step_scale()[2] = 0.95;
+    // test that jacobian & time step scale are accounted for
+    REQUIRE(cartdg::get_mcs_deformed_convective(3, 4)(elems, settings) == Approx(2e3*0.95));
     for (int i_dim = 0; i_dim < 2; ++i_dim) {
       // scale 2 entries of jacobian at one quadrature point.
       elems[2]->jacobian()[(i_dim*3 + i_dim)*n_qpoint + 2] = 0.01;
     }
     // test that degeneracy is properly detected
-    REQUIRE(cartdg::get_mcs_deformed_convective(3, 4)(elems, settings) == Approx(2e3));
+    REQUIRE(cartdg::get_mcs_deformed_convective(3, 4)(elems, settings) == Approx(2e3*0.95));
     REQUIRE(!elems[0]->degenerate);
     REQUIRE(!elems[1]->degenerate);
     REQUIRE( elems[2]->degenerate);
