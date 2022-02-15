7b53f4eaa034440de65682c2934e142a41ec3054
Micaiah Smith-Pierce <micaiah@gatech.edu>
2022-02-15 00:54:00 -0500

    abstracted complicated test grid setup

--- test/test_Solution.cpp
+++ test/test_Solution.cpp
@@ -2,6 +2,7 @@
 #include <catch2/catch.hpp>
 #include <Solution.hpp>
 #include <cartdgConfig.hpp>
+#include "testing_utils.hpp"
 
 class Test_func : public cartdg::Spacetime_func
 {
@@ -109,99 +110,13 @@ TEST_CASE("Solution class")
 
 TEST_CASE("Integration of deformed elements")
 {
-  const int row_size = cartdg::config::max_row_size;
-  cartdg::Solution sol (4, 2, row_size, 1.);
-  sol.add_empty_grid(1);
-  cartdg::Regular_grid& grid = sol.reg_grids[0];
-  sol.add_deformed_grid(1);
-  cartdg::Deformed_grid& def_grid = sol.def_grids[0];
-
-  // I know this setup is complicated... sorry :(
-  for (int i = 0; i < 5; ++i) grid.add_element({2, i - 2});
-  for (int i = 0; i < 4; ++i) grid.add_element({1 - i, 2});
-  grid.auto_connect({5, 5});
-
-  for (int i : {-1, 0})
-  {
-    for (int j : {-1, 0})
-    {
-      def_grid.add_element({i, j});
-    }
-  }
-  for (int i = 0; i < 3; ++i)
-  {
-    def_grid.add_element({1, i - 1});
-    grid.add_connection(&def_grid.element(i + 4), &grid.element(i + 1), 0);
-  }
-  grid.add_connection(&def_grid.element(6), &grid.element(5), 1);
-  def_grid.connect({5, 4}, {1, 1}, {0, 1});
-  def_grid.connect({6, 5}, {1, 1}, {0, 1});
-  for (int i = 0; i < 3; ++i)
-  {
-    def_grid.add_element({-i, 1});
-    def_grid.connect({i + 7, i + 6}, {0, 0}, {1, 0});
-    grid.add_connection(&def_grid.element(i + 7), &grid.element(i + 6), 1);
-  }
-  grid.add_connection(&grid.element(3), &def_grid.element(9), 0);
-  for (int i = 0; i < 3; ++i)
-  {
-    def_grid.add_element({-2, -i});
-    def_grid.connect({i + 10, i + 9}, {1, 1}, {1, 0});
-    grid.add_connection(&grid.element(2 - i), &def_grid.element(i + 10), 0);
-  }
-  grid.add_connection(&grid.element(8), &def_grid.element(12), 1);
-  for (int i = 0; i < 3; ++i)
-  {
-    def_grid.add_element({i - 1, -2});
-    def_grid.connect({i + 13, i + 12}, {0, 0}, {0, 1});
-    grid.add_connection(&grid.element(7 - i), &def_grid.element(i + 13), 1);
-  }
-  grid.add_connection(&def_grid.element(15), &grid.element(0), 0);
-  def_grid.connect({4, 15}, {1, 1}, {0, 1});
-
-  double center [] {0.1, 0.1};
-  {
-    cartdg::Deformed_element& elem {def_grid.deformed_element(0)};
-    elem.vertex(3).pos = {center[0], center[1], 0.};
-  }
-  {
-    cartdg::Deformed_element& elem {def_grid.deformed_element(1)};
-    elem.vertex(0).pos = {-0.5, 0.5, 0.};
-    elem.vertex(1).pos = {0., 0.5, 0.};
-    elem.vertex(2).pos = {-0.5, 0., 0.};
-    elem.vertex(3).pos = {center[0], center[1], 0.};
-  }
-  {
-    cartdg::Deformed_element& elem {def_grid.deformed_element(2)};
-    elem.vertex(1).pos = {center[0], center[1], 0.};
-  }
-  {
-    cartdg::Deformed_element& elem {def_grid.deformed_element(3)};
-    elem.vertex(0).pos = {center[0], center[1], 0.};
-  }
-  {
-    cartdg::Deformed_element& elem {def_grid.deformed_element(6)};
-    elem.vertex(0).pos[0] += 0.1;
-    elem.vertex(0).pos[1] += 0.1;
-  }
-
-  def_grid.connect({0, 1}, {1, 0}, {1, 1});
-  def_grid.connect({2, 3}, {1, 1}, {1, 0});
-  def_grid.connect({0, 2}, {0, 0}, {1, 0});
-  def_grid.connect({1, 3}, {1, 0}, {1, 0});
-  def_grid.connect({2, 4}, {0, 0}, {1, 0});
-  def_grid.connect({3, 5}, {0, 0}, {1, 0});
-  def_grid.connect({3, 7}, {1, 1}, {1, 0});
-  def_grid.connect({1, 8}, {0, 1}, {0, 0});
-  def_grid.connect({0,11}, {0, 0}, {0, 1});
-  def_grid.connect({1,10}, {1, 0}, {0, 1});
-  def_grid.connect({2,14}, {1, 1}, {0, 1});
-  def_grid.connect({0,13}, {1, 1}, {0, 1});
-  def_grid.calc_jacobian();
-
+  auto sol_ptr {deformed_test_setup_2d(false)};
+  cartdg::Solution& sol = *sol_ptr;
   cartdg::Isentropic_vortex init ({100., 0., 1.225, 2e5});
   init.argmax_radius = 0.1;
   init.max_nondim_veloc = 0.3;
+  init.center0 = 1.35;
+  init.center1 = 1.5;
   sol.initialize(init);
   int i {0};
   mkdir("deformed_integration", 0700);
@@ -209,10 +124,8 @@ TEST_CASE("Integration of deformed elements")
   char buffer [buf_size];
   snprintf(buffer, buf_size, "deformed_integration/iter_%i", i);
   sol.visualize_field(buffer);
-  for (; i < 10; ++i)
-  {
-    for (int j = 0; j < 10; ++j)
-    {
+  for (; i < 10; ++i) {
+    for (int j = 0; j < 10; ++j) {
       sol.update(0.5);
     }
     snprintf(buffer, buf_size, "deformed_integration/iter_%i", i);
@@ -230,10 +143,8 @@ TEST_CASE("Execution of non-penetration boundary condition")
   grid.deformed_element(0).vertex(1).pos = {-0.1, 0.5, 0.};
   grid.deformed_element(0).vertex(2).pos = {0.7, 0.05, 0.};
   grid.deformed_element(0).vertex(3).pos = {1.3, 1.3, 0.};
-  for (int i_dim : {0, 1})
-  {
-    for (bool is_positive : {false, true})
-    {
+  for (int i_dim : {0, 1}) {
+    for (bool is_positive : {false, true}) {
       grid.add_wall(0, i_dim, is_positive);
     }
   }
--- test/testing_utils.hpp
+++ test/testing_utils.hpp
@@ -1,8 +1,20 @@
 #include <catch2/catch.hpp>
+#include <Solution.hpp>
 
+// useful for a few very specific tests involving `cartdg::Vertex`s
 inline void assert_equal(std::array<double, 3> computed, std::array<double, 3> correct)
 {
   for (int i_dim = 0; i_dim < 3; ++i_dim) {
     CHECK(computed[i_dim] == Approx(correct[i_dim]).margin(1e-14));
   }
 }
+
+/*
+ * Sets up a grid for performing integrated tests with deformed
+ * and regular elements. It is designed to incur as many of the
+ * programming challenges associated with deformed grids as possible
+ * to verify proper treatment. See implementation for additional details.
+ * Returns a `std::uniqe_ptr` to avoid dealing with the issue of copy/move
+ * semantics for `cartdg::Solution`.
+ */
+std::unique_ptr<cartdg::Solution> deformed_test_setup_2d(bool perturb_face = true);
