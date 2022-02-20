3d9d8b8b11caae54c02ddadfc8586d47533521e2
Micaiah Smith-Pierce <micaiah@gatech.edu>
2022-02-20 01:55:52 -0500

    `Surface_func` inherits from `Output_data`

--- include/Surface_func.hpp
+++ include/Surface_func.hpp
@@ -2,13 +2,14 @@
 #define CARTDG_SURFACE_FUNC_HPP_
 
 #include <vector>
+#include "Output_data.hpp"
 
 namespace cartdg
 {
 
 class Domain_func;
 
-class Surface_func
+class Surface_func : virtual public Output_data
 {
   public:
   virtual ~Surface_func() = default;
@@ -23,6 +24,8 @@ class Force_per_area : public Surface_func
   double hr;
   public:
   Force_per_area(double heat_rat = 1.4);
+  virtual constexpr int n_var(int n_dim) {return 1;}
+  virtual inline std::string variable_name(int i_var) {return "force_per_area";}
   // Computes surface force per unit area (i.e. pressure times unit normal)
   virtual std::vector<double> operator()(std::vector<double> pos, double time,
                                          std::vector<double> state, std::vector<double> outward_normal);
