7ae79cd43d26336873d3af125bbe817a0bce007f
Micaiah Smith-Pierce <micaiah@gatech.edu>
2022-02-23 18:53:13 -0500

    initialize connectedness

--- include/Element_container.hpp
+++ include/Element_container.hpp
@@ -10,59 +10,55 @@
 namespace cartdg
 {
 
-class Element_vector
+class Element_container
 {
   public:
-  virtual int size() = 0;
-  virtual Element& operator[](int index) = 0;
-};
-
-class Element_container : public Element_vector
-{
+  class Mesh_element
+  {
+    public:
+    std::array<int, 6> connectedness;
+    virtual Element& get() = 0;
+  };
   virtual int emplace(int ref_level, std::vector<int> position) = 0;
-  virtual Element& at(int ref_level, int serial_n) = 0;
-  virtual std::array<int, 6> connectedness(int ref_level, int serial_n) = 0;
+  virtual Mesh_element& at(int ref_level, int serial_n) = 0;
 };
 
 template <typename element_t>
 class Complete_element_container : public Element_container
 {
-  Storage_params params;
-  double spacing;
-  int next_sn;
-  std::vector<std::unique_ptr<element_t>> vec;
-  struct Element_data {element_t& element; std::array<int, 6> connectedness;};
-  std::map<std::array<int, 2>, Element_data> map;
-
   public:
+  class Compl_mesh_elem : public Mesh_element
+  {
+    public:
+    element_t element;
+    Compl_mesh_elem(Storage_params par, std::vector<int> pos, double sz) : element{par, pos, sz} {connectedness.fill(0);}
+    virtual Element& get() {return element;}
+  };
+
   Complete_element_container(Storage_params storage_params, double root_spacing)
   : params{storage_params}, spacing{root_spacing}, next_sn{0}
   {}
-  virtual int size()
-  {
-    return 0;
-  }
-  virtual element_t& operator[](int index)
-  {
-    return *vec.back();
-  }
+
   virtual int emplace(int ref_level, std::vector<int> position)
   {
     double level_spacing = spacing/custom_math::pow(2, ref_level);
-    vec.emplace_back(new element_t {params, position, level_spacing});
+    vec.emplace_back(new Compl_mesh_elem {params, position, level_spacing});
     std::array<int, 2> key = {ref_level, next_sn++};
-    Element_data data {*vec.back(), {}};
-    map.insert(std::pair(key, data));
+    map.insert(std::pair<std::array<int, 2>, Compl_mesh_elem&>(key, *vec.back()));
     return key[1];
   }
-  virtual element_t& at(int ref_level, int serial_n)
+
+  virtual Compl_mesh_elem& at(int ref_level, int serial_n)
   {
-    return map.at({ref_level, serial_n}).element;
+    return map.at({ref_level, serial_n});
   }
-  virtual std::array<int, 6> connectedness(int ref_level, int serial_n)
-  {
-    return {0, 0, 0, 0, 0, 0};
-  }
+
+  private:
+  Storage_params params;
+  double spacing;
+  int next_sn;
+  std::vector<std::unique_ptr<Compl_mesh_elem>> vec;
+  std::map<std::array<int, 2>, Compl_mesh_elem&> map;
 };
 
 }
--- test/test_Element_container.cpp
+++ test/test_Element_container.cpp
@@ -11,14 +11,15 @@ TEST_CASE("Specific_container<Deformed_element>")
   int sn1 = ctn.emplace(2, {2, 1});
   int sn2 = ctn.emplace(3, {2, 1});
   int sn3 = ctn.emplace(2, {2, 1});
-  REQUIRE(&ctn.at(2, sn0) != &ctn.at(2, sn1));
-  REQUIRE(&ctn.at(2, sn3) != &ctn.at(2, sn1));
+  REQUIRE(&ctn.at(2, sn0).element != &ctn.at(2, sn1).element);
+  REQUIRE(&ctn.at(2, sn3).element != &ctn.at(2, sn1).element);
   REQUIRE_THROWS(ctn.at(2, std::abs(sn0) + std::abs(sn1) + 1));
   REQUIRE_THROWS(ctn.at(3, sn0));
-  REQUIRE(&ctn.at(3, sn2) != &ctn.at(2, sn1));
+  REQUIRE(&ctn.at(3, sn2).element != &ctn.at(2, sn1).element);
   // test that elements are constructed correctly
-  cartdg::Deformed_element& elem = ctn.at(2, sn0);
+  cartdg::Deformed_element& elem = ctn.at(2, sn0).element;
   REQUIRE(elem.vertex(0).pos[1] == Approx(0.3*3./4.));
   REQUIRE(elem.vertex(7).pos[1] == Approx(0.3*4./4.));
-  REQUIRE(ctn.at(3, sn2).vertex(0).pos[0] == Approx(0.3*2./8.));
+  for (int i = 0; i < 6; ++i) REQUIRE(ctn.at(2, sn0).connectedness[i] == 0);
+  REQUIRE(ctn.at(3, sn2).element.vertex(0).pos[0] == Approx(0.3*2./8.));
 }
