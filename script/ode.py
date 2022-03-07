fd657c20e3532257fb6cc7902f0313eabef8e008
Micaiah Smith-Pierce <micaiah@gatech.edu>
2022-03-07 11:07:52 -0500

    hacked vortex to work in 1d

--- script/ode.py
+++ script/ode.py
@@ -12,23 +12,31 @@ nodes = [(node + 1)/2 for node in nodes]
 weights = [weight/2 for weight in weights]
 basis = Basis(nodes, weights, calc_digits=calc_digits)
 
-n_elem = 2;
-diff_mat = np.zeros((n_elem*row_size, n_elem*row_size)) # time derivative matrix
+n_elem = 4;
+diff_mat = np.zeros((n_elem*row_size*2, n_elem*row_size*2)) # time derivative matrix
 pos = np.zeros(n_elem*row_size)
-global_weights = np.zeros(n_elem*row_size)
-for i_elem in range(n_elem):
-    for i_row in range(row_size):
-        pos[i_elem*row_size + i_row] = i_elem + basis.nodes[i_row]
-        global_weights[i_elem*row_size + i_row] = weights[i_row]
-        for i_col in range(row_size):
-            diff_mat[  i_elem           *row_size + i_row, i_elem*row_size + i_col] = -basis.derivative(i_row, i_col)
-            diff_mat[  i_elem           *row_size + i_row, i_elem*row_size + i_col] -= basis.interpolate(i_col, 0)*basis.interpolate(i_row, 0)/weights[i_row]
-            diff_mat[((i_elem+1)%n_elem)*row_size + i_row, i_elem*row_size + i_col]  = basis.interpolate(i_col, 1)*basis.interpolate(i_row, 0)/weights[i_row]
-ident = np.identity(n_elem*row_size)
+global_weights = np.zeros((2, 2*n_elem*row_size))
+flux_jacobian = np.array([[1, 2], [3, 4]])
+char_speeds = np.linalg.eig(flux_jacobian)[0]
+mcs = np.max(char_speeds)
+for i_var in range(2):
+    for j_var in range(2):
+        var_mat = diff_mat[j_var*n_elem*row_size:(j_var+1)*n_elem*row_size, j_var*n_elem*row_size:(j_var+1)*n_elem*row_size]
+        for i_elem in range(n_elem):
+            for i_row in range(row_size):
+                pos[i_elem*row_size + i_row] = i_elem + basis.nodes[i_row]
+                global_weights[i_var, i_var*n_elem*row_size + i_elem*row_size + i_row] = weights[i_row]
+                for i_col in range(row_size):
+                    var_mat[  i_elem           *row_size + i_row, i_elem*row_size + i_col] = -basis.derivative(i_row, i_col)*flux_jacobian[i_var, j_var]
+                    var_mat[  i_elem           *row_size + i_row, i_elem*row_size + i_col] -= basis.interpolate(i_col, 0)*basis.interpolate(i_row, 0)/weights[i_row]
+                    var_mat[((i_elem+1)%n_elem)*row_size + i_row, i_elem*row_size + i_col]  = basis.interpolate(i_col, 1)*basis.interpolate(i_row, 0)/weights[i_row]
+plt.imshow(diff_mat)
+plt.show()
+ident = np.identity(2*n_elem*row_size)
 assert np.linalg.norm(global_weights@diff_mat) < 1e-12
 
 def euler(dt):
-    return ident + dt*diff_mat
+    return ident + dt*diff_mat/mcs
 def rk(dt, step_weights):
     step_mat = ident
     for weight in step_weights:
