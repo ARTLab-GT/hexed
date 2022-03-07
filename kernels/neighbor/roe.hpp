e0d3aa585307a41303772ec329791e7ca13aae40
Micaiah Smith-Pierce <micaiah@gatech.edu>
2022-03-07 13:26:45 -0500

    test for Roe flux (fails)

---
+++ kernels/neighbor/roe.hpp
@@ -0,0 +1,16 @@
+#ifndef CARTDG_ROE_HPP_
+#define CARTDG_ROE_HPP_
+
+#include <algorithm>
+#include <cmath>
+
+namespace cartdg
+{
+
+template<int n_dim, int n_face_qpoint>
+void roe(double* state_r, double* d_flux_w, double mult, int i_dim, double sp_heat_rat)
+{
+}
+
+}
+#endif
