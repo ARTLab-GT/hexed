5686e2e20fcd3af0af14d386fa7d7a99180b6044
Micaiah Smith-Pierce <micaiah@gatech.edu>
2022-03-06 19:50:44 -0500

    calculate maximum stable time step

--- script/ode.py
+++ script/ode.py
@@ -2,6 +2,7 @@ from Basis import *
 import numpy as np
 import matplotlib.pyplot as plt
 from sympy.integrals.quadrature import gauss_legendre, gauss_lobatto
+from scipy.optimize import fsolve
 
 calc_digits = 50
 row_size = 6
@@ -11,11 +12,10 @@ nodes = [(node + 1)/2 for node in nodes]
 weights = [weight/2 for weight in weights]
 basis = Basis(nodes, weights, calc_digits=calc_digits)
 
-n_elem = 4;
+n_elem = 2;
 diff_mat = np.zeros((n_elem*row_size, n_elem*row_size)) # time derivative matrix
 pos = np.zeros(n_elem*row_size)
 global_weights = np.zeros(n_elem*row_size)
-dt = 6e-2
 for i_elem in range(n_elem):
     for i_row in range(row_size):
         pos[i_elem*row_size + i_row] = i_elem + basis.nodes[i_row]
@@ -26,19 +26,28 @@ for i_elem in range(n_elem):
             diff_mat[((i_elem+1)%n_elem)*row_size + i_row, i_elem*row_size + i_col]  = basis.interpolate(i_col, 1)*basis.interpolate(i_row, 0)/weights[i_row]
 ident = np.identity(n_elem*row_size)
 assert np.linalg.norm(global_weights@diff_mat) < 1e-12
-stage_mat = ident + dt*diff_mat
-step_weights = [1., 1./4., 2./3.]
-step_mat = ident
-for weight in step_weights:
-    step_mat = (1. - weight)*ident + weight*stage_mat@step_mat
-for mat in [stage_mat, step_mat]:
-    eigvals, eigvecs = np.linalg.eig(mat)
-    print(np.max(np.abs(np.real(eigvals))))
-    plt.scatter(np.real(eigvals), np.imag(eigvals))
-plt.show()
 
+def euler(dt):
+    return ident + dt*diff_mat
+def rk(dt, step_weights):
+    step_mat = ident
+    for weight in step_weights:
+        step_mat = (1. - weight)*ident + weight*euler(dt)@step_mat
+    return step_mat
+def ssp_rk3(dt):
+    return rk(dt, [1., 1./4., 2./3.])
 
-eigvals, eigvecs = np.linalg.eig(stage_mat)
-plt.plot(pos, np.real(eigvecs[:, np.argmin(np.real(eigvals))]))
-plt.plot(pos, np.real(eigvecs[:, np.argmax(np.real(eigvals))]))
+def max_cfl(matrix_fun):
+    def error(log_dt):
+        dt = np.exp(log_dt)
+        eigvals, eigvecs = np.linalg.eig(matrix_fun(dt))
+        return np.min(np.real(eigvals)) + 1.
+    log_dt = fsolve(error, 0.)
+    return np.exp(log_dt)
+
+mc = max_cfl(euler)
+eigvals, eigvecs = np.linalg.eig(euler(mc))
+plt.scatter(np.real(eigvals), np.imag(eigvals))
 plt.show()
+print(max_cfl(euler))
+print(max_cfl(ssp_rk3))
