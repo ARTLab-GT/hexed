9c176f12ed8c304884492c6968a113e1781a7726
Micaiah Smith-Pierce <micaiah@gatech.edu>
2022-03-07 19:49:18 -0500

    possibly works (still fails)

--- kernels/neighbor/roe.hpp
+++ kernels/neighbor/roe.hpp
@@ -5,6 +5,8 @@
 #include <cmath>
 #include <Eigen/Dense>
 
+#include <iostream> // FIXME
+
 namespace cartdg
 {
 
@@ -21,7 +23,7 @@ void roe(double* state_r, double* d_flux_w, double mult, int i_dim, double heat_
     matrix_2 state;
     for (int i_side : {0, 1}) {
       for (int i_var = 0; i_var < n_var; ++i_var) {
-        state(i_var, i_side) = state_r[i_var*n_face_qpoint + i_qpoint];
+        state(i_var, i_side) = state_r[(i_side*n_var + i_var)*n_face_qpoint + i_qpoint];
       }
     }
     // compute derived quantities
@@ -37,13 +39,13 @@ void roe(double* state_r, double* d_flux_w, double mult, int i_dim, double heat_
     matrix_sq eigvecs = matrix_sq::Zero();
     vector eigvals;
     // convective
-    eigvecs(0      , 0) = num_state(0);
-    eigvecs(n_dim  , 0) = num_state(n_dim);
-    eigvecs(n_dim+1, 0) = num_state(0)*num_state(0)/(2.*num_state(n_dim));
-    for (int j_dim = n_dim + 1; j_dim < n_dim; j_dim = (j_dim + 1)%n_dim) { // iterate through all dimensions except `i_dim`
-      eigvecs(j_dim  , 0) = num_state(j_dim)/2.;
-      eigvecs(j_dim  , 1) = num_state(n_dim);
-      eigvecs(n_dim+1, 1) = num_state(j_dim);
+    eigvecs(0      , i_dim) = num_state(0);
+    eigvecs(n_dim  , i_dim) = num_state(n_dim);
+    eigvecs(n_dim+1, i_dim) = num_state(0)*num_state(0)/(2.*num_state(n_dim));
+    for (int j_dim = i_dim + 1; j_dim != i_dim; j_dim = (j_dim + 1)%n_dim) { // iterate through all dimensions except `i_dim`
+      eigvecs(j_dim  , i_dim) = num_state(j_dim)/2.;
+      eigvecs(j_dim  , j_dim) = num_state(n_dim);
+      eigvecs(n_dim+1, j_dim) = num_state(j_dim);
     }
     for (int j_dim = 0; j_dim < n_dim; ++j_dim) eigvals(j_dim) = num_state(i_dim)/num_state(n_dim);
     // accoustic
@@ -59,7 +61,7 @@ void roe(double* state_r, double* d_flux_w, double mult, int i_dim, double heat_
       eigvals(i_col) = num_state(i_dim)/num_state(n_dim) + sign*sound_speed;
     }
     // compute flux
-    matrix_2 transformed = eigvecs.householderQr().solve(state);
+    matrix_2 transformed = eigvecs.householderQr().solve(state); // don't pivot since this is not accuracy-critical
     vector num_flux;
     for (int i_var = 0; i_var < n_var; ++i_var) {
       num_flux(i_var) = eigvals(i_var)*transformed(i_var, eigvals(i_var) < 0.);
@@ -67,7 +69,7 @@ void roe(double* state_r, double* d_flux_w, double mult, int i_dim, double heat_
     num_flux = eigvecs*num_flux;
     // write flux
     for (int i_var = 0; i_var < n_var; ++i_var) {
-      d_flux_w[n_face_qpoint + i_qpoint] = d_flux_w[i_qpoint] = num_flux(i_var);
+      for (int i_side : {0, 1}) d_flux_w[(i_side*n_var + i_var)*n_face_qpoint + i_qpoint] = num_flux(i_var);
     }
   }
 }
