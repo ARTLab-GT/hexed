4a5af1dbe2b3f1f9f01dbb21154913963cc3045b
Micaiah Smith-Pierce <micaiah@gatech.edu>
2022-03-08 14:32:17 -0500

    test ddg

--- script/ode.py
+++ script/ode.py
@@ -13,7 +13,9 @@ basis = Basis(nodes, weights, calc_digits=calc_digits)
 
 n_elem = 8;
 advection = np.zeros((n_elem*row_size, n_elem*row_size))
-unstab_grad = np.zeros((n_elem*row_size, n_elem*row_size))
+local_grad = np.zeros((n_elem*row_size, n_elem*row_size))
+neighb_correction = np.zeros((n_elem*row_size, n_elem*row_size))
+dissipation = np.zeros((n_elem*row_size, n_elem*row_size))
 pos = np.zeros(n_elem*row_size)
 global_weights = np.zeros(n_elem*row_size)
 for i_elem in range(n_elem):
@@ -24,15 +26,19 @@ for i_elem in range(n_elem):
             advection[  i_elem           *row_size + i_row, i_elem*row_size + i_col] = -basis.derivative(i_row, i_col)
             advection[  i_elem           *row_size + i_row, i_elem*row_size + i_col] -= basis.interpolate(i_col, 0)*basis.interpolate(i_row, 0)/weights[i_row]
             advection[((i_elem+1)%n_elem)*row_size + i_row, i_elem*row_size + i_col]  = basis.interpolate(i_col, 1)*basis.interpolate(i_row, 0)/weights[i_row]
-            unstab_grad[i_elem*row_size + i_row, i_elem*row_size + i_col] += basis.derivative(i_row, i_col)
+            local_grad[ i_elem*row_size + i_row, i_elem*row_size + i_col] = basis.derivative(i_row, i_col)
             for i_side in [0, 1]:
                 for j_side in [0, 1]:
-                    correction = 0.5*(2*j_side - 1)*basis.interpolate(i_col, 1-j_side)*basis.interpolate(i_row, 1-i_side)/weights[i_row]
-                    unstab_grad[((i_elem+i_side)%n_elem)*row_size + i_row, ((i_elem+j_side)%n_elem)*row_size + i_col] += correction
-diffusion = unstab_grad@unstab_grad
+                    boundary = basis.interpolate(i_col, 1-j_side)*basis.interpolate(i_row, 1-i_side)/weights[i_row]
+                    neighb_correction[((i_elem+i_side)%n_elem)*row_size + i_row, ((i_elem+j_side)%n_elem)*row_size + i_col] += 0.5*(2*j_side - 1)*boundary
+                    dissipation[      ((i_elem+i_side)%n_elem)*row_size + i_row, ((i_elem+j_side)%n_elem)*row_size + i_col] += (1 - 2*(j_side == i_side))*boundary
+unstab_grad = local_grad + neighb_correction
+ldg = unstab_grad@unstab_grad
+ddg = unstab_grad@local_grad + dissipation
 ident = np.identity(n_elem*row_size)
 assert np.linalg.norm(global_weights@advection) < 1e-12
-assert np.linalg.norm(global_weights@diffusion) < 1e-12
+assert np.linalg.norm(global_weights@ldg) < 1e-12
+assert np.linalg.norm(global_weights@ddg) < 1e-12
 
 def euler(dt, diff_mat):
     return ident + dt*diff_mat
@@ -56,9 +62,12 @@ def max_cfl(matrix_fun, diff_mat):
 mc, step_mat = max_cfl(ssp_rk3, advection)
 eigvals, eigvecs = np.linalg.eig(step_mat)
 plt.scatter(np.real(eigvals), np.imag(eigvals), label=f"Advection SSP RK3, CFL = {mc:.2g}")
-mc, step_mat = max_cfl(euler, diffusion)
+mc, step_mat = max_cfl(euler, ldg)
 eigvals, eigvecs = np.linalg.eig(step_mat)
-plt.scatter(np.real(eigvals), np.imag(eigvals), label=f"Diffusion Fwd Euler, CFL = {mc:.2g}")
+plt.scatter(np.real(eigvals), np.imag(eigvals), label=f"LDG Fwd Euler, CFL = {mc:.2g}")
+mc, step_mat = max_cfl(euler, ddg)
+eigvals, eigvecs = np.linalg.eig(step_mat)
+plt.scatter(np.real(eigvals), np.imag(eigvals), label=f"DDG Fwd Euler, CFL = {mc:.2g}")
 angle = np.linspace(0, 2.*np.pi, 1000)
 plt.plot(np.cos(angle), np.sin(angle), "k", label="stability boundary")
 plt.title("eigenvalues")
