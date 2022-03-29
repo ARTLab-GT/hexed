a075c5cd9b1b4c0e9def75d39f893c6d6c7ad24a
Micaiah Smith-Pierce <micaiah@gatech.edu>
2022-03-29 14:40:17 -0400

    implemented neighbor Cartesian

---
+++ include/Neighbor_cartesian.hpp
@@ -0,0 +1,68 @@
+#ifndef CARTDG_NEIGHBOR_CARTESIAN_HPP_
+#define CARTDG_NEIGHBOR_CARTESIAN_HPP_
+
+#include "Vector_view.hpp"
+#include "connection.hpp"
+#include "kernel_factory.hpp"
+#include "math.hpp"
+#include "hll.hpp"
+
+namespace cartdg
+{
+
+/*
+ * Computes the numerical flux at the faces based on the extrapolated state data.
+ * State is read from the `face`s of the face connections and the numerical flux is
+ * written to the same location (the same flux is written to both faces).
+ */
+class Neighbor_cartesian_dynamic
+{
+  public:
+  virtual ~Neighbor_cartesian_dynamic() = default;
+  virtual void execute(Sequence<Face_connection<Element>&>&) = 0;
+};
+
+template <int n_dim, int row_size>
+class Neighbor_cartesian : public Neighbor_cartesian_dynamic
+{
+  const double heat_rat;
+  public:
+  Neighbor_cartesian(double heat_ratio=1.4) : heat_rat{heat_ratio} {}
+
+  virtual void execute(Sequence<Face_connection<Element>&>& connections)
+  {
+    constexpr int n_var = n_dim + 2;
+    constexpr int n_face_qpoint = custom_math::pow(row_size, n_dim - 1);
+    constexpr int face_size = n_face_qpoint*n_var;
+    #pragma omp parallel for
+    for (unsigned i_con = 0; i_con < connections.size(); ++i_con)
+    {
+      double face [2*face_size];
+      auto& con = connections[i_con];
+      const int i_dim = con.direction().i_dim();
+      for (int i_side : {0, 1}) {
+        double* con_face = con.face(i_side);
+        for (int i_face_dof = 0; i_face_dof < face_size; ++i_face_dof) {
+          face[i_side*face_size + i_face_dof] = con_face[i_face_dof];
+        }
+      }
+      hll<n_dim, n_face_qpoint>(face, i_dim, heat_rat);
+      for (int i_side : {0, 1}) {
+        double* con_face = con.face(i_side);
+        for (int i_face_dof = 0; i_face_dof < face_size; ++i_face_dof) {
+          con_face[i_face_dof] = face[i_side*face_size + i_face_dof];
+        }
+      }
+    }
+  }
+};
+
+template<>
+class Kernel_traits<Neighbor_cartesian>
+{
+  public:
+  using base_t = Neighbor_cartesian_dynamic;
+};
+
+}
+#endif
---
+++ include/hll.hpp
@@ -0,0 +1,68 @@
+#ifndef CARTDG_HLL_HPP_
+#define CARTDG_HLL_HPP_
+
+#include <algorithm>
+#include <cmath>
+
+namespace cartdg
+{
+
+/*
+ * Computes the numerical face flux using a Harten-Lax-van Leer (HLL) flux.
+ * The face state is obtained from `data` (layout: `[i_side][i_var][i_face_qpoint]`).
+ * The resulting numerical flux is then written to `data` (with a copy for each side).
+ */
+template<int n_dim, int n_face_qpoint>
+void hll(double* data, int i_dim, double heat_rat)
+{
+  const int n_var = n_dim + 2;
+  const int face_size = n_var*n_face_qpoint;
+
+  for (int i_qpoint = 0; i_qpoint < n_face_qpoint; ++i_qpoint)
+  {
+    double flux[2][n_var];
+    double velocity[2];
+    double sound_speed[2];
+
+    // compute flux and other variables on each side
+    for (int i_side = 0; i_side < 2; ++i_side) {
+      #define READ(i) data[(i)*n_face_qpoint + i_qpoint + i_side*face_size]
+      #define FLUX(i) flux[i_side][i]
+      double veloc = READ(i_dim)/READ(n_var - 2);
+      double pres = 0;
+      for (int j_dim = 0; j_dim < n_var - 2; ++j_dim) {
+        FLUX(j_dim) = READ(j_dim)*veloc;
+        pres += READ(j_dim)*READ(j_dim)/READ(n_var - 2);
+      }
+      pres = (heat_rat - 1.)*(READ(n_var - 1) - 0.5*pres);
+      FLUX(i_dim) += pres;
+      FLUX(n_var - 2) = READ(i_dim);
+      FLUX(n_var - 1) = (READ(n_var - 1) + pres)*veloc;
+      velocity[i_side] = veloc;
+      sound_speed[i_side] = std::sqrt(heat_rat*pres/READ(n_var - 2));
+      #undef FLUX
+      #undef READ
+    }
+    double wave_speed [2];
+    wave_speed[0] = std::min(velocity[0] - sound_speed[0], velocity[1] - sound_speed[1]);
+    wave_speed[1] = std::max(velocity[0] + sound_speed[0], velocity[1] + sound_speed[1]);
+
+    // get numerical flux from the flux/state on both sides
+    for (int i_var = 0; i_var < n_var; ++i_var) {
+      const int i = i_var*n_face_qpoint + i_qpoint;
+      double num_flux;
+      if      (wave_speed[0] >= 0) num_flux = flux[0][i_var];
+      else if (wave_speed[1] <= 0) num_flux = flux[1][i_var];
+      else {
+        num_flux = (wave_speed[1]*flux[0][i_var] - wave_speed[0]*flux[1][i_var]
+                    + wave_speed[0]*wave_speed[1]*(data[i + face_size] - data[i]))
+                   / (wave_speed[1] - wave_speed[0]);
+      }
+      data[i            ] = num_flux;
+      data[i + face_size] = num_flux;
+    }
+  }
+}
+
+}
+#endif
