65f69af7857f29672224dd378779f0365a1c6c11
Micaiah Smith-Pierce <micaiah@gatech.edu>
2022-03-31 11:51:25 -0400

    implemented Prolong/Restrict

---
+++ include/Prolong_refined.hpp
@@ -0,0 +1,88 @@
+#ifndef CARTDG_PROLONG_REFINED_HPP_
+#define CARTDG_PROLONG_REFINED_HPP_
+
+#include "Vector_view.hpp"
+#include "Refined_face.hpp"
+#include "Basis.hpp"
+#include "kernel_factory.hpp"
+#include "math.hpp"
+
+namespace cartdg
+{
+
+/*
+ * See `Refined_face.hpp`.
+ * Transforms data from coarse face to fine faces by polynomial interpolation.
+ * This interpolation is exact (and therefore conservative).
+ */
+class Prolong_refined_dynamic
+{
+  public:
+  virtual ~Prolong_refined_dynamic() = default;
+  virtual void operator()(Sequence<Refined_face&>&) = 0;
+};
+
+template <int n_dim, int row_size>
+class Prolong_refined : public Prolong_refined_dynamic
+{
+  const Eigen::Matrix<double, row_size, row_size> prolong_mat [2];
+
+  public:
+  Prolong_refined(const Basis& basis) : prolong_mat{basis.prolong(0), basis.prolong(1)} {}
+
+  virtual void operator()(Sequence<Refined_face&>& ref_faces)
+  {
+    constexpr int n_var = n_dim + 2;
+    constexpr int n_face = custom_math::pow(2, n_dim - 1);
+    constexpr int nfq = custom_math::pow(row_size, n_dim - 1);
+
+    #pragma omp parallel for
+    for (int i_ref_face = 0; i_ref_face < ref_faces.size(); ++i_ref_face)
+    {
+      auto& ref_face {ref_faces[i_ref_face]};
+      double* coarse {ref_face.coarse_face()};
+      for (int i_face = 0; i_face < n_face; ++i_face)
+      {
+        double* fine {ref_face.fine_face(i_face)};
+        for (int i_var = 0; i_var < n_var; ++i_var)
+        {
+          double* var_face {fine + i_var*nfq};
+          for (int i_qpoint = 0; i_qpoint < nfq; ++i_qpoint) {
+            var_face[i_qpoint] = coarse[i_var*nfq + i_qpoint];
+          }
+          for (int j_dim = 0; j_dim < n_dim - 1; ++j_dim)
+          {
+            const int pow {n_dim - 2 - j_dim};
+            const int face_stride {custom_math::pow(2, pow)};
+            const int qpoint_stride {custom_math::pow(row_size, pow)};
+            const int i_half {(i_face/face_stride)%2};
+            for (int i_outer = 0; i_outer < nfq/(row_size*qpoint_stride); ++i_outer)
+            {
+              for (int i_inner = 0; i_inner < qpoint_stride; ++i_inner)
+              {
+                Eigen::Matrix<double, row_size, 1> row;
+                for (int i_qpoint = 0; i_qpoint < row_size; ++i_qpoint) {
+                  row(i_qpoint) = var_face[(i_outer*row_size + i_qpoint)*qpoint_stride + i_inner];
+                }
+                row = prolong_mat[i_half]*row;
+                for (int i_qpoint = 0; i_qpoint < row_size; ++i_qpoint) {
+                  var_face[(i_outer*row_size + i_qpoint)*qpoint_stride + i_inner] = row(i_qpoint);
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+};
+
+template<>
+class Kernel_traits<Prolong_refined>
+{
+  public:
+  using base_t = Prolong_refined_dynamic;
+};
+
+}
+#endif
---
+++ test/test_Prolong_refined.cpp
@@ -0,0 +1,39 @@
+#include <catch2/catch.hpp>
+#include <Prolong_refined.hpp>
+#include <Gauss_legendre.hpp>
+
+TEST_CASE("Prolong_refined")
+{
+  // test that prolongation operator is approximately correct for an exponential function
+  const int row_size {cartdg::config::max_row_size};
+  cartdg::Storage_params params {2, 5, 3, row_size};
+  cartdg::Gauss_legendre basis {row_size};
+  double coarse [5][row_size][row_size] {};
+  std::vector<cartdg::Refined_face> ref_faces;
+  ref_faces.emplace_back(params, coarse[0][0]);
+  cartdg::Vector_view<cartdg::Refined_face&, cartdg::Refined_face> ref_face_v {ref_faces};
+
+  for (int i_var = 0; i_var < 5; ++i_var) {
+    for (int i_node = 0; i_node < row_size; ++i_node) {
+      for (int j_node = 0; j_node < row_size; ++j_node) {
+        coarse[i_var][i_node][j_node] = std::exp(basis.node(i_node) + 0.5*basis.node(j_node)) + i_var;
+      }
+    }
+  }
+
+  (*cartdg::kernel_factory<cartdg::Prolong_refined>(3, row_size, basis))(ref_face_v);
+
+  for (int i_half : {0, 1}) {
+    for (int j_half : {0, 1}) {
+      for (int i_node = 0; i_node < row_size; ++i_node) {
+        for (int j_node = 0; j_node < row_size; ++j_node) {
+          for (int i_var = 0; i_var < 5; ++i_var) {
+            double prolonged {ref_faces[0].fine_face(i_half*2 + j_half)[(i_var*row_size + i_node)*row_size + j_node]};
+            double correct {std::exp((basis.node(i_node) + i_half)/2. + 0.5*(basis.node(j_node) + j_half)/2.) + i_var};
+            REQUIRE(prolonged == Approx(correct).margin(1e-4));
+          }
+        }
+      }
+    }
+  }
+}
