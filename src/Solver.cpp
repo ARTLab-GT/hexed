25cbea11c1eec3e9f7ba2aab7dc3a16834f8e1a5
Micaiah Smith-Pierce <micaiah@gatech.edu>
2022-08-23 17:15:28 -0400

    use Vis_data for edges (can't test without vpn)

--- src/Solver.cpp
+++ src/Solver.cpp
@@ -10,6 +10,7 @@
 #include <Local_cartesian.hpp>
 #include <Local_deformed.hpp>
 #include <Tecplot_file.hpp>
+#include <Vis_data.hpp>
 
 namespace cartdg
 {
@@ -367,7 +368,6 @@ void Solver::visualize_field_tecplot(const Qpoint_func& output_variables, std::s
   const int n_vis = output_variables.n_var(n_dim); // number of variables to visualize
   const int n_qpoint = params.n_qpoint();
   const int n_corners {custom_math::pow(2, n_dim - 1)};
-  const int nfqpoint = n_qpoint/params.row_size;
   const int n_block {custom_math::pow(n_sample, n_dim)};
   Eigen::MatrixXd interp {basis.interpolate(Eigen::VectorXd::LinSpaced(n_sample, 0., 1.))};
   std::vector<std::string> var_names;
@@ -377,6 +377,7 @@ void Solver::visualize_field_tecplot(const Qpoint_func& output_variables, std::s
   for (int i_elem = 0; i_elem < acc_mesh.elements().size(); ++i_elem)
   {
     Element& elem {acc_mesh.elements()[i_elem]};
+    Vis_data vis(elem, output_variables, basis);
     // fetch data at quadrature points
     std::vector<double> pos (n_qpoint*n_dim);
     std::vector<double> to_vis (n_qpoint*n_vis);
@@ -397,38 +398,10 @@ void Solver::visualize_field_tecplot(const Qpoint_func& output_variables, std::s
     if (n_dim > 1) // 1D elements don't really have edges
     {
       Tecplot_file::Line_segments edges {file, n_dim*n_corners, n_sample, "edges"};
-      Eigen::MatrixXd boundary {basis.boundary()};
-      for (int i_dim = 0; i_dim < n_dim; ++i_dim)
-      {
-        const int stride {custom_math::pow(params.row_size, n_dim - 1 - i_dim)};
-        const int n_outer {n_qpoint/stride/params.row_size};
-
-        auto extract_edge = [=](double* data, int n)
-        {
-          Eigen::MatrixXd edge {n_sample, n_corners*n};
-          for (int i = 0; i < n; ++i) {
-            Eigen::MatrixXd edge_qpoints {basis.row_size, n_corners};
-            for (int i_qpoint = 0; i_qpoint < basis.row_size; ++i_qpoint) {
-              Eigen::VectorXd qpoint_slab {nfqpoint};
-              for (int i_outer = 0; i_outer < n_outer; ++i_outer) {
-                for (int i_inner = 0; i_inner < stride; ++i_inner) {
-                  qpoint_slab[i_outer*stride + i_inner] = data[i*n_qpoint + i_qpoint*stride + i_outer*stride*basis.row_size + i_inner];
-                }
-              }
-              edge_qpoints.row(i_qpoint) = custom_math::hypercube_matvec(boundary, qpoint_slab);
-            }
-            for (int i_corner = 0; i_corner < n_corners; ++i_corner) {
-              edge.col(i_corner*n + i) = interp*edge_qpoints.col(i_corner);
-            }
-          }
-          return edge;
-        };
-
-        Eigen::MatrixXd edge_pos {extract_edge(pos.data(), n_dim)};
-        Eigen::MatrixXd edge_state {extract_edge(to_vis.data(), n_vis)};
-        for (int i_corner = 0; i_corner < n_corners; ++i_corner) {
-          edges.write(edge_pos.data() + i_corner*n_dim*n_sample, edge_state.data() + i_corner*n_vis*n_sample);
-        }
+      Eigen::MatrixXd edge_pos = vis.edges(n_sample - 1);
+      Eigen::MatrixXd edge_state = Eigen::MatrixXd::Zero(n_sample, output_variables.n_var(n_dim));
+      for (int i_edge = 0; i_edge < n_corners*n_dim; ++i_edge) {
+        edges.write(edge_pos(Eigen::all, i_edge).data(), edge_state.data());
       }
     }
 
