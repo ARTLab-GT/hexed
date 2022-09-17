931c84faf477aeebbef374f8025b0a5e8899b2e0
Micaiah Smith-Pierce <micaiah@gatech.edu>
2022-09-17 15:24:32 -0400

    Prolong_refined accounds for stretching

--- test/test_Prolong_refined.cpp
+++ test/test_Prolong_refined.cpp
@@ -4,27 +4,25 @@
 
 TEST_CASE("Prolong_refined")
 {
-  SECTION("no stretching")
-  {
-    // test that prolongation operator is approximately correct for an exponential function
-    const int row_size {cartdg::config::max_row_size};
-    cartdg::Storage_params params {2, 5, 3, row_size};
-    cartdg::Gauss_legendre basis {row_size};
-    double coarse [5][row_size][row_size] {};
-    std::vector<cartdg::Refined_face> ref_faces;
-    ref_faces.emplace_back(params, coarse[0][0]);
-    cartdg::Vector_view<cartdg::Refined_face&, cartdg::Refined_face> ref_face_v {ref_faces};
-
-    for (int i_var = 0; i_var < 5; ++i_var) {
-      for (int i_node = 0; i_node < row_size; ++i_node) {
-        for (int j_node = 0; j_node < row_size; ++j_node) {
-          coarse[i_var][i_node][j_node] = std::exp(basis.node(i_node) + 0.5*basis.node(j_node)) + i_var;
-        }
+  // test that prolongation operator is approximately correct for an exponential function
+  const int row_size {cartdg::config::max_row_size};
+  cartdg::Storage_params params {2, 5, 3, row_size};
+  cartdg::Gauss_legendre basis {row_size};
+  double coarse [5][row_size][row_size] {};
+  std::vector<cartdg::Refined_face> ref_faces;
+  cartdg::Vector_view<cartdg::Refined_face&, cartdg::Refined_face> ref_face_v {ref_faces};
+  for (int i_var = 0; i_var < 5; ++i_var) {
+    for (int i_node = 0; i_node < row_size; ++i_node) {
+      for (int j_node = 0; j_node < row_size; ++j_node) {
+        coarse[i_var][i_node][j_node] = std::exp(basis.node(i_node) + 0.5*basis.node(j_node)) + i_var;
       }
     }
+  }
 
+  SECTION("no stretching")
+  {
+    ref_faces.emplace_back(params, coarse[0][0]);
     (*cartdg::kernel_factory<cartdg::Prolong_refined>(3, row_size, basis))(ref_face_v);
-
     for (int i_half : {0, 1}) {
       for (int j_half : {0, 1}) {
         for (int i_node = 0; i_node < row_size; ++i_node) {
@@ -34,6 +32,40 @@ TEST_CASE("Prolong_refined")
               double correct {std::exp((basis.node(i_node) + i_half)/2. + 0.5*(basis.node(j_node) + j_half)/2.) + i_var};
               REQUIRE(prolonged == Approx(correct).margin(1e-4));
             }
+          }
+        }
+      }
+    }
+  }
+
+  SECTION("stretch dim 0")
+  {
+    ref_faces.emplace_back(params, coarse[0][0], std::array<bool, 2>{true, false});
+    (*cartdg::kernel_factory<cartdg::Prolong_refined>(3, row_size, basis))(ref_face_v);
+    for (int j_half : {0, 1}) {
+      for (int i_node = 0; i_node < row_size; ++i_node) {
+        for (int j_node = 0; j_node < row_size; ++j_node) {
+          for (int i_var = 0; i_var < 5; ++i_var) {
+            double prolonged {ref_faces[0].fine_face(j_half)[(i_var*row_size + i_node)*row_size + j_node]};
+            double correct {std::exp(basis.node(i_node) + 0.5*(basis.node(j_node) + j_half)/2.) + i_var};
+            REQUIRE(prolonged == Approx(correct).margin(1e-4));
+          }
+        }
+      }
+    }
+  }
+
+  SECTION("stretch dim 1")
+  {
+    ref_faces.emplace_back(params, coarse[0][0], std::array<bool, 2>{false, true});
+    (*cartdg::kernel_factory<cartdg::Prolong_refined>(3, row_size, basis))(ref_face_v);
+    for (int i_half : {0, 1}) {
+      for (int i_node = 0; i_node < row_size; ++i_node) {
+        for (int j_node = 0; j_node < row_size; ++j_node) {
+          for (int i_var = 0; i_var < 5; ++i_var) {
+            double prolonged {ref_faces[0].fine_face(i_half)[(i_var*row_size + i_node)*row_size + j_node]};
+            double correct {std::exp((basis.node(i_node) + i_half)/2. + 0.5*basis.node(j_node)) + i_var};
+            REQUIRE(prolonged == Approx(correct).margin(1e-4));
           }
         }
       }
