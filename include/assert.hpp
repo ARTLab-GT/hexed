dd0617d7ca9ab6c8ebec0278e215d64071824662
Micaiah Smith-Pierce <micaiah@gatech.edu>
2022-11-18 12:09:10 -0500

    throw statement works in non-parallel

--- include/assert.hpp
+++ include/assert.hpp
@@ -2,6 +2,7 @@
 #define HEXED_ASSERT_HPP_
 
 #include <stdexcept>
+#include <omp.h>
 #include "config.hpp"
 
 namespace hexed::assert
@@ -9,19 +10,24 @@ namespace hexed::assert
 
 inline void throw_critical(const char* message)
 {
-  #pragma omp critical
-  throw std::runtime_error(message);
+  if (omp_get_level()) {
+    // if this is in a parallel region, only let one thread throw
+    // or else instead of the error message all you see is "terminate called recursively"
+    #pragma omp critical
+    throw std::runtime_error(message);
+  } else {
+    // if not in a parallel region, don't use the pragma cause that messes up try/catch
+    throw std::runtime_error(message);
+  }
 }
 
 }
 
 #define HEXED_ASSERT(expression, message) \
   if (!(expression)) { \
-    { \
-      char buffer [1000]; \
-      snprintf(buffer, 1000, "%s (in %s)", (message), __PRETTY_FUNCTION__); \
-      assert::throw_critical(buffer); \
-    } \
+    char buffer [1000]; \
+    snprintf(buffer, 1000, "%s (in %s)", (message), __PRETTY_FUNCTION__); \
+    assert::throw_critical(buffer); \
   } \
 
 #endif
