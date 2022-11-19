4bb33167c27a79a61bdc7e2f7ceee0c7ec8c87f8
Micaiah Smith-Pierce <micaiah@gatech.edu>
2022-11-19 16:50:53 -0500

    fixed local cartesian test

--- test/test_Solver.cpp
+++ test/test_Solver.cpp
@@ -575,7 +575,7 @@ void test_art_visc(Test_mesh& tm, std::string name)
   sol.initialize(Sinusoid());
   sol.set_art_visc_constant(300.);
   // update
-  sol.update();
+  sol.update(1e-3);
   auto status = sol.iteration_status();
   // check that the computed update is approximately equal to the exact solution
   for (auto handle : sol.mesh().elem_handles()) {
