b9683ea0c85e5b1f110044b6f63e8ca586e2544d
Micaiah Smith-Pierce <micaiah@gatech.edu>
2022-11-25 21:40:52 -0500

    less woefully inefficient

--- script/experiment.py
+++ script/experiment.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.special import legendre, hermite, chebyt
+from scipy.special import legendre, hermite, chebyt, jacobi
 
 def interp(dest_nodes, src_nodes):
     mat = np.zeros((dest_nodes.size, src_nodes.size))
@@ -30,31 +30,50 @@ for n in [3]:
     fig, axs = plt.subplots(2, 2)
     fig.set_size_inches(14, 8)
 
-    smear_rs = 10
-    scale = 10/rs
+    smear_rs = rs
+    #scale = 10/rs
+    scale = proj_width
+    """
     smear_weight = np.exp(-(x/scale)**2)
     smear_quad = hermite(2*smear_rs + 1).weights
+    """
+    """
+    smear_weight = x*0
+    smear_weight[np.logical_and(x > -scale, x < scale)] = 1
+    smear_quad = legendre(2*smear_rs + 1).weights
+    """
+    power = 3
+    smear_weight = x*0
+    mask = np.logical_and(x > -scale, x < scale)
+    smear_weight[mask] = ((scale - x[mask])*(scale + x[mask])/scale/smear_rs)**power
+    smear_quad = jacobi(2*smear_rs - 1, power, power).weights
     smear_quad[:, 0] *= scale
+    """
+    shift_rs = smear_rs + 3
+    smear_shift = chebyt(shift_rs).weights[:, 0]*scale
+    """
+    shift_rs = smear_quad.shape[0]
+    smear_shift = smear_quad[:, 0]
 
     poly = legendre(rs)
-    sample_width = 15.
-    shift = chebyt(100).weights[:, 0]*sample_width
+    """
+    sample_width = scale + proj_width
+    shift = chebyt(30).weights[:, 0]*sample_width
     raw = f(shift[:, np.newaxis] + x)
     shifted = np.zeros((smear_quad.shape[0], rs + 1, x.size))
     for i_node in range(rs + 1):
         shifted[:, i_node, :] = interp(smear_quad[:, 0] + proj_width*nodes[i_node], shift)@raw
     """
-    shifted = f(proj_width*nodes[np.newaxis, :, np.newaxis] + smear_quad[:, 0, np.newaxis, np.newaxis] + x)
-    """
+    shifted = f(proj_width*nodes[np.newaxis, :, np.newaxis] + smear_shift[:, np.newaxis, np.newaxis] + x)
     proj = poly(nodes)*weights
     projd = proj@shifted
-    smeared = smear_quad[:, 1]@projd**2
+    smeared = smear_quad[:, 1]@interp(smear_quad[:, 0], smear_shift)@projd**2
 
-    axs[0, 0].plot(x, shifted[smear_rs, n, :])
-    axs[1, 0].plot(x, projd[smear_rs])
+    axs[0, 0].plot(x, shifted[shift_rs//2, n, :])
+    axs[1, 0].plot(x, projd[shift_rs//2])
     axs[0, 1].plot(x, smear_weight)
     axs[0, 1].scatter(smear_quad[:, 0], smear_quad[:, 1])
-    axs[1, 1].plot(x, projd[smear_rs]**2)
+    axs[1, 1].plot(x, projd[shift_rs//2]**2)
     axs[1, 1].plot(x, smeared)
 
     for ax in axs.flatten():
