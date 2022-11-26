157b99925a7106818f5c951cb27ea0e2f2f46e42
Micaiah Smith-Pierce <micaiah@gatech.edu>
2022-11-25 19:42:23 -0500

    basic hermite quadrature

--- script/experiment.py
+++ script/experiment.py
@@ -1,34 +1,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.special import legendre
+from scipy.special import legendre, hermite
 
-n = 3
-rs = 2*n
-x = np.linspace(-7, 7, int(1e3) + 1)
+#for n in range(1, 10):
+for n in [3]:
+    rs = 2*n
+    x = np.linspace(-7, 7, int(1e3) + 1)
 
-quad = legendre(rs + 1).weights
-nodes = quad[:, 0]
-weights = quad[:, 1]
-fig, axs = plt.subplots(2, 2)
-fig.set_size_inches(14, 8)
+    quad = legendre(rs + 1).weights
+    nodes = quad[:, 0]
+    weights = quad[:, 1]
+    fig, axs = plt.subplots(2, 2)
+    fig.set_size_inches(14, 8)
 
-smear_weights = np.exp(-.5*x**2)
-smear_weights /= np.trapz(smear_weights)
+    smear_rs = rs
+    scale = 7/rs
+    smear_weight = np.exp(-(x/scale)**2)
+    smear_quad = hermite(2*smear_rs + 1).weights
+    smear_quad[:, 0] *= scale
 
-def f(x):
-    return np.tanh(1.*x)
-poly = legendre(rs)
-shifted = f(3*nodes[np.newaxis, :, np.newaxis] + x[:, np.newaxis, np.newaxis] + x)
-proj = poly(nodes)*weights
-projd = proj@shifted
-smeared = np.trapz(smear_weights*projd**2)
+    def f(x):
+        return np.tanh(1.*x)
+    poly = legendre(rs)
+    shifted = f(3*nodes[np.newaxis, :, np.newaxis] + smear_quad[:, 0, np.newaxis, np.newaxis] + x)
+    proj = poly(nodes)*weights
+    projd = proj@shifted
+    smeared = smear_quad[:, 1]@projd**2
 
-axs[0, 0].plot(x, shifted[x.size//2, n, :])
-axs[1, 0].plot(x, projd[x.size//2])
-axs[0, 1].plot(x, smear_weights)
-axs[1, 1].plot(x, projd[x.size//2]**2)
-axs[1, 1].plot(x, smeared)
+    axs[0, 0].plot(x, shifted[smear_rs, n, :])
+    axs[1, 0].plot(x, projd[smear_rs])
+    axs[0, 1].plot(x, smear_weight)
+    axs[0, 1].scatter(smear_quad[:, 0], smear_quad[:, 1])
+    axs[1, 1].plot(x, projd[smear_rs]**2)
+    axs[1, 1].plot(x, smeared)
 
-for ax in axs.flatten():
-    ax.grid(True)
-plt.show()
+    for ax in axs.flatten():
+        ax.grid(True)
+    plt.show()
