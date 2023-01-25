8d136ee2240c6d3af24f4ebf0b9e74ec9a4a79b9
Micaiah Smith-Pierce <micaiah@gatech.edu>
2023-01-25 13:17:28 -0500

    simpler investigation into flux eigenvalues

--- script/eigenvalues.py
+++ script/eigenvalues.py
@@ -1,45 +1,30 @@
-import sympy as sp
+import numpy as np
 
-# setup flux and jacobian
-heat_rat = sp.Symbol("gamma")
-state = sp.symbols("q_0, q_1, q_2, q_3") # conserved variables
-mass = state[2]
-veloc_norm = state[0]/state[2]
-veloc_tang = state[1]/state[2]
-pres = (heat_rat - 1)*(state[3] - mass/2*(veloc_norm**2 + veloc_tang**2))
-sound_speed = sp.sqrt(heat_rat*pres/mass)
-flux = [
-    mass*veloc_norm**2 + pres,
-    mass*veloc_norm*veloc_tang,
-    mass*veloc_norm,
-    veloc_norm*(heat_rat*pres/(heat_rat - 1) + mass/2*(veloc_norm**2 + veloc_tang**2))
-]
-flux = [sp.simplify(f) for f in flux]
-jacobian = sp.Matrix([[sp.simplify(flux[i_var].diff(state[j_var])) for j_var in range(4)] for i_var in range(4)])
+def flux(state):
+    pres = .4*(state[2] - .5*state[0]**2/state[1])
+    f = np.zeros(3)
+    f[0] = state[0]**2/state[1] + pres
+    f[1] = state[0]
+    f[2] = state[0]/state[1]*(state[2] + pres)
+    return f
 
-# compute eigensystem
-eigvals = list(jacobian.eigenvals())
-print("eigenvalues:")
-sp.pprint(eigvals)
-print()
-convective = (jacobian - eigvals[0]*sp.eye(4)).nullspace()
-convective[0] += convective[1]*state[0]**2/(2*state[2]**2)
-convective[0] = sp.simplify(convective[0]*state[2])
-convective[1] *= state[1]
-print("convective eigenvectors:")
-sp.pprint(convective)
-print()
-print("accoustic eigenvectors:")
-for i in [0, 1]:
-    eigval = eigvals[i + 1]
-    sign = 2*i - 1
-    eigvec = (jacobian - eigval*sp.eye(4)).nullspace()[0]
-    eigvec *= state[2]/eigvec[2]
-    eigvec = sp.simplify(eigvec)
-    sp.pprint(eigvec)
-    diff = eigvec - state[2]*sp.Matrix([veloc_norm + sign*sound_speed, veloc_tang, 1, (state[3] + pres)/mass + sign*veloc_norm*sound_speed])
-    diff = diff.subs(heat_rat, 1.4)
-    vals = [4.6, 3.78, 1.02, 1.401e5]
-    for i_var in range(4):
-        diff = diff.subs(state[i_var], vals[i_var])
-    print("error vs. expected expression: ", diff.evalf().norm())
+mass = 1.225
+pres = 101325
+sound_speed = (1.4*pres/mass)**.5
+#veloc = sound_speed
+veloc = 12.
+state = np.array([veloc*mass, mass, pres/.4 + .5*mass*veloc**2])
+d_mass = 1
+d_veloc = sound_speed/mass*d_mass
+d_pres = sound_speed**2*d_mass
+d_state = np.array([
+    d_mass*veloc + mass*d_veloc,
+    d_mass,
+    d_pres/.4 + .5*d_mass*veloc**2 + mass*veloc*d_veloc,
+])
+d = 1e-6
+d_flux = (flux(state + d*d_state) - flux(state))/d
+print(d_state)
+print(d_flux)
+print(d_flux/d_state)
+print(veloc + sound_speed)
