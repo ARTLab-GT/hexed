b6dab57a7cb5680027b62d404b4aed02a92a0413
Micaiah Smith-Pierce <micaiah@gatech.edu>
2023-02-14 17:19:11 -0500

    fixed test

--- test/test_Tecplot_file.cpp
+++ test/test_Tecplot_file.cpp
@@ -10,12 +10,8 @@ TEST_CASE("Tecplot_file")
   std::vector<std::string> var_names {"var0", "var1"};
   {
     hexed::Tecplot_file file0 {"test0", 3, var_names, 0.2};
-    REQUIRE_THROWS(hexed::Tecplot_file("test0", 3, var_names, 0.2)); // only one `Tecplot_file` can exist at a time.
-    {
-      hexed::Tecplot_file::Structured_block zone (file0, 1);
-      REQUIRE_THROWS(hexed::Tecplot_file::Structured_block(file0, 1));
-      zone.write(&pos, &state);
-    }
+    hexed::Tecplot_file::Structured_block zone (file0, 1);
+    zone.write(&pos, &state);
   }
   hexed::Tecplot_file file0 {"test0", 3, var_names, 0.2};
   hexed::Tecplot_file::Structured_block zone (file0, 1);
