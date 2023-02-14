c3d7693a23e89f270cb6b6b86b087c367710c848
Micaiah Smith-Pierce <micaiah@gatech.edu>
2023-02-14 17:21:31 -0500

    deleted obsolete code

--- src/Tecplot_file.cpp
+++ src/Tecplot_file.cpp
@@ -34,36 +34,11 @@ Tecplot_file::Tecplot_file(std::string file_name, int n_dim, std::vector<std::st
   tecDataSetAddAuxData(file_handle, "Common.CVar", std::to_string(2*n_dim + 1).c_str());
   tecDataSetAddAuxData(file_handle, "Common.DensityVar", std::to_string(2*n_dim + 1).c_str());
   tecDataSetAddAuxData(file_handle, "Common.StagnationEnergyVar", std::to_string(2*n_dim + 2).c_str());
-  #if 0
-  INTEGER4 Debug = 0;
-  INTEGER4 VIsDouble = 1;
-  INTEGER4 FileType = 0;
-  INTEGER4 fileFormat = 1; // 0 indicates .plt and 1 indicates .szplt
-  TECINI142("flow solution", var_name_list.c_str(), file_name.c_str(), ".", &fileFormat, &FileType, &Debug, &VIsDouble); // opens a new Tecplot file
-  #if 0
-  TECAUXSTR142("Common.Incompressible", "False");
-  TECAUXSTR142("Common.VectorVarsAreVelocity", "False");
-  TECAUXSTR142("Common.Gamma", std::to_string(heat_rat).c_str());
-  TECAUXSTR142("Common.GasConstant", std::to_string(gas_const).c_str());
-  std::vector<std::string> xyz {"X", "Y", "Z"};
-  std::vector<std::string> uvw {"U", "V", "W"};
-  for (int i_dim = 0; i_dim < n_dim; ++i_dim) {
-    TECAUXSTR142(("Common." + xyz[i_dim] + "Var").c_str(), std::to_string(i_dim + 1).c_str());
-    TECAUXSTR142(("Common." + uvw[i_dim] + "Var").c_str(), std::to_string(n_dim + i_dim + 1).c_str());
-  }
-  TECAUXSTR142("Common.CVar", std::to_string(2*n_dim + 1).c_str());
-  TECAUXSTR142("Common.DensityVar", std::to_string(2*n_dim + 1).c_str());
-  TECAUXSTR142("Common.StagnationEnergyVar", std::to_string(2*n_dim + 2).c_str());
-  #endif
-  #endif
 }
 
 Tecplot_file::~Tecplot_file()
 {
   tecFileWriterClose(&file_handle);
-  #if 0
-  TECEND142(); // closes Tecplot file
-  #endif
 }
 
 Tecplot_file::Zone::Zone(Tecplot_file& file, int n_nodes, std::string name_arg)
@@ -88,13 +63,6 @@ void Tecplot_file::Zone::write(const double* pos, const double* vars)
   for (int i_var = 0; i_var < file.n_var; ++i_var) {
     tecZoneVarWriteDoubleValues(file.file_handle, tecio_zone_index, file.n_dim + i_var + 1, 0, n_nodes, vars + i_var*n_nodes);
   }
-  #if 0
-  INTEGER4 IsDouble {1};
-  INTEGER4 size {file.n_dim*n_nodes};
-  TECDAT142(&size, pos, &IsDouble);
-  size = file.n_var*n_nodes;
-  TECDAT142(&size, vars, &IsDouble);
-  #endif
 }
 
 Tecplot_file::Structured_block::Structured_block(Tecplot_file& file, int row_size, std::string name_arg, int n_dim_arg)
@@ -103,30 +71,6 @@ Tecplot_file::Structured_block::Structured_block(Tecplot_file& file, int row_siz
   tecZoneCreateIJK(file.file_handle, name.c_str(),
                    row_size, (n_dim >= 2) ? row_size : 1, (n_dim >= 3) ? row_size : 1,
                    var_types.data(), shared.data(), location.data(), passive.data(), 0, 0, 0, &tecio_zone_index);
-  #if 0
-  INTEGER4 ICellMax = 0;
-  INTEGER4 JCellMax = 0;
-  INTEGER4 KCellMax = 0;
-  INTEGER4 unused = 0; // ParentZone is no longer used
-  INTEGER4 IsBlock = 1; // always set to 1
-  INTEGER4 NFConns = 0;
-  INTEGER4 FNMode = 0;
-  INTEGER4 TotalNumFaceNodes = 1;
-  INTEGER4 TotalNumBndryFaces = 1;
-  INTEGER4 TotalNumBndryConnections = 1;
-  INTEGER4 ShrConn = 0;
-  INTEGER4 IMax = row_size;
-  INTEGER4 JMax = (n_dim >= 2) ? row_size : 1;
-  INTEGER4 KMax = (n_dim >= 3) ? row_size : 1;
-
-  INTEGER4 ZoneType = 0; // 0 indicates ordered
-  TECZNE142(name.c_str(),
-            &ZoneType, &IMax, &JMax, &KMax, &ICellMax, &JCellMax, &KCellMax, &file.time, &file.strand_id,
-            &unused, &IsBlock, &NFConns, &FNMode, &TotalNumFaceNodes, &TotalNumBndryFaces,
-            &TotalNumBndryConnections, NULL, NULL, NULL, &ShrConn); // initialize new zone
-  #endif
-  ++file.strand_id; // next zone will be on next time strand
-  ++file.i_zone; // next zone will be named with next number
 }
 
 Tecplot_file::Line_segments::Line_segments(Tecplot_file& file, int n_segs, int row_size, std::string name_arg)
@@ -138,35 +82,6 @@ Tecplot_file::Line_segments::Line_segments(Tecplot_file& file, int n_segs, int r
   int zone_type = 1; // FELINESEG
   tecZoneCreateFE(file.file_handle, name.c_str(), zone_type, n_nodes, n_segs,
                   var_types.data(), shared.data(), location.data(), passive.data(), 0, 0, 0, &tecio_zone_index);
-
-  #if 0
-  INTEGER4 ZoneType {1}; // 1 indicates unstructured ("finite element", in Tecplot parlance) line segment
-  INTEGER4 NumPoints {n_nodes};
-  INTEGER4 NumElements {(row_size - 1)*n_segs};
-  INTEGER4 NumFaces {0};
-  INTEGER4 ICellMax {0};
-  INTEGER4 JCellMax {0};
-  INTEGER4 KCellMax {0};
-  INTEGER4 ParentZone {0};
-  INTEGER4 IsBlock {1};
-  INTEGER4 NumFaceConnections {0};
-  INTEGER4 FaceNeighborMode {0};
-  INTEGER4 TotalNumFaceNodes {0};
-  INTEGER4 NumConnectedBoundaryFaces {0};
-  INTEGER4 TotalNumBoundaryConnections {0};
-  INTEGER4* PassiveVarList {nullptr};
-  INTEGER4* ValueLocation {nullptr};
-  INTEGER4* SharVarFromZone {nullptr};
-  INTEGER4 ShareConnectivityFromZone {0};
-  TECZNE142(name.c_str(),
-            &ZoneType, &NumPoints, &NumElements, &NumFaces, &ICellMax, &JCellMax,
-            &KCellMax, &file.time, &file.strand_id, &ParentZone, &IsBlock, &NumFaceConnections,
-            &FaceNeighborMode, &TotalNumFaceNodes, &NumConnectedBoundaryFaces,
-            &TotalNumBoundaryConnections, PassiveVarList, ValueLocation, SharVarFromZone,
-            &ShareConnectivityFromZone); // initialize new zone
-  #endif
-  ++file.strand_id;
-  ++file.i_zone;
 }
 
 void Tecplot_file::Line_segments::write(const double* pos, const double* vars)
@@ -198,9 +113,6 @@ Tecplot_file::Line_segments::~Line_segments()
     }
   }
   tecZoneNodeMapWrite32(file.file_handle, tecio_zone_index, 0, 0, inds.size(), inds.data());
-  #if 0
-  TECNOD142(inds.data());
-  #endif
 }
 
 }
