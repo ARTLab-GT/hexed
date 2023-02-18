3d55158f62eae37aa4ab42caf41f7cff13ea76c2
Micaiah Smith-Pierce <micaiah@gatech.edu>
2023-02-18 06:23:31 -0500

    documented Mesh.hpp

--- include/Mesh.hpp
+++ include/Mesh.hpp
@@ -9,7 +9,7 @@
 namespace hexed
 {
 
-/*
+/*!
  * Represents a collection of interconnected elements. This class is an interface which supports
  * only manipulation of the mesh itself, not access to the elements it contains. One common theme
  * in this interface is the use of "serial numbers" to identify objects owned by the `Mesh` object.
@@ -23,21 +23,22 @@ class Mesh
 {
   public:
   virtual ~Mesh() = default;
+  //! Nominal size of elements with refinement level 0.
   virtual double root_size() = 0;
-  /*
+  /*!
    * Add an element at specified nominal position and serial number which uniquely identifies it
    * among elements of this mesh with the same refinement level and deformedness.
    */
   virtual int add_element(int ref_level, bool is_deformed, std::vector<int> position) = 0;
-  /*
+  /*!
    * Specify that two elements are connected via a Cartesian face. Note: although the interface
    * is stipulated to be Cartesian, the elements themselves can be deformed
    */
   virtual void connect_cartesian(int ref_level, std::array<int, 2> serial_n, Con_dir<Element> dir,
                                  std::array<bool, 2> is_deformed = {false, false}) = 0;
-  // Specify that two elements are connected via a deformed face. Requires both elements to be deformed.
+  //! Specify that two elements are connected via a deformed face. Requires both elements to be deformed.
   virtual void connect_deformed(int ref_level, std::array<int, 2> serial_n, Con_dir<Deformed_element> direction) = 0;
-  /*
+  /*!
    * specify that an element of refinement level `coarse_ref_level` is connected to some elements of refinement level
    * `coarse_ref_level + 1`.
    * Coarse element comes first in `Con_dir`.
@@ -54,20 +55,20 @@ class Mesh
   virtual void connect_hanging(int coarse_ref_level, int coarse_serial, std::vector<int> fine_serial, Con_dir<Deformed_element>,
                                bool coarse_deformed = false, std::vector<bool> fine_deformed = {false, false, false, false},
                                std::array<bool, 2> stretch = {false, false}) = 0;
-  /*
+  /*!
    * Acquires owenership of `*flow_bc` and `*mesh_bc` and constructs a `Boundary_condition` from them.
    * Returns a serial number which uniquely identifies the new boundary condition among this `Mesh`'s boundary conditions.
    * It is recommended to use this with `new`, like the constructor for `std::unique_ptr`.
    */
   virtual int add_boundary_condition(Flow_bc* flow_bc, Mesh_bc* mesh_bc) = 0;
-  /*
+  /*!
    * Connect a face of an element to a boundary condition. This BC will now be applied to that face. `i_dim` and `face_sign`
    * are used to identify which face of the element is participating in the boundary condition.
    */
   virtual void connect_boundary(int ref_level, bool is_deformed, int element_serial_n, int i_dim, int face_sign, int bc_serial_n) = 0;
-  // delete all boundary connections involving a certain boundary condition
+  //! delete all boundary connections involving a certain boundary condition
   virtual void disconnect_boundary(int bc_sn) = 0;
-  /*
+  /*!
    * Extrudes a layer of elements from unconnected faces:
    * 1. Extrudes one deformed element from every unconnected face of every deformed element.
    * 2. Connects the new elements to the faces they were generated from.
@@ -97,19 +98,18 @@ class Mesh
       height = new_height;
     }
   }
-  // connects all yet-unconnected faces to a boundary condition specified by serial number
+  //! connects all yet-unconnected faces to a boundary condition specified by serial number
   virtual void connect_rest(int bc_sn) = 0;
 
-  // An object to provide information about whether the mesh connectivity is valid
-  // and if not, why.
+  //! An object to provide information about whether the mesh connectivity is valid and if not, why.
   class Connection_validity
   {
     public:
-    const int n_duplicate; // number of faces with duplicate connections
-    const int n_missing; // number of missing connections
-    // returns true if connectivity is valid
+    const int n_duplicate; //!< number of faces with duplicate connections
+    const int n_missing; //!< number of missing connections
+    //! returns true if connectivity is valid
     inline operator bool() {return (n_duplicate == 0) && (n_missing == 0);}
-    // if connectivity is invalid, throw an exception with a helpful message
+    //! if connectivity is invalid, throw an exception with a helpful message
     inline void assert_valid()
     {
       if (!*this) {
@@ -119,17 +119,16 @@ class Mesh
       }
     }
   };
-  /*
+  /*!
    * returns a `Connection_validity` object describing whether the mesh connectivity is valid.
    * Suggested uses:
-   * - `if (mesh.valid())` {...do something that requires a valid mesh}
+   * - `if (mesh.valid()) {\\...do something that requires a valid mesh}`
    * - `mesh.valid().assert_valid();`
    */
   virtual Connection_validity valid() = 0;
-  // minimum information required to identify an element
+  //! minimum information required to identify an element
   struct elem_handle {int ref_level; bool is_deformed; int serial_n;};
-  // get handles for all elements currently in the mesh, in no particular order
-  // (mostly for testing/debugging)
+  //! get handles for all elements currently in the mesh, in no particular order (mostly for testing/debugging)
   virtual std::vector<elem_handle> elem_handles() = 0;
 };
 
