e03b00dea0e25465bd73cc3878cea7a416eb0ae9
micaiah <micaiah@smithpierce.net>
2023-06-30 00:56:30 -0400

    angles + doc improvements

--- include/constants.hpp
+++ include/constants.hpp
@@ -2,8 +2,10 @@
 #define HEXED_CONSTANTS_HPP_
 
 /*! \file constants.hpp
- * Definition of physical constants as `const` global variables
+ * Definition of mathematical and physical constants as `const` global variables
  * (in terms of [SI base units](https://en.wikipedia.org/wiki/SI_base_unit) when possible).
+ * Note the doc descriptions with the mathematical symbols for the variables are mostly just
+ * so the variables can be linked to in Doxygen but maybe they will be useful to someone...
  */
 
 #include "math.hpp"
@@ -11,21 +13,33 @@
 namespace hexed
 {
 
-// SI definitions (wikipedia)
-const double light_speed = 299792458;
-const double plank = 6.62607015e-34;
-const double boltzmann = 1.380649e-23;
-const double avogadro = 6.02214076e23;
+//! \name mathematical constants
+//!\{
+const double pi = M_PI; //!< \brief \f$ \pi \f$
+const double degree = pi/360;//!< \brief \f$ ^{\circ} \f$
+//!\}
 
-// empirical values
-const double mol_mass_air = 28.9647e-3; // engineering toolbox
+//! \name SI definitions (wikipedia)
+//!\{
+const double light_speed = 299792458; //!< \brief \f$ c \f$
+const double plank = 6.62607015e-34; //!< \brief \f$ h \f$
+const double boltzmann = 1.380649e-23; //!< \brief \f$ k \f$
+const double avogadro = 6.02214076e23; //!< \brief \f$ mol \f$
+//!\}
 
-// derived quantities
+//! \name empirical values
+//!\{
+const double mol_mass_air = 28.9647e-3; //!< \brief \f$ \mu_{air} \f$ (from engineering toolbox)
+//!\}
+
+//! \name derived quantities
+//!\{
 #define P(base, exp) math::pow((base), (exp))
-const double universal_gas = boltzmann*avogadro;
-const double specific_gas_air = universal_gas/mol_mass_air;
-const double stefan_boltzmann = 2*P(M_PI, 5)*P(boltzmann, 4)/(15*P(light_speed, 2)*P(plank, 3));
+const double universal_gas = boltzmann*avogadro; //!< \brief \f$ R_u \f$
+const double specific_gas_air = universal_gas/mol_mass_air; //!< \brief \f$ R_{air} \f$
+const double stefan_boltzmann = 2*P(pi, 5)*P(boltzmann, 4)/(15*P(light_speed, 2)*P(plank, 3)); //!< \brief \f$ \sigma \f$
 #undef P
+//!\}
 
 }
 #endif
