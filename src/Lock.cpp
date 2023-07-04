24bec0e2f76e228b46734d6fe4813f89121329ee
micaiah <micaiah@smithpierce.net>
2023-07-04 14:12:56 -0400

    works if unthreaded

--- src/Lock.cpp
+++ src/Lock.cpp
@@ -6,22 +6,30 @@ namespace hexed
 Lock::Acquire::Acquire(Lock& ref)
 : lock{ref}
 {
+  #if HEXED_THREADED
   omp_set_lock(&lock.l);
+  #endif
 }
 
 Lock::Acquire::~Acquire()
 {
+  #if HEXED_THREADED
   omp_unset_lock(&lock.l);
+  #endif
 }
 
 Lock::Lock()
 {
+  #if HEXED_THREADED
   omp_init_lock(&l);
+  #endif
 }
 
 Lock::~Lock()
 {
+  #if HEXED_THREADED
   omp_destroy_lock(&l);
+  #endif
 }
 
 }
