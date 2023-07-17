b746a647153eea0bf2cc27bff49a078d64091eeb
Micaiah Smith-Pierce <micaiah@gatech.edu>
2023-07-17 14:44:54 -0400

    CMake integration

--- python/hexed_python.py
+++ python/hexed_python.py
@@ -1,12 +1,12 @@
 import cppyy
-import cppyy.ll
-from cppyy.gbl.std import vector
+from hexed_py_config import *
 
-path = "build_Release"
-cppyy.add_include_path("include")
-cppyy.add_include_path(f"{path}/include/auto")
-cppyy.load_library(f"{path}/libhexed")
-cppyy.include("Solver_interface.hpp")
+for path in include_paths:
+    print(path)
+    cppyy.add_include_path(path)
+for lib in libraries:
+    cppyy.load_library(lib)
+cppyy.include("hexed/Solver_interface.hpp")
 cpp = cppyy.gbl.hexed
 solver = cpp.make_solver(2, 6, 1.)
 solver.mesh().add_tree([cpp.new_copy(cpp.Nonpenetration()) for i in range(4)])
--- samples/naca0012.py
+++ samples/naca0012.py
@@ -1,5 +1,7 @@
-import hexed
+import hexed_python
 
+"""
 solver = hexed.Solver(2, 6)
 solver.generate_mesh([-10, -10], [10, 10], geometries = [hexed.naca("0012", int(1e4))], surf_resolution = .1, refine_sweeps = 8)
 solver.visualize_field("mesh")
+"""
