4f1bdd08ab4f06272a883c94a75a6b4733a6d599
Micaiah Smith-Pierce <micaiah@gatech.edu>
2023-07-17 15:48:30 -0400

    setup documentation

--- python/hexed_python.py
+++ python/hexed_python.py
@@ -1,13 +1,30 @@
 import cppyy
 from hexed_py_config import *
 
+## \namespace hexed_python
+# \brief Internal namespace for the Python API.
+# \details In the Doxygen documentation,
+# everything in the Python API will appear in the namespace `hexed_python` because it is defined in the file `python/hexed_python.py`
+# However, when you actually use it, it lives in the module `hexed` (import it with `import hexed`).
+# This distinction prevents naming ambiguities in the documentation,
+# since both the C++ namespace and the Python module are called `hexed`.
+
+## \namespace hexed_python.cpp
+# \brief Namespace for accessing the C++ API in Python.
+# \details This submodule provides Python bindings to the C++ namespace `hexed`
+# via [cppyy](https://cppyy.readthedocs.io/en/latest/starting.html).
+# For the most part, everything should work pretty much exactly as it would in C++.
+# For example, to call `hexed::math::pow``(3, 2)` in Python you can do `hexed.cpp.math.pow(3, 2)`.
+# However, please read the docs on cppyy if you want to do anything fancy.
+# This submodule doesn't provide access to all the C++ definitions,
+# but it should provide everything you need for high-level solver execution.
+
 for path in include_paths:
-    print(path)
     cppyy.add_include_path(path)
 for lib in libraries:
     cppyy.load_library(lib)
+cppyy.include("hexed/math.hpp")
 cppyy.include("hexed/Solver_interface.hpp")
+## \cond
 cpp = cppyy.gbl.hexed
-solver = cpp.make_solver(2, 6, 1.)
-solver.mesh().add_tree([cpp.new_copy(cpp.Nonpenetration()) for i in range(4)])
-solver.visualize_field_tecplot("hexed_out/new_mesh")
+## \endcond
--- samples/naca0012.py
+++ samples/naca0012.py
@@ -1,4 +1,8 @@
-import hexed_python
+import hexed
+
+solver = hexed.cpp.make_solver(2, 6, 1.)
+solver.mesh().add_tree([hexed.cpp.new_copy(hexed.cpp.Nonpenetration()) for i in range(4)])
+solver.visualize_field_tecplot("hexed_out/new_mesh")
 
 """
 solver = hexed.Solver(2, 6)
