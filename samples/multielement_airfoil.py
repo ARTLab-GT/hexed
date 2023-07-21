75c613cfb11c0516424c817795c08745ba261975
Micaiah Smith-Pierce <micaiah@gatech.edu>
2023-07-21 01:45:44 -0400

    new files

---
+++ samples/multielement_airfoil.py
@@ -0,0 +1,7 @@
+import hexed
+import os
+
+solver = hexed.create_solver(2, 6, [-10, -10], [10, 10], freestream = hexed.flow_state(altitude = 0, velocity = [0., 0.]),
+                             geometries = ["samples/crmhl-2dcut.igs"],
+                             final_resolution = "res_bad > 3e-3 && ref_level < 13", final_max_iters = 30)
+solver.visualize()
