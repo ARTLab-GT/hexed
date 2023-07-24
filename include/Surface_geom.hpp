1e37b36419dfea448ab98bf7161020be9fa4d4d6
Micaiah Smith-Pierce <micaiah@gatech.edu>
2023-07-24 13:10:12 -0400

    Revert "more declarations"
    This reverts commit d82680a975a80e2c0dbbfdee5a0f456db55b1e6f.

--- include/Surface_geom.hpp
+++ include/Surface_geom.hpp
@@ -9,7 +9,7 @@ namespace hexed
 
 /*! \brief Represents a surface geometry implicitly for meshing.
  * \details Abstract class which represents geometry by supporting
- * the operations `Surface_geom::nearest_point`/`Surface_geom::bounded_nearest` and `Surface_geom::intersections`.
+ * the operations `Surface_geom::nearest_point` and `Surface_geom::intersections`.
  * Either of these operations fully defines the surface geometry
  * and each could, in theory, be defined in terms of the other.
  * However, in practice they are defined separately
@@ -18,8 +18,6 @@ namespace hexed
  * that they are mutually consistent.
  * The derived class may impose dimensionality restrictions on the arguments.
  * Implementations must be thread-safe.
- * \note Depending on whether `nearest_point` or `bounded_nearest` is easier to implement efficiently,
- * consider subclassing `Bounded_search_geom` or `Unbounded_search_geom`.
  */
 class Surface_geom
 {
@@ -28,12 +26,15 @@ class Surface_geom
   /*! \brief Computes the point on the surface which is nearest to `point`.
    * \param distance_guess If you have some reason to suspect the nearest point is within a certain distance
    * of the input point, you can pass it to this parameter as a hint to improve performance.
+   * \see `bounded_nearest`
    */
-  virtual Mat<> nearest_point(Mat<> point, double distance_guess = std::numeric_limits<double>::max()) = 0;
+  virtual Mat<> nearest_point(Mat<> point, double distance_guess = std::numeric_limits<double>::max());
   /*! \brief Computes the nearest point, if any, within a certain radius.
    * \details If there is a point on the geometry within `max_distance` of `point`,
    * you will get it as a `math::Nearest_point` object.
    * If there is no such point, you will get an empty `math::Nearest_point`.
+   * \note `Surface_geom::nearest_point` calls this function with `distance_guess = max_distance`
+   * and iteratively doubles the radius until it gets a non-empty result.
    */
   virtual math::Nearest_point<dyn> bounded_nearest(Mat<> point, double max_distance) = 0;
   /*! \brief Computes the set of intersection points between a line and the surface.
@@ -42,34 +43,6 @@ class Surface_geom
    * Returns the (potentially empty) set of \f$ t \f$ values where the line intersects the surface.
    */
   virtual std::vector<double> intersections(Mat<> point0, Mat<> point1) = 0;
-};
-
-/*! \brief A `Surface_geom` where `nearest_point` is implemented in terms of `bounded_nearest`.
- * \details If you want to make a `Surface_geom` subclass
- * where searching within a finite radius is inherently easier to implement efficiently,
- * this class can provide a convenient and reasonably fast generalization to unbounded searches.
- */
-class Bounded_search_geom
-{
-  public:
-  /*! \details Calls `bounded_nearest(point, distance_guess)` and if no point is found,
-   * iteratively doubles the search radius until it finds a point.
-   * If the speed of your `bounded_nearest` implementation is strongly dependent on the search radius,
-   * this should be a pretty fast way to compute `nearest_point`.
-   */
-  Mat<> nearest_point(Mat<> point, double distance_guess = std::numeric_limits<double>::max()) override;
-};
-
-/*! \brief A `Surface_geom` where `bounded_nearest` is implemented in terms of `nearest_point`.
- * \details If you want to make a `Surface_geom` subclass
- * where there is no particular benefit to searching within a finite radius,
- * this class can provide a convenient generalization to bounded searches.
- */
-class Unbounded_search_geom
-{
-  public:
-  //! \details calls `nearest_point(point, max_distance)` and returns an empty point if it is too far away.
-  math::Nearest_point<dyn> bounded_nearest(Mat<> point, double max_distance) override;
 };
 
 /*! \brief Combines multiple `Surface_geom`s into one.
