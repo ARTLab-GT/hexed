c0cf1a52de516bfa8c77a9d9cc2cdc253c2be90a
Micaiah Smith-Pierce <micaiah@gatech.edu>
2023-07-27 14:21:13 -0400

    script updates

--- samples/wing_store.py
+++ samples/wing_store.py
@@ -8,8 +8,8 @@ solver = hexed.create_solver(3, 3, [-.1, 0., -.3], [.5, .6, .3], freestream = he
 """
 solver = hexed.create_solver(3, 5, [-.1, 0., -.3], [.5, .6, .3], freestream = hexed.flow_state(altitude = 0., velocity = [0., 0., 0.]),
                              geom = hexed.make_geom("samples/wing_store.IGS", n_dim = 3),
-                             final_resolution = "res_bad > 1e-2 && ref_level < 7", final_max_iters = 6,
-                             n_smooth = 10)
+                             final_resolution = "res_bad > 1e-2 && ref_level < 10", final_max_iters = 10,
+                             n_smooth = 15)
 print("visualizing")
 solver.visualize_surface_tecplot(solver.mesh().surface_bc_sn(), "hexed_out/mesh", 10)
 print("Geometry processing performance: " + hexed.cpp.Simplex_geom[3].performance_report())
