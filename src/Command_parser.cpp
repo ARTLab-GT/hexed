3c1d00a3463eb589c4abdd870381f262d58846b9
micaiah <micaiah@smithpierce.net>
2023-07-29 04:24:49 -0400

    multiple assignments and doubles

--- src/Command_parser.cpp
+++ src/Command_parser.cpp
@@ -11,7 +11,9 @@ void Command_parser::exec(std::string commands)
   auto more = [&](){return int(commands.size()) > place;};
   auto skip_spaces = [&](){while (commands[place] == ' ') ++place;}; // note: commands[commands.size()] exists and is a null character so this is safe
   while (more()) {
-    if (std::isalpha(commands[place]) || commands[place] == '_') {
+    skip_spaces();
+    if (commands[place] == '\n') ++place;
+    else if (std::isalpha(commands[place]) || commands[place] == '_') {
       std::string name = "";
       while (std::isalpha(commands[place]) || std::isdigit(commands[place])  || commands[place] == '_') {
         name.push_back(commands[place++]);
@@ -19,12 +21,22 @@ void Command_parser::exec(std::string commands)
       skip_spaces();
       HEXED_ASSERT(commands[place++] == '=', "expected assignment operator `=` after variable name");
       skip_spaces();
-      std::string value;
-      while (std::isdigit(commands[place])) value.push_back(commands[place++]);
-      variables->assign(name, std::atoi(value.c_str()));
+      HEXED_ASSERT(more(), "unexpected end of line in assignment statement");
+      if (std::isdigit(commands[place]) || commands[place] == '.') {
+        std::string value;
+        bool is_int = true;
+        while (std::isdigit(commands[place]) || commands[place] == '.' || std::tolower(commands[place]) == 'e') {
+          is_int = is_int && std::isdigit(commands[place]);
+          value.push_back(commands[place++]);
+        }
+        if (is_int) variables->assign(name, std::stoi(value.c_str()));
+        else        variables->assign(name, std::stod(value.c_str()));
+      } else {
+        HEXED_ASSERT(false, format_str(100, "failed to parse value starting with `%c`", commands[place]));
+      }
+      skip_spaces();
       HEXED_ASSERT(!more() || commands[place] == '\n', "expected end of line after assignment statement");
     }
-    break;
   }
 }
 
--- test/test_Command_parser.cpp
+++ test/test_Command_parser.cpp
@@ -6,6 +6,12 @@
 TEST_CASE("Command_parser")
 {
   hexed::Command_parser parser;
-  parser.exec("shock_wave = 7");
+  parser.exec("shock_wave = 7\n  boundary0layer=14 \n\ninteraction = 1.2");
   REQUIRE(parser.variables->lookup<int>("shock_wave").value() == 7);
+  REQUIRE(parser.variables->lookup<int>("boundary0layer").value() == 14);
+  REQUIRE(parser.variables->lookup<double>("interaction").value() == Catch::Approx(1.2));
+  parser.exec("interaction = .2");
+  REQUIRE(parser.variables->lookup<double>("interaction").value() == Catch::Approx(.2));
+  parser.exec("interaction = 1e3");
+  REQUIRE(parser.variables->lookup<double>("interaction").value() == Catch::Approx(1e3));
 }
