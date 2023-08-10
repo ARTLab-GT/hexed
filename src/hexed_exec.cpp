1ce5ee1919a75ee0306e584467a766b3fc3a6fca
micaiah <micaiah@smithpierce.net>
2023-08-10 03:00:45 -0400

    renamed executable

--- src/hexed_exec.cpp
+++
@@ -1,7 +0,0 @@
-#include <hexed/Case.hpp>
-
-int main(int argc, char* argv[])
-{
-  if (argc != 2) throw std::runtime_error("`hexed_exec` takes exactly one argument, namely the path to the input file");
-  hexed::Case c(argv[1]);
-}
