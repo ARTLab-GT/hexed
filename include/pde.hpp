22c4b95613a5f46a49ffb908dc530af4423298b8
micaiah <micaiah@smithpierce.net>
2023-08-10 15:40:40 -0400

    Revert "constant normalization (no good)"
    This reverts commit a4fc25b83d48edc553f9390dab1228be5250f26c.

--- include/pde.hpp
+++ include/pde.hpp
@@ -262,12 +262,12 @@ class Advection
     Mat<1, 2> vol_flux = normal.transpose()*face_vars(Eigen::seqN(0, n_dim), Eigen::all);
     Mat<1, 2> face_state = face_vars(n_dim, Eigen::all);
     Mat<1, 2> face_flux = face_state.cwiseProduct(vol_flux);
-    return face_flux*Mat<2>{.5, .5} + face_state*Mat<2>{.5, -.5}*normal.norm()*std::max(1., face_vars(Eigen::seqN(0, n_dim), all).norm());
+    return face_flux*Mat<2>{.5, .5} + face_state*Mat<2>{.5, -.5}*normal.norm();
   }
 
   double char_speed(Mat<n_var> state) const
   {
-    return std::max(1., state(Eigen::seqN(0, n_dim)).norm());
+    return 1.;
   }
 };
 
