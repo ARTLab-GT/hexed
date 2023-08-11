ac9c6275420f5fcffa5c861d2ccb38449e59f917
micaiah <micaiah@smithpierce.net>
2023-08-10 22:41:28 -0400

    renamed autogenerated geometry

--- samples/cylinder/make_geom.py
+++ samples/cylinder/make_geom.py
@@ -2,4 +2,4 @@ import numpy as np
 
 angle = np.linspace(0, 2*np.pi, 10**4)
 coords = np.array([np.sin(angle), np.cos(angle)]).transpose()
-np.savetxt("geom.csv", coords, delimiter=", ")
+np.savetxt("auto.csv", coords, delimiter=", ")
