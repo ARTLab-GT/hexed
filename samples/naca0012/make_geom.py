355b7c299b46c2c5d062c4cc2e8fa9c5b922e06d
Micaiah Smith-Pierce <micaiah@gatech.edu>
2023-09-01 13:49:03 -0400

    updated NACA0012 case

--- samples/naca0012/make_geom.py
+++ samples/naca0012/make_geom.py
@@ -1,3 +1,9 @@
+"""
+Subsonic NACA 0012 Example Problem: Geometry generation script.
+Creates a file `auto.csv` with airfoil coordinates.
+This script is executed by the input file `run.hil`.
+"""
+
 import numpy as np
 from hexed_utils import naca
 
