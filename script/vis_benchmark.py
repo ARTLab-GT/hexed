6e42d3cdf3dd77753e83fd22b6b750d966750ba0
Micaiah Smith-Pierce <micaiah@gatech.edu>
2023-09-06 15:56:13 -0400

    acquire timing data

--- script/vis_benchmark.py
+++ script/vis_benchmark.py
@@ -5,11 +5,29 @@ file_name = sys.argv[1]
 with open(file_name, "r") as in_file:
     text = in_file.read()
 
+class Timing_data:
+    def __init__(self, lines):
+        line = lines.pop(0)
+        stripped = line.lstrip(" ")
+        self.n_spaces = len(line) - len(stripped)
+        self.name, rest = stripped.split(": ")
+        unit, rest = rest.split(" completed in ")
+        self.n_units = unit.split(" ")[0]
+        self.unit = unit[len(self.n_units) + 1:-1]
+        self.n_units = int(self.n_units)
+        self.time = float(rest.split(" s")[0])
+        print(self.n_spaces, self.name)
+        self.children = []
+        while lines and len(lines[0]) - len(lines[0].lstrip(" ")) > self.n_spaces:
+            self.children.append(Timing_data(lines))
+
 class Benchmark:
     def __init__(self, t):
         split = t.split("output:")
         self.context = split[0]
         self.output = split[1]
+        lines = ("iteration: " + self.output.split("performance summary:\n")[1].split("\n\n")[0]).split("\n")
+        self.timing = Timing_data(lines)
 
     def _get_attr(self, name, transform = lambda x: x):
         return transform(self.context.split(name + ": ")[1].split("\n")[0])
@@ -26,4 +44,3 @@ class Benchmark:
     def elapsed_time(self): return self._get_attr("elapsed execution time", lambda s: float(s.split(" ")[0]))
 
 benchmarks = [Benchmark(t) for t in text.split("execution context:\n") if "output:" in t]
-print(benchmarks[0].commit)
