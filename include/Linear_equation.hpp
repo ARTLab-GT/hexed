68822a50d647d6af2237e6f96b32f6df685107b4
micaiah <micaiah@smithpierce.net>
2023-10-20 18:54:54 -0400

    linear equation class

---
+++ include/Linear_equation.hpp
@@ -0,0 +1,35 @@
+#ifndef HEXED_LINEAR_SYSTEM_HPP_
+#define HEXED_LINEAR_SYSTEM_HPP_
+
+#include "math.hpp"
+
+namespace hexed
+{
+
+class Linear_equation
+{
+  public:
+  virtual int n_vecs() = 0;
+  virtual void scale(int output, int input, double scalar) = 0;
+  virtual void add(int output, double coef0, int vec0, double coef1, int vec1) = 0;
+  virtual double inner(int input0, int input1) = 0;
+  virtual void matvec(int output, int input) = 0;
+};
+
+class Dense_equation : public Linear_equation
+{
+  Mat<dyn, dyn> _mat;
+  Mat<dyn, dyn> _vecs;
+  public:
+  Dense_equation(Mat<dyn, dyn> matrix, Mat<> rhs, int nv);
+  Dense_equation(Mat<dyn, dyn> matrix, Mat<> rhs, int nv, Mat<> guess);
+  inline auto vec(int i) {return _vecs(all, i);}
+  int n_vecs() override;
+  void scale(int output, int input, double scalar) override;
+  void add(int output, double coef0, int vec0, double coef1, int vec1) override;
+  double inner(int input0, int input1) override;
+  void matvec(int output, int input) override;
+};
+
+}
+#endif
---
+++ src/Linear_equation.cpp
@@ -0,0 +1,42 @@
+#include <Linear_equation.hpp>
+
+namespace hexed
+{
+
+Dense_equation::Dense_equation(Mat<dyn, dyn> matrix, Mat<> rhs, int nv, Mat<> guess)
+: _mat{matrix}, _vecs{Mat<dyn, dyn>::Zero(matrix.cols(), nv)}
+{
+  HEXED_ASSERT(matrix.rows() == matrix.cols() && matrix.rows() == rhs.size() && matrix.rows() == guess.size(),
+               "size of input matrices and vectors must match");
+  HEXED_ASSERT(nv >= 2, "must at least provide storage for initial guess and residual");
+  _vecs(all, 0) = guess;
+  _vecs(all, 1) = rhs - matrix*guess;
+}
+
+Dense_equation::Dense_equation(Mat<dyn, dyn> matrix, Mat<> rhs, int nv)
+: Dense_equation(matrix, rhs, nv, Mat<>::Zero(rhs.size()))
+{}
+
+int Dense_equation::n_vecs() {return _vecs.cols();}
+
+void Dense_equation::scale(int output, int input, double scalar)
+{
+  vec(output) = scalar*vec(input);
+}
+
+void Dense_equation::add(int output, double coef0, int vec0, double coef1, int vec1)
+{
+  vec(output) = coef0*vec(vec0) + coef1*vec(vec1);
+}
+
+double Dense_equation::inner(int input0, int input1)
+{
+  return vec(input0).dot(vec(input1));
+}
+
+void Dense_equation::matvec(int output, int input)
+{
+  vec(output) = _mat*vec(input);
+}
+
+}
