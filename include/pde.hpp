d16875368cc92957ad89805085dce803ad36b245
Micaiah Smith-Pierce <micaiah@gatech.edu>
2023-10-26 13:18:24 -0400

    interface art visc is always nonnegative

--- include/pde.hpp
+++ include/pde.hpp
@@ -121,7 +121,7 @@ class Navier_stokes
       double sqrt_temp = std::sqrt(std::max(state(n_dim + 1)/mass - .5*veloc.squaredNorm(), 0.)*(heat_rat - 1)/constants::specific_gas_air);
       double nat_visc = dyn_visc.coefficient(sqrt_temp);
       Mat<n_dim, n_dim> stress = nat_visc*(veloc_grad + veloc_grad.transpose())
-                                 + (av_coef*mass - 2./3.*nat_visc)*veloc_grad.trace()*Mat<n_dim, n_dim>::Identity();
+                                 + (std::abs(av_coef)*mass - 2./3.*nat_visc)*veloc_grad.trace()*Mat<n_dim, n_dim>::Identity();
       Mat<n_dim, n_update> flux;
       flux(all, seq) = -stress;
       flux(all, n_dim).setZero();
@@ -145,7 +145,7 @@ class Navier_stokes
       double mass = state(n_dim);
       auto veloc = state(Eigen::seqN(0, n_dim))/mass;
       double sqrt_temp = std::sqrt(std::max(state(n_dim + 1)/mass - .5*veloc.squaredNorm(), 0.)*(heat_rat - 1)/constants::specific_gas_air);
-      return std::max(av_coef + dyn_visc.coefficient(sqrt_temp)/mass, therm_cond.coefficient(sqrt_temp)*(heat_rat - 1)/constants::specific_gas_air/mass);
+      return std::max(std::abs(av_coef) + dyn_visc.coefficient(sqrt_temp)/mass, therm_cond.coefficient(sqrt_temp)*(heat_rat - 1)/constants::specific_gas_air/mass);
     }
 
     /*!
