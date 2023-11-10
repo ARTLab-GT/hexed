62c8f22c31ced1b13eb0e3654d228cdd2f484fda
Micaiah Smith-Pierce <micaiah@gatech.edu>
2023-11-10 02:18:05 -0500

    attempted to match faces (doesn't quite work)

--- src/Solver.cpp
+++ src/Solver.cpp
@@ -498,6 +498,101 @@ void Solver::snap_faces()
   }
   }
   #endif
+
+  #pragma omp parallel for
+  for (int i_elem = 0; i_elem < elems.size(); ++i_elem) {
+    double* state = elems[i_elem].stage(0);
+    for (int i_dof = 0; i_dof < params.n_dof(); ++i_dof) state[i_dof] = 0;
+  }
+  #pragma omp parallel for
+  for (int i_con = 0; i_con < bc_cons.size(); ++i_con) {
+    auto& con = bc_cons[i_con];
+    Lock::Acquire acq(con.element().lock);
+    int bc_sn = con.bound_cond_serial_n();
+    if (bc_sn == acc_mesh.surface_bc_sn()) {
+      double* state = con.element().stage(0);
+      double* node_adj = con.element().node_adjustments();
+      if (node_adj) {
+        node_adj += (2*con.i_dim() + con.inside_face_sign())*params.n_qpoint()/params.row_size;
+        for (Row_index ind(params.n_dim, params.row_size, con.i_dim()); ind; ++ind) {
+          for (int i_node = 0; i_node < params.row_size; ++i_node) {
+            state[ind.i_qpoint(i_node)] = math::sign(con.inside_face_sign())*node_adj[ind.i_face_qpoint()];
+          }
+        }
+      }
+    }
+  }
+  (*write_face)(elems);
+  (*kernel_factory<Prolong_refined>(params.n_dim, params.row_size, basis))(acc_mesh.refined_faces());
+  Copy fake_bc;
+  #pragma omp parallel for
+  for (int i_con = 0; i_con < bc_cons.size(); ++i_con) {
+    auto& con = bc_cons[i_con];
+    Lock::Acquire acq(con.element().lock);
+    fake_bc.apply_state(con);
+  }
+  (*kernel_factory<Spatial<Deformed_element, pde::Smooth_art_visc>::Neighbor>(params.n_dim, params.row_size))(acc_mesh.deformed().face_connections());
+  if (nd == 3) {
+    (*kernel_factory<Restrict_refined>(params.n_dim, params.row_size, basis, false, true))(acc_mesh.refined_faces());
+    (*kernel_factory<Prolong_refined>(params.n_dim, params.row_size, basis, false, true))(acc_mesh.refined_faces());
+    auto& cons = acc_mesh.deformed().face_connections();
+    #pragma omp parallel for
+    for (int i_con = 0; i_con < cons.size(); ++i_con) {
+      double* state = cons[i_con].state();
+      for (int i_qpoint = 0; i_qpoint < nfq; ++i_qpoint) {
+        for (int i_side = 0; i_side < 2; ++i_side) {
+          state[i_side*nfq*params.n_var + i_qpoint] = state[(i_side + 2)*nfq*params.n_var + i_qpoint];
+        }
+      }
+    }
+    auto& ref_cons = acc_mesh.deformed().refined_connections();
+    #pragma omp parallel for
+    for (int i_con = 0; i_con < ref_cons.size(); ++i_con) {
+      for (int i_fine = 0; i_fine < 2; ++i_fine) {
+        double* state = ref_cons[i_con].connection(i_fine).state();
+        for (int i_qpoint = 0; i_qpoint < nfq; ++i_qpoint) {
+          state[i_qpoint] *= 2;
+          state[nfq*params.n_var + i_qpoint] = 0;
+        }
+      }
+    }
+    (*kernel_factory<Spatial<Deformed_element, pde::Smooth_art_visc>::Neighbor>(params.n_dim, params.row_size))(acc_mesh.deformed().face_connections());
+  }
+  (*kernel_factory<Restrict_refined>(params.n_dim, params.row_size, basis, false, true))(acc_mesh.refined_faces());
+  #pragma omp parallel for
+  for (int i_con = 0; i_con < bc_cons.size(); ++i_con) {
+    auto& con = bc_cons[i_con];
+    Lock::Acquire acq(con.element().lock);
+    int bc_sn = con.bound_cond_serial_n();
+    if (bc_sn == acc_mesh.surface_bc_sn()) {
+      double* state = con.element().stage(0);
+      double* node_adj = con.element().node_adjustments();
+      if (node_adj) {
+        node_adj += (2*con.i_dim() + con.inside_face_sign())*params.n_qpoint()/params.row_size;
+        for (int j_dim = 0; j_dim < params.n_dim; ++j_dim) if (j_dim != con.i_dim()) {
+          for (Row_index ind(params.n_dim, params.row_size, j_dim); ind; ++ind) {
+            Mat<> row(params.row_size);
+            for (int i_node = 0; i_node < params.row_size; ++i_node) {
+              row(i_node) = state[ind.i_qpoint(i_node)];
+            }
+            Mat<> lrow = to_lob*row;
+            for (int i_sign = 0; i_sign < 2; ++i_sign) {
+              lrow(i_sign*(lob.row_size - 1)) = con.element().faces[2*j_dim + i_sign][2*params.n_var*params.n_qpoint()/params.row_size + ind.i_face_qpoint()];
+            }
+            row = from_lob*lrow;
+            for (int i_node = 0; i_node < params.row_size; ++i_node) {
+              state[ind.i_qpoint(i_node)] = row(i_node);
+            }
+          }
+        }
+        for (Row_index ind(params.n_dim, params.row_size, con.i_dim()); ind; ++ind) {
+          double interp = 1.;
+          node_adj[ind.i_face_qpoint()] *= (1 - interp);
+          node_adj[ind.i_face_qpoint()] += interp*math::sign(con.inside_face_sign())*state[ind.i_qpoint(0)];
+        }
+      }
+    }
+  }
 }
 
 void Solver::calc_jacobian()
