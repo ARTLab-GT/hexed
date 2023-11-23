31ab0c5ac005d282c063ca1661329abcdfd87665
micaiah <micaiah@smithpierce.net>
2023-11-23 12:28:28 -0500

    successful heat transfer coef calculation

--- samples/cylinder/compare1.py
+++ samples/cylinder/compare1.py
@@ -2,16 +2,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-exper = np.loadtxt("experiment.csv", delimiter = ",")
-#plt.scatter(exper[:, 0], exper[:, 1])
+max_angle = 80
 exper = np.loadtxt("tewfik.csv", delimiter = ",")
-plt.scatter(exper[:, 0], exper[:, 1]/exper[0, 1])
-analy = np.loadtxt("analytic.csv", delimiter = ",")
-#plt.plot(analy[:, 0], analy[:, 1], linestyle = "--", color = "k")
-for suffix in ["8"]:
-    sim = pd.read_csv(f"hexed_out_isothermal{suffix}/simulation.csv")
-    sim.sort_values("angle", inplace = True)
-    max_angle = 80
-    sim = sim.loc[sim["angle"] < max_angle*np.pi/180]
-    plt.plot(sim["angle"]*180/np.pi, sim["heat_flux"]/sim["heat_flux"][0])
+exper = exper[exper[:, 0] < max_angle + 3]
+plt.scatter(exper[:, 0], exper[:, 1]/exper[0, 1], label = "experiment")
+sim = pd.read_csv(f"hexed_out_isothermal8/simulation.csv")
+sim.sort_values("angle", inplace = True)
+sim = sim.loc[sim["angle"] < max_angle*np.pi/180]
+ad = pd.read_csv(f"hexed_out_ad8/simulation.csv")
+ad.sort_values("angle", inplace = True)
+ad = ad.loc[ad["angle"] < max_angle*np.pi/180]
+htc = np.array(sim["heat_flux"])/(np.array(ad["temperature"]) - np.array(sim["temperature"]))
+plt.plot(sim["angle"]*180/np.pi, htc/htc[0], label = "simulation")
+plt.xlabel(r"$\theta$ ($\degree$)")
+plt.ylabel(r"$c_h(\theta)/c_h(0)$")
+plt.grid(True)
+plt.legend()
 plt.show()
