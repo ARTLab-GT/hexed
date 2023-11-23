90b35e23016100ff79c800b767ccbb70ee86db4d
micaiah <micaiah@smithpierce.net>
2023-11-22 21:00:56 -0500

    replicated tewfik

--- samples/cylinder/compare1.py
+++ samples/cylinder/compare1.py
@@ -3,10 +3,12 @@ import numpy as np
 import pandas as pd
 
 exper = np.loadtxt("experiment.csv", delimiter = ",")
-plt.scatter(exper[:, 0], exper[:, 1])
+#plt.scatter(exper[:, 0], exper[:, 1])
+exper = np.loadtxt("tewfik.csv", delimiter = ",")
+plt.scatter(exper[:, 0], exper[:, 1]/exper[0, 1])
 analy = np.loadtxt("analytic.csv", delimiter = ",")
-plt.plot(analy[:, 0], analy[:, 1], linestyle = "--", color = "k")
-for suffix in ["", "2", "4", "6", "7"]:
+#plt.plot(analy[:, 0], analy[:, 1], linestyle = "--", color = "k")
+for suffix in ["8"]:
     sim = pd.read_csv(f"hexed_out_isothermal{suffix}/simulation.csv")
     sim.sort_values("angle", inplace = True)
     max_angle = 80
