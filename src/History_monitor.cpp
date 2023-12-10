57b03071ae8c6c834841883bef1b8ae75c491279
micaiah <micaiah@smithpierce.net>
2023-12-09 20:43:32 -0500

    declarations for `History_monitor`

---
+++ src/History_monitor.cpp
@@ -0,0 +1,23 @@
+#include <History_monitor.hpp>
+
+namespace hexed
+{
+
+History_monitor::History_monitor(double window_size, int max_samples)
+{}
+
+void History_monitor::add_sample(int iteration, double value)
+{
+}
+
+double History_monitor::max()
+{
+  return 0.;
+}
+
+double History_monitor::min()
+{
+  return 0.;
+}
+
+}
---
+++ test/test_History_monitor.cpp
@@ -0,0 +1,6 @@
+#include <catch2/catch_all.hpp>
+#include <hexed/History_monitor.hpp>
+
+TEST_CASE("History_monitor")
+{
+}
