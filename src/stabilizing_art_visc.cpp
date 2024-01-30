d42fe3364d7217ecf7654c46130d7bfdef330312
Micaiah Smith-Pierce <micaiah@gatech.edu>
2024-01-30 13:18:16 -0500

    Revert "included pressure"
    This reverts commit b89b254f7411218032355167539cba57bc9f7ac5.

--- src/stabilizing_art_visc.cpp
+++ src/stabilizing_art_visc.cpp
@@ -33,28 +33,21 @@ class Stab_art_visc : public Kernel<Kernel_element&>
     for (int i_elem = 0; i_elem < elements.size(); ++i_elem)
     {
       double* state = elements[i_elem].state();
-      double indicator_vars [2][n_qpoint];
-      double norm_sq [2] {};
+      double indicator_var [n_qpoint];
+      double norm_sq = 0;
       for (int i_qpoint = 0; i_qpoint < n_qpoint; ++i_qpoint) {
-        indicator_vars[0][i_qpoint] = 1./state[n_dim*n_qpoint + i_qpoint];
-        double mmtm_sq = 0;
-        for (int i_dim = 0; i_dim < n_dim; ++i_dim) mmtm_sq += math::pow(state[i_dim*n_qpoint + i_qpoint], 2);
-        indicator_vars[1][i_qpoint] = 1./(state[(n_dim + 1)*n_qpoint + i_qpoint] - .5*mmtm_sq*indicator_vars[0][i_qpoint]);
-        for (int i_var = 0; i_var < 2; ++i_var) {
-          norm_sq[i_var] += math::pow(indicator_vars[i_var][i_qpoint], 2)*_qpoint_weights(i_qpoint);
-        }
+        indicator_var[i_qpoint] = 1./state[n_dim*n_qpoint + i_qpoint];
+        norm_sq += indicator_var[i_qpoint]*indicator_var[i_qpoint]*_qpoint_weights(i_qpoint);
       }
-      Mat<2> nonsmooth = Mat<2>::Zero();
+      double nonsmooth = 0;
       for (int i_dim = 0; i_dim < n_dim; ++i_dim) {
         for (Row_index ind(n_dim, row_size, i_dim); ind; ++ind) {
-          Mat<row_size, 2> row = Row_rw<2, row_size>::read_row(indicator_vars[0], ind);
-          Mat<2> dot = row.transpose()*_proj;
-          nonsmooth += dot.cwiseProduct(dot)*_face_weights(ind.i_face_qpoint());
+          Mat<row_size> row = Row_rw<1, row_size>::read_row(indicator_var, ind);
+          nonsmooth += math::pow(row.dot(_proj), 2)*_face_weights(ind.i_face_qpoint());
         }
       }
-      double indicator = 0;
-      for (int i_var = 0; i_var < 2; ++i_var) indicator += nonsmooth(i_var)/(norm_sq[i_var]*n_dim);
-      indicator = std::log(indicator)/std::log(10);
+      nonsmooth /= norm_sq*n_dim;
+      double indicator = std::log(nonsmooth)/std::log(10);
       if (indicator <= _ramp_center - _half_width) indicator = 0;
       else if (indicator < _ramp_center + _half_width) indicator = .5*(1 + std::sin(constants::pi*(indicator - _ramp_center)/2/_half_width));
       else indicator = 1;
