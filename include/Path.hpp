89ce63e27a0d91abfe58c305c6e56a6ba93a61e7
micaiah <micaiah@gatech.edu>
2024-03-31 16:08:09 -0400

    updated docs

--- include/Path.hpp
+++ include/Path.hpp
@@ -22,18 +22,18 @@ class Path
   std::vector<std::filesystem::path> _paths;
 
   public:
-  using path = std::filesystem::path;
-  //! \brief constructs a `Path` object with `subdir` appended to all search paths
-  //! \details E.g., if the file your looking for is a shared library, you probably want to do `subdir = "lib"`
+  using path = std::filesystem::path; //!< \brief convenience alias for `std::filesystem::path`
+  //! \brief Constructs a `Path` object with `subdir` appended to all search paths.
+  //! \details E.g., if the file your looking for is a shared library, you probably want to do `subdir = "lib"`.
   Path(path subdir = {});
-  //! \brief returns the list of system paths to search, including the `subdir` suffix
+  //! \brief Returns the list of system paths to search, including the `subdir` suffix.
   inline std::vector<path> paths() {return _paths;}
   /*! \brief finds the file `target`
    * \details If `target` is a relative path, it will be searched for in the following directories are searched,
    * in order of precedence:
    * - All directories in `extra_dirs`, which defaults to the current working directory
    * - All directories in the environment variable `HEXEDPATH`, which should be a colon-separated list, if it is set
-   * - All the system paths, defined above.
+   * - All the system paths, defined above
    *
    * If `target` is not found, the return value is an empty path.
    * If `target` is an absolute path, it will be returned if it exists and the empty path will be returned otherwise.
