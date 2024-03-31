916b50131a20953b1f2c924899146330ed2b40d8
micaiah <micaiah@gatech.edu>
2024-03-31 15:25:30 -0400

    eliminated dependency on hardcoded root path

--- test/test_read_csv.cpp
+++ test/test_read_csv.cpp
@@ -1,9 +1,13 @@
+#include <fstream>
 #include <catch2/catch_all.hpp>
 #include <hexed/read_csv.hpp>
 
 TEST_CASE("read_csv")
 {
-  auto data = hexed::read_csv(hexed::config::root_dir + std::string("/test/test_csv.csv"));
+  std::ofstream csv("test_csv.csv");
+  csv << "0.1, 1e3,\t10\n  -.3   ,4,1.\n";
+  csv.close();
+  auto data = hexed::read_csv("test_csv.csv");
   REQUIRE(data.rows() == 2);
   REQUIRE(data.cols() == 3);
   Eigen::MatrixXd correct(2, 3);
