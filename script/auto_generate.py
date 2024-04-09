53935539df507f546ea8f76eaa4d419a085f841d
micaiah <micaiah@gatech.edu>
2024-04-09 01:29:43 -0400

    auto_generate searches source tree first

--- script/auto_generate.py
+++ script/auto_generate.py
@@ -52,7 +52,7 @@ namespace hexed
 
 ### Calculate bases ###
 
-sys.path.append("../python")
+sys.path.insert(0, "../python")
 from hexed_utils import Basis
 from sympy.integrals.quadrature import gauss_legendre, gauss_lobatto
 
