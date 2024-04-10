2042dab2ce1d9670ad5bebf11d4f4b6a232d7416
micaiah <micaiah@gatech.edu>
2024-04-10 13:37:40 -0400

    fixed index out of bounds

--- src/Element_new.cpp
+++ src/Element_new.cpp
@@ -12,9 +12,10 @@ Element_new::Element_new(Storage_params params, bool def, int ref_level, Array<i
   _origin{og},
   _basis{b},
   _vertices(std::vector<int>(_params.n_dim, 2), [this](int i) {
-    Mat<> pos = nominal_position().vector();
+    Mat<> pos = Mat<>::Zero(3);
+    auto np = nominal_position();
     for (int i_dim = 0; i_dim < _params.n_dim; ++i_dim) {
-      pos(i_dim) += nominal_size()*((i/math::pow(2, _params.n_dim - 1 - i_dim))%2);
+      pos(i_dim) = np[i_dim] + nominal_size()*((i/math::pow(2, _params.n_dim - 1 - i_dim))%2);
     }
     return Vertex::Transferable_ptr(pos, deformed());
   }),
