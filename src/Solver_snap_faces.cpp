caf8fc2bdca9c02a2e074f0e3e87b37d9a66afee
micaiah <micaiah@gatech.edu>
2024-04-21 17:41:24 -0400

    Revert "cleaned out old version"
    This reverts commit f2b847d474c6ee42223ad6a8d65c57bdd89130ee.

--- src/Solver_snap_faces.cpp
+++ src/Solver_snap_faces.cpp
@@ -1,4 +1,7 @@
 #include <Solver.hpp>
+#include <Spatial.hpp>
+#include <pde.hpp>
+#include <kernels.hpp>
 #include <Gauss_lobatto.hpp>
 
 // this gets its own file cause it's its own special brand of scuffed
@@ -18,19 +21,97 @@ void hexed::Solver::snap_faces()
   Gauss_lobatto lob(std::max(2, basis.row_size - 1));
   Mat<dyn, dyn> to_lob = basis.interpolate(lob.nodes());
   Mat<dyn, dyn> from_lob = lob.interpolate(basis.nodes());
-  auto apply_mat = [&](Mat<dyn, dyn> mat) {
-    auto ext_cons = acc_mesh->extruded_connections();
+  #pragma omp parallel for
+  for (int i_elem = 0; i_elem < elems.size(); ++i_elem) {
+    double* state = elems[i_elem].state();
+    double* rk_ref = elems[i_elem].residual_cache();
+    for (int i_dof = 0; i_dof < params.n_dof(); ++i_dof) rk_ref[i_dof] = state[i_dof];
+  }
+  for (int sweep = 0; sweep < nd - 1; ++sweep) {
     #pragma omp parallel for
-    for (int i_con = 0; i_con < ext_cons.size(); ++i_con) {
-      auto& con = ext_cons[i_con];
-      auto& elem = con.element(0);
-      HEXED_ASSERT(!elem.tree, "oops this was supposed to be extruded");
-      auto dir = con.get_direction();
-      Eigen::Map<Mat<>> node_adj(elem.node_adjustments() + (2*dir.i_dim[0] + dir.face_sign[0])*nfq, nfq);
-      node_adj = math::hypercube_matvec(mat, node_adj);
+    for (int i_elem = 0; i_elem < elems.size(); ++i_elem) {
+      double* state = elems[i_elem].state();
+      for (int i_dof = 0; i_dof < params.n_dof(); ++i_dof) state[i_dof] = 0;
     }
-  };
-  apply_mat(basis.interpolate(lob.nodes()));
-  apply_mat(lob.interpolate(basis.nodes()));
+    #pragma omp parallel for
+    for (int i_con = 0; i_con < bc_cons.size(); ++i_con) {
+      auto& con = bc_cons[i_con];
+      Lock::Acquire acq(con.element().lock);
+      int bc_sn = con.bound_cond_serial_n();
+      if (bc_sn == acc_mesh->surface_bc_sn()) {
+        double* state = con.element().state();
+        double* node_adj = con.element().node_adjustments();
+        if (node_adj) {
+          node_adj += (2*con.i_dim() + con.inside_face_sign())*params.n_qpoint()/params.row_size;
+          for (Row_index ind(params.n_dim, params.row_size, con.i_dim()); ind; ++ind) {
+            for (int i_node = 0; i_node < params.row_size; ++i_node) {
+              state[ind.i_qpoint(i_node)] = math::sign(con.inside_face_sign())*node_adj[ind.i_face_qpoint()];
+            }
+          }
+        }
+      }
+    }
+    compute_write_face(_kernel_mesh());
+    compute_prolong(_kernel_mesh());
+    Copy fake_bc;
+    #pragma omp parallel for
+    for (int i_con = 0; i_con < bc_cons.size(); ++i_con) {
+      auto& con = bc_cons[i_con];
+      Lock::Acquire acq(con.element().lock);
+      fake_bc.apply_state(con);
+    }
+    if (sweep) {
+      auto& ref_cons = acc_mesh->deformed().refined_connections();
+      #pragma omp parallel for
+      for (int i_con = 0; i_con < ref_cons.size(); ++i_con) {
+        for (int i_fine = 0; i_fine < 2; ++i_fine) {
+          double* state = ref_cons[i_con].connection(i_fine).state(0, false);
+          for (int i_qpoint = 0; i_qpoint < nfq; ++i_qpoint) state[i_qpoint] *= 2;
+          state = ref_cons[i_con].connection(i_fine).state(1, false);
+          for (int i_qpoint = 0; i_qpoint < nfq; ++i_qpoint) state[nfq*params.n_var + i_qpoint] = 0;
+        }
+      }
+    }
+    (*kernel_factory<Spatial<pde::Smooth_art_visc, false>::Neighbor>(params.n_dim, params.row_size, 0, 0, 1., 1.))(acc_mesh->deformed().kernel_connections());
+    compute_restrict(_kernel_mesh(), false, true);
+    #pragma omp parallel for
+    for (int i_con = 0; i_con < bc_cons.size(); ++i_con) {
+      auto& con = bc_cons[i_con];
+      Lock::Acquire acq(con.element().lock);
+      int bc_sn = con.bound_cond_serial_n();
+      if (bc_sn == acc_mesh->surface_bc_sn()) {
+        double* state = con.element().state();
+        double* node_adj = con.element().node_adjustments();
+        if (node_adj) {
+          node_adj += (2*con.i_dim() + con.inside_face_sign())*params.n_qpoint()/params.row_size;
+          for (int j_dim = 0; j_dim < params.n_dim; ++j_dim) if (j_dim != con.i_dim()) {
+            for (Row_index ind(params.n_dim, params.row_size, j_dim); ind; ++ind) {
+              Mat<> row(params.row_size);
+              for (int i_node = 0; i_node < params.row_size; ++i_node) {
+                row(i_node) = state[ind.i_qpoint(i_node)];
+              }
+              Mat<> lrow = to_lob*row;
+              for (int i_sign = 0; i_sign < 2; ++i_sign) {
+                lrow(i_sign*(lob.row_size - 1)) = con.element().face(2*j_dim + i_sign, true)[ind.i_face_qpoint()];
+              }
+              row = from_lob*lrow;
+              for (int i_node = 0; i_node < params.row_size; ++i_node) {
+                state[ind.i_qpoint(i_node)] = row(i_node);
+              }
+            }
+          }
+          for (Row_index ind(params.n_dim, params.row_size, con.i_dim()); ind; ++ind) {
+            node_adj[ind.i_face_qpoint()] = math::sign(con.inside_face_sign())*state[ind.i_qpoint(0)];
+          }
+        }
+      }
+    }
+  }
+  #pragma omp parallel for
+  for (int i_elem = 0; i_elem < elems.size(); ++i_elem) {
+    double* state = elems[i_elem].state();
+    double* rk_ref = elems[i_elem].residual_cache();
+    for (int i_dof = 0; i_dof < params.n_dof(); ++i_dof) state[i_dof] = rk_ref[i_dof];
+  }
 }
 
