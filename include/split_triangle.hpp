1a4ff30c6ac95e3e11f0f8cc08b4c1f101a72ebb
Micaiah Smith-Pierce <micaiah@gatech.edu>
2024-04-24 17:59:47 -0400

    test for split_triangle (fails)

---
+++ include/split_triangle.hpp
@@ -0,0 +1,25 @@
+#ifndef HEXED_SPLIT_TRIANGLE_HPP_
+#define HEXED_SPLIT_TRIANGLE_HPP_
+
+#include "math.hpp"
+
+namespace hexed
+{
+
+template <int n_dim>
+double triangle_area(Mat<n_dim, 3> triangle)
+{
+  Mat<n_dim> base = triangle(all, 1) - triangle(all, 0);
+  Mat<n_dim> side = triangle(all, 2) - triangle(all, 0);
+  double bsq = base.squaredNorm();
+  return 0.5*sqrt(bsq*(side - side.dot(base)/bsq*base).squaredNorm());
+}
+
+/*! \brief splits a triangle into 4 congruent parts
+ * \param verts each column is one vertex of the triangle
+ * \param params if provided, each column is the parametric coordinates of one vertex with respect to some analytic surface
+ */
+std::vector<std::pair<Mat<3, 3>, Mat<2, 3>>> split_triangle(Mat<3, 3> verts, Mat<2, 3> params = Mat<2, 3>::Zero());
+
+}
+#endif
