6ded91ac5ddfb16192bf2ebc14404fadf4816fa3
micaiah <micaiah@gatech.edu>
2024-05-05 12:18:26 -0400

    docs and cleanup

--- include/Command_input.hpp
+++ include/Command_input.hpp
@@ -1,24 +1,33 @@
 #ifndef HEXED_COMMAND_INPUT_HPP_
 #define HEXED_COMMAND_INPUT_HPP_
 
-#include <termios.h>
 #include <string>
 #include <deque>
 
 namespace hexed
 {
 
+//! \brief Handles command line input with history.
 class Command_input
 {
-  static int _instances;
-  static termios _old_settings;
   int _n_hist;
   std::deque<std::string> _history;
   public:
-  static const int unlimited;
+  static const int unlimited; //! \brief opaque value used to communicate no limit on the size of the history buffer
+  /*! \brief Constructs a `Command_input` and sets the history buffer size.
+   * \details All calls to `get()` with this `Command_input` instance will share the same history.
+   * If `n_history` is not `Command_input::unlimited`,
+   * whenever the history reaches a size of `n_history + 1` entries,
+   * the earliest entry will be forgotten to keep a size of `n_history`.
+   */
   Command_input(int n_history = unlimited);
-  Command_input(const Command_input&);
-  ~Command_input();
+  /*! \brief Obtains a line of input from the command line.
+   * \details Waits for input until a newline or EOF is received.
+   * This input is then returned as a string and added to the history buffer.
+   * The terminal newline or EOF is not included, and only printable characters are included.
+   * The up and down arrows can be used to access and navigate the history buffer.
+   * Empty lines are not added to the history, nor are any lines that are identical to the previous line.
+   */
   std::string get();
 };
 
--- src/Command_input.cpp
+++ src/Command_input.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdio>
+#include <termios.h>
 #include <unistd.h>
 #include <Command_input.hpp>
 #include <assert.hpp>
@@ -8,29 +9,9 @@
 namespace hexed
 {
 
-int Command_input::_instances = 0;
-termios Command_input::_old_settings;
 const int Command_input::unlimited = -1;
 
-Command_input::Command_input(int n_history)
-: _n_hist{n_history}
-{
-  if (!_instances++) {
-    tcgetattr(fileno(stdin), &_old_settings);
-    termios new_settings = _old_settings;
-    new_settings.c_lflag &= ~ECHO;
-    new_settings.c_lflag &= ~ICANON;
-    tcsetattr(fileno(stdin), TCSANOW, &new_settings);
-  }
-}
-
-Command_input::Command_input(const Command_input& other)
-: _n_hist{other._n_hist}, _history{other._history}
-{
-  ++_instances;
-}
-
-Command_input::~Command_input() {if (!--_instances) tcsetattr(fileno(stdin), TCSANOW, &_old_settings);}
+Command_input::Command_input(int n_history) : _n_hist{n_history} {}
 
 void read_char(char* c, int n)
 {
@@ -39,10 +20,18 @@ void read_char(char* c, int n)
 
 std::string Command_input::get()
 {
+  // add entry to history buffer
   while (_history.size() > size_t(_n_hist)) _history.pop_back();
   _history.emplace_front();
   auto display = _history.begin();
   auto begin = _history.begin();
+  // set up termios
+  termios old_settings;
+  tcgetattr(fileno(stdin), &old_settings);
+  termios new_settings = old_settings;
+  new_settings.c_lflag &= ~ECHO;
+  new_settings.c_lflag &= ~ICANON;
+  tcsetattr(fileno(stdin), TCSANOW, &new_settings);
   // get current cursor position
   std::cout << "\x1b[6n" << std::flush;
   char response [100] {};
@@ -52,53 +41,62 @@ std::string Command_input::get()
   }
   HEXED_ASSERT(response[0] == 27 && response[1] == '[', "attempt to obtain cursor position received unintelligible response");
   int line_start = std::stoi(std::find(response, response + 100, ';') + 1);
-  char c;
-  int pos = 0;
+  // function to set the current command to a new (modified) command rather than an unmodified entry from the history
   auto modify = [&]() {
     if (display != begin) {
       *begin = *display;
       display = begin;
     }
   };
+  // obtain input
+  // There will be a string (the latest history entry) and a cursor position.
+  // Every time the user enters a character, first the input string and the cursor position variables are updated,
+  // and then the display is updated to match their current values.
+  char c;
+  int pos = 0; // cursor position
   do {
-    read_char(&c, 1);
-    if (std::isprint(c)) {
+    read_char(&c, 1); // get character
+    if (std::isprint(c)) { // if printable, add to input line
       modify();
       begin->insert(pos++, 1, c);
-    } else if (c == 127) {
+    } else if (c == 127) { // if backspace, delete the previous character
       if (pos) {
         --pos;
         modify();
         begin->erase(pos, 1);
       }
-    } else if (c == 27) {
+    } else if (c == 27) { // if escape sequence...
       std::string escape = "  ";
-      read_char(escape.data(), 2);
-      if (escape == "[A") {
+      read_char(escape.data(), 2); // get rest of escape sequence
+      if (escape == "[A") { // if up arrow, switch to previous history entry
         if (display < _history.end() - 1) {
           ++display;
           pos = display->size();
         }
-      } else if (escape == "[B") {
+      } else if (escape == "[B") { // if down arrow, switch to next history entry
         if (display > begin) {
           --display;
           pos = display->size();
         }
-      } else if (escape == "[C") {
+      } else if (escape == "[C") { // if right arrow, move cursor right
         if (pos < int(display->size())) ++pos;
-      } else if (escape == "[D") {
+      } else if (escape == "[D") { // if left arrow, move cursor left
         if (pos) --pos;
       } else HEXED_ASSERT(false, format_str(100, "could not parse escape sequence `%s` in keyboard input", escape.c_str()));
     }
+    // update the text displayed on the screen
     printf("\x1b[%iG\x1b[K%s\x1b[%iG", line_start, display->c_str(), line_start + pos);
     std::cout << std::flush;
   } while (c != '\n' && c != EOF);
   std::cout << std::endl;
+  // delete latest history entry if it doesn't contain new information
   std::string input = *display;
   modify();
   if (begin->size() == 0) {
     _history.pop_front();
   } else if (_history.size() > 1) if (_history[0] == _history[1]) _history.pop_front();
+  // clean up termios
+  tcsetattr(fileno(stdin), TCSANOW, &old_settings);
   return input;
 }
 
