134f5b3c0e1a561369c28b0fa7b91a1e9982e2dd
micaiah <micaiah@gatech.edu>
2024-06-17 03:04:31 -0400

    submodule shared_ptr

--- python/hexedpy/build_utils.py
+++ python/hexedpy/build_utils.py
@@ -452,6 +452,7 @@ class Xdmf(C_project):
             "mpl",
             "preprocessor",
             "static_assert.hpp",
+            "shared_ptr.hpp",
             "throw_exception.hpp",
             "tokenizer.hpp",
             "type_traits",
