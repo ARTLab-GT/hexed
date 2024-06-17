122b83d0b27f189d4b2dbc12585942b4b544fd65
micaiah <micaiah@gatech.edu>
2024-06-17 02:36:40 -0400

    fixed module bug

--- python/hexedpy/build_utils.py
+++ python/hexedpy/build_utils.py
@@ -422,10 +422,16 @@ class Boost(C_project):
         self.installed_files = {"include":["boost/version.hpp"], "lib":[], "cmake":[f"Boost-{self.version}"]}
         self.modules = modules
         for module in self.modules:
-            self.installed_files["include"].append(f"boost/{module}.hpp")
+            self.installed_files["include"].append(f"boost/{module}")
     def build(self):
         self.builder.assert_command("git", "git")
-        submods = ["libs/" + mod for mod in self.modules] + ["libs/config", "libs/headers", "tools/boost_install", "tools/build"]
+        submods = ["libs/config", "libs/headers", "tools/boost_install", "tools/build"]
+        for mod in self.modules:
+            if "." in mod:
+                mod = mod.split(".")[0]
+            if "/" in mod:
+                mod = mod.split("/")[0]
+            submods.append(f"{self.bdir}/boost/{mod}/.git")
         self[Subprocess](["git", "clone", "https://github.com/boostorg/boost.git"], ["boost"]).do
         self[Subprocess]([
             ["git", "checkout", f"boost-{self.version}"],
@@ -438,7 +444,7 @@ class Boost(C_project):
 class Xdmf(C_project):
     installed_files = {"include":["Xdmf.hpp"], "lib":["Xdmf", "XdmfCore"], "cmake":["Xdmf"]}
     def depends(self):
-        return self[Boost](modules=["assert", "iterator", "tokenizer"]) & self[Libxml2]() & self[HDF5]()
+        return self[Boost](modules=["assert.hpp", "iterator", "tokenizer.hpp"]) & self[Libxml2]() & self[HDF5]()
     def build(self):
         self.builder.assert_command("git", "git")
         self[Subprocess](["git", "clone", "https://gitlab.kitware.com/xdmf/xdmf.git"], ["xdmf"]).do
