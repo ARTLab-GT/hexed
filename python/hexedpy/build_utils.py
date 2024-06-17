428e821accf69d132f61456842de9ca554e7f8f5
micaiah <micaiah@gatech.edu>
2024-06-17 03:06:43 -0400

    submodule smart_ptr

--- python/hexedpy/build_utils.py
+++ python/hexedpy/build_utils.py
@@ -452,7 +452,7 @@ class Xdmf(C_project):
             "mpl",
             "preprocessor",
             "static_assert.hpp",
-            "shared_ptr.hpp",
+            "smart_ptr.hpp",
             "throw_exception.hpp",
             "tokenizer.hpp",
             "type_traits",
