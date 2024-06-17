2ce69bd8045a9f496cd50cd11dfc15b9b6f8dd5e
micaiah <micaiah@gatech.edu>
2024-06-17 03:19:26 -0400

    submodule call_traits

--- python/hexedpy/build_utils.py
+++ python/hexedpy/build_utils.py
@@ -446,6 +446,7 @@ class Xdmf(C_project):
     def depends(self):
         return self[Boost](modules=[
             "assert.hpp",
+            "call_traits.hpp",
             "container_hash",
             "core",
             "detail",
