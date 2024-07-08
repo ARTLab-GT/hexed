74a155d24e4befc1c0d888aa7fe266117635936b
micaiah <micaiah@smithpierce.net>
2024-07-08 11:16:14 -0400

    documented `reciprocal.hpp`

--- include/reciprocal.hpp
+++ include/reciprocal.hpp
@@ -6,6 +6,22 @@
 namespace hexed
 {
 
+/*! \brief Implements pairs of reciprocally-connected pointers.
+ * \details This class addresses the case where object `a` needs to have a pointer to object `b`
+ * iff object `b` has a pointer to `a`.
+ * This might sound like exactly what `mutual::Single` does,
+ * but the problem is that a `Single<T, U>` can _only_ point to a `Single<U, T>`.
+ * If you want an object to have a variety of reciprocal pointers to objects of multiple different types,
+ * you need another layer of indirection:
+ * `a` has a dedicated member which is a pointer object that derives from `Single` to point to `b`,
+ * and `b` needs to have a pointer object that points to `a`,
+ * and these pointers need to know about each other to enforce reciprocity.
+ * Furthermore, both `a` and `b` must be derived from `Mortal`.
+ * That is what this class provides.
+ * If you want objects of class `A` and of class `B` to be able to point to each other,
+ * give them members of type `Reciprocal_ptr<A, B>` and `Reciprocal_ptr<B, A>`, respectively,
+ * and `pair()` the `Reciprocal_ptr`s.
+ */
 template <typename T, typename U>
 class Reciprocal_ptr : public mutual::Single<T, U>, public Pointer<U>
 {
@@ -13,10 +29,18 @@ class Reciprocal_ptr : public mutual::Single<T, U>, public Pointer<U>
   const T* _mine() const {return mine.get();}
 
   public:
+  /*! \brief The object that `this` is a proxy for in reciprocal connections.
+   * \details Usually, `this` will be a member of the object `mine` points to,
+   * but that is not technically necessary.
+   * The user should feel free to modify this data member at will (including setting it to null).
+   */
   Mortal_ptr<T> mine;
+  //! \brief constructs a `Reciprocal_ptr` and sets its `mine` to `data` (which can be null)
   Reciprocal_ptr(T* data) : mine(data) {}
 
   #define ACCESS(CONST) \
+    /* \brief points to the `mine` of the `partner()` of `this` */ \
+    /* \details if not paired, returns `nullptr` */ \
     CONST U* get() CONST \
     { \
       if (this->paired()) return this->_yours(*this->partner()); \
@@ -27,6 +51,10 @@ class Reciprocal_ptr : public mutual::Single<T, U>, public Pointer<U>
   #undef ACCESS
 };
 
+/*! \brief Like `Reciprocal_ptr`, put it can be connected with multiple partners
+ * \details `Reciprocal_list`s can be connected with other `Reciprocal_list`s or with `Reciprocal_ptr`s.
+ * Of course, in both cases connections are reciprocated.
+ */
 template <typename T, typename U>
 class Reciprocal_list : public mutual::Multiple<T, U>
 {
@@ -34,17 +62,25 @@ class Reciprocal_list : public mutual::Multiple<T, U>
   const T* _mine() const {return mine.get();}
 
   public:
+  //! \brief The object that `this` is a proxy for in reciprocal connections.
+  //! \see `Reciprocal_ptr::mine`
   Mortal_ptr<T> mine;
+  //! \brief Constructs a `Reciprocal_list` and sets its `mine` to `data` (which can be null).
   Reciprocal_list(T* data) : mine(data) {}
-
-  void add(mutual::Base<U, T>& other) {this->_connect(other);}
+  //! \brief Reciprocally connects `this` with `that`.
+  //! \details In other words, adds `that` to `this`'s list of partners.
+  void add(mutual::Base<U, T>& that) {this->_connect(that);}
+  //! \brief disconnects all partners
   void clear() {for (auto& p : this->partners()) this->_disconnect(p);}
 
-  void remove(mutual::Base<U, T>& other) {
-    if (this->partners().address().contains(&other)) this->_disconnect(other);
+  //! \brief Reciprocally disconnects `this` from `that`.
+  //! \details In other words, removes `that` from `this`'s list of partners.
+  void remove(mutual::Base<U, T>& that) {
+    if (this->partners().address().contains(&that)) this->_disconnect(that);
   }
 
   #define ACCESS(CONST) \
+    /*! \brief Obtains all of `this`'s partners' `mine`s as a `next::Sequence` */ \
     next::Sequence<CONST U*> theirs() CONST { \
       auto p = this->partners(); \
       return { \
