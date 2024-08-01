719556b9f7b125e479d8d793f3da4bbe21851df8
micaiah <micaiah@smithpierce.net>
2024-08-01 11:14:22 -0400

    pulled updates from `release_cleanup`

--- script/benchmark.py
+++ script/benchmark.py
@@ -5,8 +5,11 @@ import subprocess
 
 root_dir = os.getcwd()
 repo = git.Repo(root_dir)
-#for case in ["naca0012", "flat_plate", "blottner_sphere"]:
-for case in ["naca0012", "flat_plate"]:
+machine = os.uname().nodename
+cases = ["naca0012", "flat_plate"]
+if machine != "micaiah-legion": # my laptop can't handle this with row size 6 for some reason
+    cases.append("blottner_sphere")
+for case in cases:
     print(f"benchmarking case `{case}`")
     os.chdir(f"{root_dir}/samples/{case}/")
     start_time = time.time()
