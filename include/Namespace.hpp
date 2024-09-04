2c6906f4780c551624c443776cc3821369c024a2
Micaiah Smith-Pierce <micaiah@gatech.edu>
2024-09-04 12:35:18 -0400

    declarations and formatting

--- include/Namespace.hpp
+++ include/Namespace.hpp
@@ -9,23 +9,20 @@
 #include <functional>
 #include "utils.hpp"
 #include "assert.hpp"
+#include "Array.hpp"
 
-namespace hexed
-{
+namespace hexed {
 
 
-class Hil_exception : public assert::Exception
-{
+class Hil_exception : public assert::Exception {
   public:
   Hil_exception(std::string message) : Exception(message) {}
 };
 
-class Namespace
-{
+class Namespace {
   public:
   template <typename T>
-  class Variable
-  {
+  class Variable {
     public:
     virtual ~Variable() = default;
     virtual void set(T) = 0;
@@ -33,8 +30,7 @@ class Namespace
   };
 
   template <typename T>
-  class Value : public Variable<T>
-  {
+  class Value : public Variable<T> {
     T _val;
     public:
     Value(T v) : _val(v) {}
@@ -43,14 +39,12 @@ class Namespace
   };
 
   template <typename T>
-  class Heisenberg : public Variable<T>
-  {
+  class Heisenberg : public Variable<T> {
     std::function<T()> fetcher;
     public:
     Heisenberg(std::function<T()>f ) : fetcher{f} {}
     void set(T v) override {throw Hil_exception("attempt to write to `Heisenberg` variable.");}
-    T get() override
-    {
+    T get() override {
       return fetcher();
     }
   };
@@ -59,6 +53,7 @@ class Namespace
   std::map<std::string, std::unique_ptr<Variable<int>>> _ints;
   std::map<std::string, std::unique_ptr<Variable<double>>> _doubles;
   std::map<std::string, std::unique_ptr<Variable<std::string>>> _strings;
+  std::map<std::string, std::unique_ptr<Variable<Array<double>>>> _arrays;
   template<typename T> std::map<std::string, std::unique_ptr<Variable<T>>>& _get_map();
 
   public:
@@ -74,21 +69,21 @@ class Namespace
   std::vector<std::string> names() const;
 };
 
-template<> inline std::map<std::string, std::unique_ptr<Namespace::Variable<int>>>&         Namespace::_get_map() {return _ints;}
-template<> inline std::map<std::string, std::unique_ptr<Namespace::Variable<double>>>&      Namespace::_get_map() {return _doubles;}
-template<> inline std::map<std::string, std::unique_ptr<Namespace::Variable<std::string>>>& Namespace::_get_map() {return _strings;}
+template<> inline std::map<std::string, std::unique_ptr<Namespace::Variable<int>>>&           Namespace::_get_map() {return _ints;}
+template<> inline std::map<std::string, std::unique_ptr<Namespace::Variable<double>>>&        Namespace::_get_map() {return _doubles;}
+template<> inline std::map<std::string, std::unique_ptr<Namespace::Variable<std::string>>>&   Namespace::_get_map() {return _strings;}
+template<> inline std::map<std::string, std::unique_ptr<Namespace::Variable<Array<double>>>>& Namespace::_get_map() {return _arrays;}
 
 template<> std::string inline Namespace::type_name<int>() {return "int";}
 template<> std::string inline Namespace::type_name<double>() {return "double";}
 template<> std::string inline Namespace::type_name<std::string>() {return "string";}
+template<> std::string inline Namespace::type_name<Array<double>>() {return "array";}
 
-inline bool Namespace::exists(std::string name)
-{
+inline bool Namespace::exists(std::string name) {
   return _ints.count(name) || _doubles.count(name) || _strings.count(name);
 }
 
-inline bool Namespace::exists_recursive(std::string name)
-{
+inline bool Namespace::exists_recursive(std::string name) {
   if (exists(name)) return true;
   if (!supers.empty()) {
     auto predicate = [name](std::shared_ptr<Namespace>& space) {return space->exists_recursive(name);};
@@ -98,8 +93,7 @@ inline bool Namespace::exists_recursive(std::string name)
 }
 
 template<typename T>
-void Namespace::create(std::string name, Namespace::Variable<T>* value)
-{
+void Namespace::create(std::string name, Namespace::Variable<T>* value) {
   std::unique_ptr<Variable<T>> ptr(value);
   HEXED_ASSERT(!exists(name),
     format_str(100, "attempt to re-create existing variable `%s` as type `%s`", name.c_str(), type_name<T>().c_str()), Hil_exception)
@@ -107,8 +101,7 @@ void Namespace::create(std::string name, Namespace::Variable<T>* value)
 }
 
 template<typename T>
-void Namespace::assign(std::string name, T value)
-{
+void Namespace::assign(std::string name, T value) {
   if (_get_map<T>().count(name)) return _get_map<T>().at(name)->set(value);
   if (_get_map<double>().count(name)) {
     if constexpr (std::is_same<T, int>::value) {
@@ -119,14 +112,12 @@ void Namespace::assign(std::string name, T value)
 }
 
 template<typename T>
-void Namespace::assign_default(std::string name, T value)
-{
+void Namespace::assign_default(std::string name, T value) {
   if (!exists_recursive(name)) assign(name, value);
 }
 
 template<typename T>
-std::optional<T> Namespace::lookup(std::string name)
-{
+std::optional<T> Namespace::lookup(std::string name) {
   if (_get_map<T>().count(name)) {
     return {_get_map<T>().at(name)->get()};
   }
@@ -144,15 +135,13 @@ std::optional<T> Namespace::lookup(std::string name)
 }
 
 template<typename T>
-T Namespace::get(std::string name)
-{
+T Namespace::get(std::string name) {
   auto val = lookup<T>(name);
   HEXED_ASSERT(val, format_str(1000, "failed to obtain variable `%s` as type `%s`", name.c_str(), typeid(T).name()));
   return *val;
 }
 
-inline std::vector<std::string> Namespace::names() const
-{
+inline std::vector<std::string> Namespace::names() const {
   std::vector<std::string> n;
   for (auto& pair : _ints)    n.push_back(pair.first);
   for (auto& pair : _doubles) n.push_back(pair.first);
