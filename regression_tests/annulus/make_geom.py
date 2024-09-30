fd13b021ac2643476fd85c1da7caa3975ed240b3
micaiah <micaiah@smithpierce.net>
2024-09-30 13:38:03 -0400

    updated regression test

--- regression_tests/annulus/make_geom.py
+++ regression_tests/annulus/make_geom.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-angle = np.linspace(0., 2*np.pi, 10**4)
+angle = np.linspace(0., 2*np.pi, 10**5)
 radius = [1., .25]
 for i in range(2):
     coords = radius[i]*np.array([np.cos(angle), np.sin(angle)]).transpose()
---
+++ regression_tests/run_all.py
@@ -0,0 +1,71 @@
+import subprocess
+import numpy as np
+import pandas as pd
+import sys
+import matplotlib.pyplot as plt
+from matplotlib import colormaps
+import git
+import datetime
+import os
+
+os.chdir(sys.argv[1])
+root_dir = os.getcwd() + "/../.."
+repo = git.Repo(root_dir)
+
+if len(sys.argv) > 2:
+    for degree in np.loadtxt("degree.txt").astype(np.int64):
+        for ref_level in np.loadtxt("ref_level.txt").astype(np.int64):
+            with open("resolution.hil", "w") as resolution:
+                resolution.write(f"""
+                    row_size = {degree + 1}
+                    init_ref_level = {ref_level}
+                """)
+            subprocess.run(["hexecute", "run.hil"])
+
+results = pd.read_csv("results.txt")
+cases = {}
+commit_times = {}
+for i_row in range(results.shape[0]):
+    row_size = results.at[i_row, "row size"]
+    if row_size not in cases.keys():
+        cases[row_size] = {}
+    commit = results.at[i_row, "commit"]
+    commit_times[commit] = repo.commit(commit).committed_date
+    if commit not in cases[row_size].keys():
+        cases[row_size][commit] = []
+    cases[row_size][commit].append(list(results.loc[i_row, ["ref level", "wall time", "error"]]))
+min_time = min(commit_times.values())
+max_time = max(commit_times.values())
+
+colormap = colormaps["plasma"]
+row_sizes = sorted(cases.keys())
+fig, axs = plt.subplots(len(cases.keys()), 2)
+fig.set_size_inches(15, 6*len(row_sizes))
+for i_row_size in range(len(row_sizes)):
+    row_size = row_sizes[i_row_size]
+    def get_ax(col):
+        if len(row_sizes) > 1:
+            return axs[i_row_size, col]
+        else:
+            return axs[col]
+    for commit in cases[row_size].keys():
+        data = np.array(cases[row_size][commit], dtype=np.float64)
+        t = commit_times[commit]
+        color = colormap((t - min_time)/(max_time - min_time))
+        get_ax(0).scatter(data[:, 0], data[:, 2], color=color)
+        get_ax(1).scatter(data[:, 1], data[:, 2], color=color)
+        get_ax(1).annotate("  " + datetime.date.fromtimestamp(t).isoformat(), data[0, [1, 2]])
+        coefs = np.polyfit(data[:, 0], np.log(data[:, 2])/np.log(2), 1)
+        get_ax(0).plot(data[:, 0], 2**(coefs[0]*data[:, 0] + coefs[1]), color=color)
+        get_ax(0).annotate(f"  order = {-coefs[0]:.2f}", data[0, [0, 2]])
+    get_ax(0).set_ylabel(f"$p$ = {row_size - 1}\n$L^2$ nondimensional velocity error")
+    get_ax(0).set_xlabel("refinement level")
+    get_ax(1).set_xlabel("wall clock time, s")
+    get_ax(1).set_xscale("log")
+
+for ax in axs.flatten():
+    ax.set_yscale("log")
+    ax.grid(True)
+plt.savefig("results.svg")
+plt.savefig("results.pdf")
+plt.show()
