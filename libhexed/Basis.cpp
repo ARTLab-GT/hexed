814d09eeb1890ba5752394fdc592ed2682b4f819
micaiah <micaiah@smithpierce.net>
2024-10-08 15:37:37 -0400

    formatting

--- libhexed/Basis.cpp
+++ libhexed/Basis.cpp
@@ -1,30 +1,25 @@
 #include <hexed/Basis.hpp>
 
-namespace hexed
-{
+namespace hexed {
 
-double Basis::max_cfl() const
-{
+double Basis::max_cfl() const {
   return -2*quadratic_safety()/min_eig_convection();
 }
 
-double Basis::step_ratio() const
-{
+double Basis::step_ratio() const {
   return .5/quadratic_safety();
 }
 
 Basis::Basis(int row_size_arg) : row_size(row_size_arg) {}
 Basis::~Basis() {}
 
-Mat<> Basis::nodes() const
-{
+Mat<> Basis::nodes() const {
   Mat<> n(row_size);
   for (int i_node = 0; i_node < row_size; ++i_node) n(i_node) = node(i_node);
   return n;
 }
 
-Mat<dyn, dyn> Basis::interpolate(const Mat<>& sample) const
-{
+Mat<dyn, dyn> Basis::interpolate(const Mat<>& sample) const {
   Mat<dyn, dyn> interp {Mat<dyn, dyn>::Ones(sample.size(), row_size)};
   for (int i_node = 0; i_node < row_size; ++i_node) {
     for (int j_node = 0; j_node < row_size; ++j_node) {
@@ -37,8 +32,7 @@ Mat<dyn, dyn> Basis::interpolate(const Mat<>& sample) const
   return interp;
 }
 
-Mat<dyn, dyn> Basis::prolong(int i_half) const
-{
+Mat<dyn, dyn> Basis::prolong(int i_half) const {
   return interpolate(.5*(nodes() + Mat<>::Constant(row_size, double(i_half))));
 }
 
