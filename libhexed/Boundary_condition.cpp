8b3dcfc8685d258f997b19b2d56a1388218a00e8
micaiah <micaiah@smithpierce.net>
2024-12-05 03:39:17 -0500

    tweaked parameters

--- libhexed/Boundary_condition.cpp
+++ libhexed/Boundary_condition.cpp
@@ -386,7 +386,7 @@ void No_slip::apply_state(Boundary_face& bf) {
   if (params.n_var == params.n_dim + 7) {
     for (int i_qpoint = 0; i_qpoint < nfq; ++i_qpoint) {
       double spec_turb_kin_ener_wall = 0.;
-      double spec_turb_diss_wall = 12.87;
+      double spec_turb_diss_wall = 4e7;
       gh_f[(params.n_dim + 2)*nfq + i_qpoint] = 2*spec_turb_kin_ener_wall*in_f[params.n_dim*nfq + i_qpoint]
                                                 - in_f[(params.n_dim + 2)*nfq + i_qpoint];
       gh_f[(params.n_dim + 3)*nfq + i_qpoint] = 2*spec_turb_diss_wall*in_f[params.n_dim*nfq + i_qpoint]
