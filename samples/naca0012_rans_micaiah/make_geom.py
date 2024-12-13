8a9d2d079f822e236068960b782d73f0d7dbae74
micaiah <micaiah@smithpierce.net>
2024-12-13 03:22:57 -0500

    input file tweaks

--- samples/naca0012_rans_micaiah/make_geom.py
+++ samples/naca0012_rans_micaiah/make_geom.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 from hexedpy.utils import naca
 
 data = naca("0012", n_points = 3*10**4)
+"""
 plt.plot(data[:, 0], data[:, 1])
 data = data[:-1, :]
 n = data.shape[0]
@@ -24,4 +25,5 @@ data = np.concatenate([data, data[[0], :]])
 plt.plot(data[:, 0], data[:, 1])
 plt.axis("equal");
 plt.close();
+"""
 np.savetxt("auto.csv", data, delimiter = ", ")
