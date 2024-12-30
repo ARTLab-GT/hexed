c2a3e3b918106be89062e4a08511c18b16618cc5
micaiah <micaiah@smithpierce.net>
2024-12-30 04:39:41 -0500

    fixed bugs

--- include/pde.hpp
+++ include/pde.hpp
@@ -47,7 +47,6 @@ class Navier_stokes {
     static constexpr double heat_rat = 1.4;
 
     static constexpr double alpha = 13./25.;
-    static constexpr double alpha_s = 1.; //TODO: Confirm definition of alpha_s
     static constexpr double beta_s = 9./100.;
     static constexpr double beta_0 = 0.0708;
     static constexpr double sigma = 1./2.;
@@ -195,8 +194,8 @@ class Navier_stokes {
         double lim_sq = c_lim*c_lim*2*(strain_rate - 1./3.*divergence*identity).squaredNorm()/beta_s;
         double omega_hat = std::pow(math::pow(real_turb_diss, 4) + lim_sq*lim_sq, .25);
 
-        Mat<n_dim, n_dim> turb_stress_per_k = 2*mass/omega_hat*strain_rate - 2./3.*mass*(1. + 1./omega_hat)*identity;
-        Mat<n_dim, n_dim> stress = 2*dyn_visc_coef*strain_rate + (bulk_av*mass - 2./3.*dyn_visc_coef)*identity
+        Mat<n_dim, n_dim> turb_stress_per_k = 2*mass/omega_hat*strain_rate - 2./3.*mass*(1. + divergence/omega_hat)*identity;
+        Mat<n_dim, n_dim> stress = 2*dyn_visc_coef*strain_rate + (bulk_av*mass - 2./3.*dyn_visc_coef)*divergence*identity
                                    + turb_stress_per_k*k_bar;
         Mat<n_update, n_dim> flux_diff_phys = -laplacian_av*gradient; // flux in physical space
         flux_diff_phys(seq, all) -= stress;
@@ -239,13 +238,10 @@ class Navier_stokes {
           prod_per_k = lim_factor*prod_per_k;
           double dot = grad_k.dot(grad_omega);
           double grad_k_omega_source = std::max(sigma_do*mass/real_turb_diss*grad_k.dot(grad_omega), 0.);
-          double grad_omega_source = (dyn_visc_coef + sigma*mass*k_bar/omega_hat)*grad_omega.squaredNorm();
+          double grad_omega_source = (dyn_visc_coef + sigma*mass*k_bar/real_turb_diss)*grad_omega.squaredNorm();
           source(i_turb_kin_ener) = prod_per_k*k_bar - beta_s*real_turb_diss*state(i_turb_kin_ener);
           source(i_turb_diss) = alpha*prod_per_k + grad_omega_source + grad_k_omega_source - beta*mass*real_turb_diss;
           source(i_energy) = -source(i_turb_kin_ener);
-          HEXED_ASSERT(std::isfinite(source.norm()),
-            format_str(200, "source term is not finite %e %e %e %e %e %e %e", state(i_turb_diss), mass, real_turb_diss, omega_hat, prod_per_k, grad_omega_source, grad_k_omega_source),
-            assert::Numerical_exception);
         }
         flux_diff = flux_diff_phys*normal; // flux in reference space
       }
