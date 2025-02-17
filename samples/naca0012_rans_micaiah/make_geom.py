480e58108a4d0a76ba707adab9f03450a538364a
micaiah <micaiah@smithpierce.net>
2025-02-17 16:05:47 -0500

    fixed geometry

--- samples/naca0012_rans_micaiah/make_geom.py
+++ samples/naca0012_rans_micaiah/make_geom.py
@@ -5,25 +5,7 @@ This script is executed by the input file `run.hil`.
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
 from hexedpy.utils import naca
 
-data = naca("0012", n_points=3*10**4)
-plt.plot(data[:, 0], data[:, 1])
-"""
-data = data[:-1, :]
-n = data.shape[0]
-for i in range(10**4):
-    shift0 = data*0
-    shift0[:-1, :] = data[1:, :]
-    shift0[-1, :] = data[0]
-    shift1 = data*0
-    shift1[1:, :] = data[:-1, :]
-    shift1[0, :] = data[-1, :]
-    data = .5*(shift0 + shift1)
-data = np.concatenate([data, data[[0], :]])
-plt.plot(data[:, 0], data[:, 1])
-plt.axis("equal");
-plt.show();
-"""
+data = naca("0012", 2**15 + 1)
 np.savetxt("auto.csv", data, delimiter = ", ")
