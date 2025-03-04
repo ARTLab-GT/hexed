63bdf0538768923ac0f7aacd92a12f2012623248
micaiah <micaiah@smithpierce.net>
2025-03-03 20:50:28 -0500

    reverted `Storage_params`

--- include/Storage_params.hpp
+++ include/Storage_params.hpp
@@ -16,7 +16,6 @@ class Storage_params {
   int n_dim; //!< \brief number of dimensions
   int row_size; //!< \brief \ref basis_row_size "row size" of basis
   int n_forcing = 4; //!< \brief number of artificial viscosity forcing variables
-  int n_rk = 1; //!< \brief number of Runge-Kutta stages for time-accurate iteration
 
   int n_qpoint() const; //!< \brief number of quadrature points per element
   int n_face_qpoint() const; //!< \brief number of quadrature points on each element face
