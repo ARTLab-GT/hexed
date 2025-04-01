d29aafe1e425f023be79f280a85a0770fc73767c
micaiah <micaiah@smithpierce.net>
2025-04-01 15:15:49 -0400

    fixed docs

--- include/Accessible_mesh.hpp
+++ include/Accessible_mesh.hpp
@@ -112,8 +112,10 @@ class Accessible_mesh : public Mesh {
   /*!
    * \param params parameters specifying what data is stored in each Element (row size, number of dimensions, etc.)
    * \param root_size defines the \ref root_size of the mesh.
+   * \param turb defines the turbulence model that will be used by the solver,
+   *     which is relevant because it determines the amound of memory that must be allocated for each element.
    */
-  Accessible_mesh(Storage_params params, double root_size, Turbulence_model);
+  Accessible_mesh(Storage_params params, double root_size, Turbulence_model turb);
   /*! \brief Reads mesh from a file created by `Mesh::write`.
    * \details Acquires ownership of boundary condition pointers.
    * This variant is only for tree meshing.
@@ -146,19 +148,23 @@ class Accessible_mesh : public Mesh {
   void connect_hanging(int coarse_ref_level, Int coarse_serial, std::vector<Int> fine_serial, Con_dir<Deformed_element>,
                        bool coarse_deformed = false, std::vector<bool> fine_deformed = {false, false, false, false},
                        std::array<bool, 2> stretch = {false, false}) override;
-  //! \returns a view of all connections between elements, including one connection for every fine element in hanging node connections.
+  //! \returns a view of all connections between elements,
+  //! including one connection for every fine element in hanging node connections.
   Sequence<Element_connection&>& element_connections() {return elem_cons;}
   int add_boundary_condition(Flow_bc*) override;
-  void connect_boundary(int ref_level, bool is_deformed, Int element_serial_n, int i_dim, int face_sign, int bc_serial_n) override;
+  void connect_boundary(int ref_level, bool is_deformed, Int element_serial_n, int i_dim, int face_sign,
+                        int bc_serial_n) override;
   void disconnect_boundary(int bc_sn) override;
   void cleanup() override;
   next::Sequence<next::Vertex&> shape_vertices() {return _blocks.verts();}
   next::Sequence<next::Vertex&> shape_boundary_vertices() {return _blocks.boundary_verts();}
 
   void add_tree(std::vector<Flow_bc*> extremal_bcs, Mat<> origin = Mat<>::Zero(3)) override;
-  void set_surface(Surface_geom* geometry, Flow_bc* surface_bc, Eigen::VectorXd flood_fill_start = Eigen::VectorXd::Zero(3)) override;
+  void set_surface(Surface_geom* geometry, Flow_bc* surface_bc,
+                   Eigen::VectorXd flood_fill_start = Eigen::VectorXd::Zero(3)) override;
   void set_unref_locks(std::function<bool(Element&)> lock_if = criteria::never) override;
-  bool update(std::function<bool(Element&)> refine_criterion = criteria::always, std::function<bool(Element&)> unrefine_criterion = criteria::never) override;
+  bool update(std::function<bool(Element&)> refine_criterion = criteria::always,
+              std::function<bool(Element&)> unrefine_criterion = criteria::never) override;
   inline int surface_bc_sn() override {return surf_bc_sn;}
   inline Surface_geom& surface_geometry() {return *surf_geom;}
 
@@ -166,8 +172,10 @@ class Accessible_mesh : public Mesh {
    * \details Supply a function that returns `true` for elements that should be operated on.
    * The masked mesh is described by two objects:
    * - a `Kernel_mesh` which includes all the elements where the mask is `true`
-   *   and all the connections and refined faces where the mask is `true` for at least one of the participating elements
-   * - a `Sequence` of `Boundary_connection`s which includes all boundary connections for whose element the mask is `true`
+   *   and all the connections and refined faces where the mask is `true`
+   *   for at least one of the participating elements
+   * - a `Sequence` of `Boundary_connection`s which includes all boundary connections
+   *   for whose element the mask is `true`
    *
    * The members `elements()`, `element_connections()`, etc. will not be affected.
    * This feature has some quirks.
@@ -196,7 +204,8 @@ class Accessible_mesh : public Mesh {
   //! \see `Masked_mesh`
   void reset_masks();
   /*! \brief Experimental feature. Ignore for now.
-   * \details Has to do with an experimental performance-enhancing feature where anisotropic elements are updated more frequently than isotropic ones.
+   * \details Has to do with an experimental performance-enhancing feature
+   * where anisotropic elements are updated more frequently than isotropic ones.
    * Not ready for production use, although it was the motivation for the `Masked_mesh` feature.
    */
   std::vector<std::unique_ptr<Masked_mesh>> preti_masks(const Basis&);
@@ -208,19 +217,22 @@ class Accessible_mesh : public Mesh {
   Flow_bc& boundary_condition(int bc_sn) {return *bound_conds[bc_sn];}
   //! \returns a view of all connections between an element and a boundary condition
   Sequence<Boundary_connection&>& boundary_connections() {return bound_cons;}
-  //! \returns a view of all Refined_face objects owned by this mesh (there will be one for every hanging node connection)
+  //! \returns a view of all Refined_face objects owned by this mesh
+  //! (there will be one for every hanging node connection)
   inline Sequence<Refined_face&>& refined_faces() {return ref_face_v;}
   inline int n_elements() override {return elements().size();}
   Connection_validity valid() override;
   //! \brief if any invalid mesh connections are found, throws an exception with a diagnostic visualization
   void assert_valid();
   //! convenience typedef for the Vector_view used to access Vertex objects
-  void extrude(bool collapse = false, double offset = 0, bool force = false) override; // note: test for this is in `test_Solver.cpp` so that the result can be visualized
+  //! \note test for this is in `test_Solver.cpp` so that the result can be visualized
+  void extrude(bool collapse = false, double offset = 0, bool force = false) override;
   void connect_rest(int bc_sn) override;
   std::vector<elem_handle> elem_handles() override;
   //! \returns a view of all Element_connection between extruded elements and the elemens they were extruded from
   inline Vector_view<Element_connection&, Element_face_connection<Deformed_element>*,
-                     ptr_convert<Element_connection&, Element_face_connection<Deformed_element>*>> extruded_connections() {return {extrude_cons};}
+                     ptr_convert<Element_connection&, Element_face_connection<Deformed_element>*>>
+    extruded_connections() {return {extrude_cons};}
   void write(std::string file_name) override;
   void export_polymesh(std::string dir_name) override;
   void visualize(std::string format, std::string file_name, double time = 0) override;
