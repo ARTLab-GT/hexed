7e0908d35240a26330c61ce6b4cc93fcdaaad7df
micaiah <micaiah@smithpierce.net>
2025-04-09 18:00:58 -0400

    `History_monitor` won't give results until it has enough samples

--- libhexed/History_monitor.cpp
+++ libhexed/History_monitor.cpp
@@ -1,40 +1,46 @@
 #include <hexed/History_monitor.hpp>
-#include <hexed/math.hpp>
 
 namespace hexed {
 
-History_monitor::History_monitor(double window_size, int max_samples)
-: _samples{max_samples}, _start{0}, _sz{0},
+History_monitor::History_monitor(double window_size, Int max_samples, Int min_samples)
+: _samples{max_samples}, _min_samples{min_samples}, _start{0}, _sz{0},
   _iterations(_samples, 0), _values(_samples, 0.),
-  _win_sz{window_size}, _add_threshold{1.}, _min{-huge}, _max{huge}
+  _win_sz{window_size}, _add_threshold{1.}, _min{-std::sqrt(huge)}, _max{std::sqrt(huge)}
 {}
 
-void History_monitor::add_sample(int iteration, double value) {
+void History_monitor::add_sample(Int iteration, double value) {
   if (_sz && iteration < _add_threshold) return;
-  int end = (_start + _sz)%_samples;
+  // everything below will happen only if `iteration` is large enough to merit adding a sample
+  Int end = (_start + _sz)%_samples; // index at which to add the newest sample
+  // add sample
   _iterations[end] = iteration;
   _values[end] = value;
   ++_sz;
+  // if the number of samples has exceeded the desired window size, forget the oldest one
+  // (by simply adjusting the storage window not to include it)
   while (_sz > _samples || _iterations[_start] < iteration*(1 - _win_sz)) {
     _start = (_start + 1)%_samples;
     --_sz;
   }
+  // recompute the min and max over the window
   _min =  huge;
   _max = -huge;
-  for (int offset = 0; offset < _sz; ++offset) {
-    int ind = (_start + offset)%_samples;
+  for (Int offset = 0; offset < _sz; ++offset) {
+    Int ind = (_start + offset)%_samples;
     _min = std::min(_min, _values[ind]);
     _max = std::max(_max, _values[ind]);
   }
+  // adjust the threshold for adding the next sample in a way that will keep it representing
+  // roughly the last `_win_sz` fraction of the iteraions
   while (_add_threshold <= iteration) _add_threshold *= std::pow(1/(1 - _win_sz), 1./_samples);
 }
 
-double History_monitor::min() {
-  return _min;
+double History_monitor::min() const {
+  return _sz >= _min_samples ? _min : -std::sqrt(huge);
 }
 
-double History_monitor::max() {
-  return _max;
+double History_monitor::max() const {
+  return _sz >= _min_samples ? _max : std::sqrt(huge);
 }
 
 void History_monitor::clear() {
--- test/test_History_monitor.cpp
+++ test/test_History_monitor.cpp
@@ -4,11 +4,11 @@
 TEST_CASE("History_monitor") {
   hexed::History_monitor hist(.3, 10);
   for (int i = 0; i < 2; ++i) {
-    REQUIRE(hist.max() ==  std::numeric_limits<double>::max());
-    REQUIRE(hist.min() == -std::numeric_limits<double>::max());
+    REQUIRE(hist.min() < -100.);
+    REQUIRE(hist.max() >  100.);
     hist.add_sample(0, 1.);
-    REQUIRE(hist.max() == Catch::Approx(1.));
-    REQUIRE(hist.min() == Catch::Approx(1.));
+    REQUIRE(hist.min() < -100.);
+    REQUIRE(hist.max() >  100.);
     for (int j = 1; j < 10; ++j) hist.add_sample(j, 1./(j + 1));
     REQUIRE(hist.min() == Catch::Approx(.1));
     REQUIRE(hist.max() == Catch::Approx(1./8.));
