30bbe6b8afbf83281c33cbd16bc8c83f5be8218e
micaiah <micaiah@smithpierce.net>
2025-04-22 01:16:47 -0400

    updates to `format_str`

--- include/assert.hpp
+++ include/assert.hpp
@@ -6,26 +6,11 @@
 #include <string>
 #include <omp.h>
 #include "config.hpp"
+#include "format_str.hpp"
 
 //! \file assert.hpp utilities for custom assertions
 
 namespace hexed {
-
-/*! \brief Standard string formatting.
- * \details Basically a knockoff of `std::format` in C++20 (which at the time of writing we can't use on the lab machines).
- * Invokes `snprintf`, but works in terms of `std::string`s and handles buffer creation for you.
- * Will allocate a buffer of size `max_chars`.
- * Throws an exception if resulting formatted string is larger than `max_chars`.
- * \headerfile utils.hpp
- */
-template <typename... format_args>
-std::string format_str(int max_chars, std::string fstring, format_args... args) {
-  std::vector<char> buffer(max_chars);
-  int overflow = snprintf(buffer.data(), max_chars, fstring.c_str(), args...);
-  if (overflow < 0) throw std::runtime_error("encoding error in `hexed::format_str`");
-  if (overflow >= max_chars) throw std::runtime_error("`max_chars` is too small in `hexed::format_str`");
-  return std::string(buffer.data());
-}
 
 //! \brief utilities for custom assertions
 namespace assert {
