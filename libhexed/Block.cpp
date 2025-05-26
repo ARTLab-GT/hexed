0d4244bd90be8d2d7e4ac3238316ee466f26e4e5
micaiah <micaiah@smithpierce.net>
2025-05-26 19:57:49 -0400

    fixed objective

--- libhexed/Block.cpp
+++ libhexed/Block.cpp
@@ -225,13 +225,9 @@ void Vertex::_compute_state_recursive(_Optimization_state& state, double gradien
         for (int i_dim = 0; i_dim < nd; ++i_dim) {
           double orth_diff = ma.orthogonality(i_dim) - ortho_tolerance;
           state.objective += (!skip_obj)*10./orth_diff;
-          state.gradient += (!skip_grad)*gradient_weight*10.*(-1/orth_diff/orth_diff)
-                            *ma.grad_orth(i_dim, all).transpose();
-          double num = ma.edge_lengths(i_dim) - 1.;
+          double num = ma.edge_lengths(i_dim)*ma.orthogonality(i_dim) - 1.;
           double denom = ma.edge_lengths(i_dim) - edge_tolerance;
           state.objective += (!skip_obj)*num*num/denom;
-          state.gradient += (!skip_grad)*gradient_weight*(2*num/denom - num*num/(denom*denom))
-                            *ma.grad_lengths(i_dim, all).transpose();
         }
         // note: the valid values for `gradient_weight` are 1., .5, and .25
         if (gradient_weight > .3 && that_vert.glued() && i_this != i_that) {
