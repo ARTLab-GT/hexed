bda9bfb4103a7f1e5db9486d6c7de26a371ef4d0
micaiah <micaiah@smithpierce.net>
2025-06-16 18:26:24 -0400

    fixed warnings

--- libhexed/Case.cpp
+++ libhexed/Case.cpp
@@ -382,7 +382,9 @@ Case::Case(std::string input_script)
     else if (!implicit) ts = explicit_unsteady;
     else if (ts_str == "backward euler") ts = backward_euler;
     else if (ts_str == "crank-nicolson") ts = crank_nicolson;
-    else HEXED_THROW("`" + ts_str + "` is not a supported time integration scheme.")
+    else {
+      HEXED_THROW("`" + ts_str + "` is not a supported time integration scheme.") throw;
+    }
     _solver_ptr.reset(new Solver(n_dim, _vari("row_size"), root_size, ts, transport_models[0],
                                  transport_models[1], turb_model, _inter.variables));
     _solver().mesh().add_tree(_make_extremal_bcs(), mesh_extremes(all, 0));
@@ -606,7 +608,7 @@ Case::Case(std::string input_script)
     for (unsigned i_monitor = 0; i_monitor < _monitor_expr->names.size(); ++i_monitor) {
       double val = vals[i_monitor];
       if (be) val -= _vard(_monitor_expr->names[i_monitor] + "_prev");
-      _monitors[i_monitor].add_sample(iter, vals[i_monitor]);
+      _monitors[i_monitor].add_sample(iter, val);
       _inter.variables->assign(_monitor_expr->names[i_monitor] + (be ? "_diff" : "") + "_min", _monitors[i_monitor].min());
       _inter.variables->assign(_monitor_expr->names[i_monitor] + (be ? "_diff" : "") + "_max", _monitors[i_monitor].max());
     }
