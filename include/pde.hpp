e662a714cabc39e5f8120c82975fa894cff42c92
micaiah <micaiah@smithpierce.net>
2026-01-04 02:04:34 -0500

    more experimentation

--- include/pde.hpp
+++ include/pde.hpp
@@ -656,7 +656,14 @@ class Eikonal {
     void compute_flux_conv() {flux_conv = state(Eigen::seqN(1, n_dim)).transpose()*normal*state(0);}
     Mat<n_extrap, n_dim> gradient;
     Mat<n_update, n_dim_flux> flux_diff;
-    void compute_flux_diff() {flux_diff.setZero();}
+    void compute_flux_diff() {
+      flux_diff.setZero();
+      for (int i_dim = 0; i_dim < n_dim_flux; ++i_dim) {
+        for (int j_dim = 0; j_dim < n_dim; ++j_dim) {
+          flux_diff(0, i_dim) += _eq._smoothing*(state(1 + j_dim) - gradient(0, j_dim))*normal(j_dim, i_dim);
+        }
+      }
+    }
     double char_speed;
     void compute_char_speed() {
       char_speed = 0;
@@ -665,16 +672,17 @@ class Eikonal {
     }
     double diffusivity;
     void compute_diffusivity() {
-      char_speed = 0;
-      for (int i_dim = 0; i_dim < n_dim; ++i_dim) char_speed += state(1 + i_dim)*state(1 + i_dim);
-      diffusivity = (1. + _eq._smoothing)*std::abs(state(0)) + _eq._base_diff + _eq._grad_smoothing*std::abs(std::sqrt(char_speed) - 1.);
+      double grad_sq = 0;
+      for (int i_dim = 0; i_dim < n_dim; ++i_dim) grad_sq += state(1 + i_dim)*state(1 + i_dim);
+      double extra_diff = math::pow(std::sqrt(grad_sq) - 1., 2);
+      diffusivity = (1. + _eq._smoothing)*std::abs(state(0)) + _eq._base_diff + _eq._grad_smoothing*extra_diff + _eq._smoothing;
     }
 
     Mat<n_update> source;
     void compute_source() {
-      char_speed = 0;
-      for (int i_dim = 0; i_dim < n_dim; ++i_dim) char_speed += state(1 + i_dim)*state(1 + i_dim);
-      double extra_diff = std::abs(std::sqrt(char_speed) - 1.);
+      double grad_sq = 0;
+      for (int i_dim = 0; i_dim < n_dim; ++i_dim) grad_sq += state(1 + i_dim)*state(1 + i_dim);
+      double extra_diff = math::pow(std::sqrt(grad_sq) - 1., 2);
       source(0) = 1. + ((1. + _eq._smoothing)*std::abs(state(0)) + _eq._grad_smoothing*extra_diff)*state(n_dim + 1);
     }
     double decay;
