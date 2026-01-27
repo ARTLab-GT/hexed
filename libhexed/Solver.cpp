97889a64ab032dd37abf81f67aa74437be498618
micaiah <micaiah@smithpierce.net>
2026-01-27 13:38:33 -0500

    avoid excess visualization of nonphysical states

--- libhexed/Solver.cpp
+++ libhexed/Solver.cpp
@@ -1500,7 +1500,9 @@ bool Solver::fix_nonphysical(double stability_ratio, int sub_iter) {
       printers::warn("Warning: ", true);
       printers::warn(str_cat("Nonphysical flow state detected (solver iteration ", status.iteration,
                              " sub-iteration ", sub_iter, ").\n", is_phys, "\n", "Attempting to fix...\n"));
-      visualize_field("default", str_cat(wd, "nonphysical", status.iteration, "_", sub_iter), vis_expr);
+      if (_namespace->get<int>("vis_field")) {
+        visualize_field("default", str_cat(wd, "nonphysical", status.iteration, "_", sub_iter), vis_expr);
+      }
     }
     printers::warn(format_str("    nonphysical iteration %i\n", iter));
     for (int inner = 0; inner < 100; ++inner, ++iter) {
